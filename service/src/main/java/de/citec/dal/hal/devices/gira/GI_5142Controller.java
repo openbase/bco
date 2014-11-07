7a4a06e2cf9fc06f286ff3f4690fa5f2f0f952d1
Marian Pohling <mpohling@cit-ec.uni-bielefeld.de>
2014-11-07 15:18:56 +0100

    implement multi labels for knx powerswitch

--- service/src/main/java/de/citec/dal/hal/devices/gira/GI_5142Controller.java
+++ service/src/main/java/de/citec/dal/hal/devices/gira/GI_5142Controller.java
@@ -35,15 +35,15 @@ public class GI_5142Controller extends AbstractDeviceController<GI_5133Type.GI_5
     private final ButtonController button_1;
     private final ButtonController button_2;
     private final ButtonController button_3;
-
-    public GI_5142Controller(final String id, final String label, final Location location) throws RSBBindingException {
+    
+    public GI_5142Controller(final String id, final String label, final String[] unitLable, final Location location) throws RSBBindingException {
         super(id, label, location, GI_5133Type.GI_5133.newBuilder());
 
         builder.setId(id);
-        this.button_0 = new ButtonController(COMPONENT_BUTTON_0, label, this, builder.getButton0Builder());
-        this.button_1 = new ButtonController(COMPONENT_BUTTON_1, label, this, builder.getButton1Builder());
-        this.button_2 = new ButtonController(COMPONENT_BUTTON_2, label, this, builder.getButton2Builder());
-        this.button_3 = new ButtonController(COMPONENT_BUTTON_3, label, this, builder.getButton3Builder());
+        this.button_0 = new ButtonController(COMPONENT_BUTTON_0, unitLable[0], this, builder.getButton0Builder());
+        this.button_1 = new ButtonController(COMPONENT_BUTTON_1, unitLable[1], this, builder.getButton1Builder());
+        this.button_2 = new ButtonController(COMPONENT_BUTTON_2, unitLable[2], this, builder.getButton2Builder());
+        this.button_3 = new ButtonController(COMPONENT_BUTTON_3, unitLable[3], this, builder.getButton3Builder());
         this.register(button_0);
         this.register(button_1);
         this.register(button_2);
