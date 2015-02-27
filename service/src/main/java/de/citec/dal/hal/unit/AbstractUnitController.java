66573b4ba0a3863dfc049ea25bb7757a0f9c8233
Marian Pohling <mpohling@cit-ec.uni-bielefeld.de>
2015-02-27 10:22:11 +0100

    MultiExcpetion adjustments.

--- service/src/main/java/de/citec/dal/hal/unit/AbstractUnitController.java
+++ service/src/main/java/de/citec/dal/hal/unit/AbstractUnitController.java
@@ -38,153 +38,151 @@ import rsb.patterns.LocalServer;
  */
 public abstract class AbstractUnitController<M extends GeneratedMessage, MB extends GeneratedMessage.Builder> extends RSBCommunicationService<M, MB> implements Unit {
 
-	public final static String TYPE_FILED_ID = "id";
-	public final static String TYPE_FILED_NAME = "name";
-	public final static String TYPE_FILED_LABEL = "label";
+    public final static String TYPE_FILED_ID = "id";
+    public final static String TYPE_FILED_NAME = "name";
+    public final static String TYPE_FILED_LABEL = "label";
 
-	protected final String id;
-	protected final String name;
-	protected final String label;
-	protected final Location location;
-	private final Device device;
-	private final List<Service> serviceList;
+    protected final String id;
+    protected final String name;
+    protected final String label;
+    protected final Location location;
+    private final Device device;
+    private final List<Service> serviceList;
 
-	public AbstractUnitController(final Class unitClass, final String label, final Device device, final MB builder) throws InstantiationException {
-		this(unitClass, label, device.getLocation(), device, builder);
-	}
+    public AbstractUnitController(final Class unitClass, final String label, final Device device, final MB builder) throws InstantiationException {
+        this(unitClass, label, device.getLocation(), device, builder);
+    }
 
-	public AbstractUnitController(final Class unitClass, final String label, final Location location, final Device device, final MB builder) throws InstantiationException {
-		super(generateScope(generateName(unitClass), label, device), builder);
-		try {
-			this.id = generateID(scope);
-			this.name = generateName();
-			this.label = label;
-			this.device = device;
-			this.location = location;
-			this.serviceList = new ArrayList<>();
+    public AbstractUnitController(final Class unitClass, final String label, final Location location, final Device device, final MB builder) throws InstantiationException {
+        super(generateScope(generateName(unitClass), label, device), builder);
+        try {
+            this.id = generateID(scope);
+            this.name = generateName();
+            this.label = label;
+            this.device = device;
+            this.location = location;
+            this.serviceList = new ArrayList<>();
 
-			setField(TYPE_FILED_ID, id);
-			setField(TYPE_FILED_NAME, name);
-			setField(TYPE_FILED_LABEL, label);
+            setField(TYPE_FILED_ID, id);
+            setField(TYPE_FILED_NAME, name);
+            setField(TYPE_FILED_LABEL, label);
 
-			try {
-				validateUpdateServices();
-			} catch (MultiException ex) {
-				logger.error(this + " is not valid!", ex);
-				ex.printExceptionStack();
-			}
+            try {
+                validateUpdateServices();
+            } catch (MultiException ex) {
+                logger.error(this + " is not valid!", ex);
+                ex.printExceptionStack();
+            }
 
-			DALService.getRegistryProvider().getUnitRegistry().register(this);
+            DALService.getRegistryProvider().getUnitRegistry().register(this);
 
-			init(RSBInformerInterface.InformerType.Distributed);
-		} catch (CouldNotPerformException ex) {
-			throw new InstantiationException(this, ex);
-		}
-	}
+            init(RSBInformerInterface.InformerType.Distributed);
+        } catch (CouldNotPerformException ex) {
+            throw new InstantiationException(this, ex);
+        }
+    }
 
-	@Override
-	public String getId() {
-		return id;
-	}
+    @Override
+    public String getId() {
+        return id;
+    }
 
-	@Override
-	public String getName() {
-		return name;
-	}
+    @Override
+    public String getName() {
+        return name;
+    }
 
-	@Override
-	public String getLabel() {
-		return label;
-	}
+    @Override
+    public String getLabel() {
+        return label;
+    }
 
-	@Override
-	public Location getLocation() {
-		return location;
-	}
+    @Override
+    public Location getLocation() {
+        return location;
+    }
 
-	public Device getDevice() {
-		return device;
-	}
+    public Device getDevice() {
+        return device;
+    }
 
-	public Collection<Service> getServices() {
-		return Collections.unmodifiableList(serviceList);
-	}
+    public Collection<Service> getServices() {
+        return Collections.unmodifiableList(serviceList);
+    }
 
-	public void registerService(final Service service) {
-		serviceList.add(service);
-	}
+    public void registerService(final Service service) {
+        serviceList.add(service);
+    }
 
-	public static String generateID(final String label, final Location location, final Class clazz) {
-		return generateID(generateScope(generateName(clazz), label, location));
-	}
+    public static String generateID(final String label, final Location location, final Class clazz) {
+        return generateID(generateScope(generateName(clazz), label, location));
+    }
 
-	public static String generateID(final Scope scope) {
-		return scope.toString().toLowerCase();
-	}
+    public static String generateID(final Scope scope) {
+        return scope.toString().toLowerCase();
+    }
 
-	public final String generateName() {
-		return generateName(getClass());
-	}
+    public final String generateName() {
+        return generateName(getClass());
+    }
 
-	public static final String generateName(final Class clazz) {
-		return clazz.getSimpleName().replace("Controller", "");
-	}
+    public static final String generateName(final Class clazz) {
+        return clazz.getSimpleName().replace("Controller", "");
+    }
 
-	public Scope generateScope() {
-		return generateScope(generateName(), label, device);
-	}
+    public Scope generateScope() {
+        return generateScope(generateName(), label, device);
+    }
 
-	public static Scope generateScope(final String name, final String label, final Device device) {
-		return generateScope(name, label, device.getLocation());
-	}
+    public static Scope generateScope(final String name, final String label, final Device device) {
+        return generateScope(name, label, device.getLocation());
+    }
 
-	public static Scope generateScope(final String name, final String label, final Location location) {
-		return location.getScope().concat(new Scope(Scope.COMPONENT_SEPARATOR + name).concat(new Scope(Scope.COMPONENT_SEPARATOR + label)));
-	}
+    public static Scope generateScope(final String name, final String label, final Location location) {
+        return location.getScope().concat(new Scope(Scope.COMPONENT_SEPARATOR + name).concat(new Scope(Scope.COMPONENT_SEPARATOR + label)));
+    }
 
-	@Override
-	public String toString() {
-		return getClass().getSimpleName() + "[" + name + "[" + label + "]]";
-	}
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + "[" + name + "[" + label + "]]";
+    }
 
-	@Override
-	public void registerMethods(LocalServer server) throws RSBException {
-		ServiceType.registerServiceMethods(server, this);
-	}
+    @Override
+    public void registerMethods(LocalServer server) throws RSBException {
+        ServiceType.registerServiceMethods(server, this);
+    }
 
-	@Override
-	public ServiceType getServiceType() {
-		return ServiceType.MULTI;
-	}
+    @Override
+    public ServiceType getServiceType() {
+        return ServiceType.MULTI;
+    }
 
-	/**
-	 * *
-	 *
-	 * @throws MultiException
-	 */
-	private void validateUpdateServices() throws MultiException {
-		logger.debug("Validating unit update methods...");
+    /**
+     * *
+     *
+     * @throws MultiException
+     */
+    private void validateUpdateServices() throws MultiException {
+        logger.debug("Validating unit update methods...");
 
-		final Map<Object, Exception> exceptions = new HashMap<>();
-		List<String> unitMethods = new ArrayList<>();
+        MultiException.ExceptionStack exceptionStack = null;
+        List<String> unitMethods = new ArrayList<>();
 
-		// === Transform unit methods into string list. ===
-		for (Method medhod : getClass().getMethods()) {
-			unitMethods.add(medhod.getName());
-		}
+        // === Transform unit methods into string list. ===
+        for (Method medhod : getClass().getMethods()) {
+            unitMethods.add(medhod.getName());
+        }
 
-		// === Validate if all update methods are registrated. ===
-		for (ServiceType service : ServiceType.getServiceTypeList(this)) {
-			for (String serviceMethod : service.getUpdateMethods()) {
-				if (!unitMethods.contains(serviceMethod)) {
-					exceptions.put(this, new ValidationException(this + " dose not contain service method [" + service.getUpdateMethods() + "]"));
-				}
-			}
-		}
+        // === Validate if all update methods are registrated. ===
+        for (ServiceType service : ServiceType.getServiceTypeList(this)) {
+            for (String serviceMethod : service.getUpdateMethods()) {
+                if (!unitMethods.contains(serviceMethod)) {
+                    MultiException.push(service, null, exceptionStack);
+                }
+            }
+        }
 
-		// === throw multi exception in error case. ===
-		if (!exceptions.isEmpty()) {
-			throw new MultiException("Update service not valid!", exceptions);
-		}
-	}
+        // === throw multi exception in error case. ===
+        MultiException.checkAndThrow("Update service not valid!", exceptionStack);
+    }
 }
