cae50ce53d2e64f6ecd3b89620cc63fc728bac8d
Marian Pohling <mpohling@cit-ec.uni-bielefeld.de>
2015-03-13 10:44:57 +0100

    comment correction

--- src/main/java/de/citec/csra/dm/view/JavaFXView.java
+++ src/main/java/de/citec/csra/dm/view/JavaFXView.java
@@ -127,7 +127,7 @@ public class JavaFXView extends Application {
     public static void main(String[] args) {
         logger.info("Start " + APP_NAME + "...");
 
-        /* Setup CLParser */
+        /* Setup JPService */
         JPService.setApplicationName(APP_NAME);
         JPService.registerProperty(JPScope.class, DEFAULT_SCOPE);
         launch(args);
