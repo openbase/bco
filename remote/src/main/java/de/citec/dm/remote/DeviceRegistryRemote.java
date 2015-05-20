00f58751c90b3ebbbf4ad18d3cbe763380b2ffab
Marian Pohling <mpohling@cit-ec.uni-bielefeld.de>
2015-05-20 17:32:55 +0200

    add data sync

--- remote/src/main/java/de/citec/dm/remote/DeviceRegistryRemote.java
+++ remote/src/main/java/de/citec/dm/remote/DeviceRegistryRemote.java
@@ -88,6 +88,7 @@ public class DeviceRegistryRemote extends RSBRemoteService<DeviceRegistry> imple
 
     @Override
     public UnitConfig getUnitConfigById(String unitConfigId) throws CouldNotPerformException {
+        getData();
         for (IdentifiableMessage<String, DeviceConfig, DeviceConfig.Builder> deviceConfig : deviceConfigRemoteRegistry.getEntries()) {
             for (UnitConfig unitConfig : deviceConfig.getMessage().getUnitConfigList()) {
                 if (unitConfig.getId().equals(unitConfigId)) {
