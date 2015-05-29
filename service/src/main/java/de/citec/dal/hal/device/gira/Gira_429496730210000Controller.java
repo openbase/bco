8c0d7937fe3063e8a403a060ed1c8b8dba77acba
Tamino Huxohl <thuxohl@techfak.uni-bielefeld.de>
2015-05-29 10:25:07 +0200

    refactoring of the gira device classes with the new product number

--- service/src/main/java/de/citec/dal/hal/device/gira/Gira_429496730210000Controller.java
+++ service/src/main/java/de/citec/dal/hal/device/gira/Gira_429496730210000Controller.java
@@ -1,4 +1,5 @@
 /*
+
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -12,20 +13,20 @@ import de.citec.jul.exception.InstantiationException;
 import rsb.converter.DefaultConverterRepository;
 import rsb.converter.ProtocolBufferConverter;
 import rst.homeautomation.device.DeviceConfigType;
-import rst.homeautomation.device.gira.Gira_5142Type.Gira_5142;
+import rst.homeautomation.device.gira.Gira_5133Type.Gira_5133;
 
 /**
  *
  * @author mpohling
  */
-public class Gira_5142Controller extends AbstractOpenHABDeviceController<Gira_5142, Gira_5142.Builder> {
+public class Gira_429496730210000Controller extends AbstractOpenHABDeviceController<Gira_5133, Gira_5133.Builder> {
 
     static {
-        DefaultConverterRepository.getDefaultConverterRepository().addConverter(new ProtocolBufferConverter<>(Gira_5142.getDefaultInstance()));
+        DefaultConverterRepository.getDefaultConverterRepository().addConverter(new ProtocolBufferConverter<>(Gira_5133.getDefaultInstance()));
     }
 
-    public Gira_5142Controller(final DeviceConfigType.DeviceConfig config) throws InstantiationException, CouldNotTransformException {
-        super(config, Gira_5142.newBuilder());
+    public Gira_429496730210000Controller(final DeviceConfigType.DeviceConfig config) throws InstantiationException, CouldNotTransformException {
+        super(config, Gira_5133.newBuilder());
         try {
             registerUnits(config.getUnitConfigList());
         } catch (CouldNotPerformException ex) {
