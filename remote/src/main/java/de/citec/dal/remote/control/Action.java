4100456cb6a4d626bfd9aa0772e162387fd1be8a
Divine Threepwood <divinethreepwood@gmail.com>
2015-06-23 00:27:00 +0200

    implement scene and action prototype

---
+++ remote/src/main/java/de/citec/dal/remote/control/Action.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.citec.dal.remote.control;
+
+import de.citec.dal.hal.service.ServiceType;
+import de.citec.jul.exception.CouldNotPerformException;
+import rst.communicationpatterns.TaskStateType;
+import rst.homeautomation.service.ServiceConfigType;
+
+/**
+ *
+ * @author Divine <a href="mailto:DivineThreepwood@gmail.com">Divine</a>
+ */
+public class Action implements ActionService {
+
+    public enum ActionState {
+
+        INITIATED,
+        SCHEDULED,
+        INITIALIZING,
+        EXECUTING,
+        FINISHING,
+        FINISHED,
+        ABORTING,
+        ABORTED,
+        REJECTED,
+        FAILED
+    }
+
+    public enum ActionPriority {
+
+        USER_LOW,
+        USER_NORMAL,
+        USER_HIGHT,
+        SYSTEM_LOW,
+        SYSTEM_NORMAL,
+        SYSTEM_HIGH
+    };
+
+    // config
+    private String name;
+    private String description;
+    private ActionPriority priority;
+    private String responsibleFOR;
+    private long executionDelay;
+    private long period;
+    private ActionState state;
+
+    @Override
+    public void execute() throws CouldNotPerformException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ServiceType getServiceType() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ServiceConfigType.ServiceConfig getServiceConfig() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
---
+++ remote/src/main/java/de/citec/dal/remote/control/ActionService.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.citec.dal.remote.control;
+
+import de.citec.dal.hal.service.Service;
+import de.citec.jul.exception.CouldNotPerformException;
+
+/**
+ *
+ * @author Divine <a href="mailto:DivineThreepwood@gmail.com">Divine</a>
+ */
+public interface ActionService extends Service {
+    public void execute() throws CouldNotPerformException;
+}
