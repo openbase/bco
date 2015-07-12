d0d5cf793f95d1a9733d7ced30778cb83f464c82
Divine Threepwood <DivineThreepwood@gmail.com>
2015-07-12 20:12:52 +0200

    add param support for install script

--- install.sh
+++ install.sh
@@ -4,10 +4,7 @@ clear &&
 echo "=== clean ${APP_NAME} ===" &&
 mvn clean &&
 clear &&
-echo "=== install ${APP_NAME} ===" &&
-mvn install &&
-clear &&
 echo "=== deploy ${APP_NAME} to ${prefix} ===" &&
-mvn install -DassembleDirectory=${prefix} &&
+mvn install -DassembleDirectory=${prefix} $@ &&
 clear &&
 echo "=== ${APP_NAME} is successfully installed to ${prefix} ==="
