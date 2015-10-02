c4f170bee1dedbd4ea8a9038aa57274a65a2f2d0
Tamino Huxohl <thuxohl@techfak.uni-bielefeld.de>
2015-10-02 19:10:09 +0200

    adjusted the tamperStateTransformer for tamper for every value bigger than 0

--- service/src/main/java/de/citec/dal/bindings/openhab/transform/TamperStateTransformer.java
+++ service/src/main/java/de/citec/dal/bindings/openhab/transform/TamperStateTransformer.java
@@ -16,14 +16,13 @@ import rst.homeautomation.state.TamperStateType.TamperState;
 public class TamperStateTransformer {
 
     public static TamperState transform(final double state) throws CouldNotTransformException {
-        switch ((int) state) {
-            case 0:
-                return TamperState.newBuilder().setValue(TamperState.State.NO_TAMPER).build();
-            case 255:
-            case 1:
-                return TamperState.newBuilder().setValue(TamperState.State.TAMPER).build();
-            default:
-                throw new CouldNotTransformException("Could not transform " + Double.class.getName() + "! " + Double.class.getSimpleName() + "[" + state + "] is unknown!");
+        if (state == 0) {
+            return TamperState.newBuilder().setValue(TamperState.State.NO_TAMPER).build();
+        } else if (state > 0) {
+            //TODO:mpohling/thuxohl adjust the tamper state to also reflect the intensity of the alarm!
+            return TamperState.newBuilder().setValue(TamperState.State.TAMPER).build();
+        } else {
+            throw new CouldNotTransformException("Could not transform " + Double.class.getName() + "! " + Double.class.getSimpleName() + "[" + state + "] is unknown!");
         }
     }
 
