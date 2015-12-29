0e40096d7e32bc9c031fcb0e2bae9c652f2da4c1
Divine Threepwood <DivineThreepwood@gmail.com>
2015-12-29 14:14:58 +0100

    Move control components from dal to coma. Cleanup.

--- remote/src/main/java/de/citec/dal/remote/control/location/LocatinRemote.java
+++
@@ -1,9 +0,0 @@
-package de.citec.dal.remote.control.location;
-
-/**
- *
- * @author * @author <a href="mailto:DivineThreepwood@gmail.com">Divine Threepwood</a>
- */
-public class LocatinRemote {
-
-}
