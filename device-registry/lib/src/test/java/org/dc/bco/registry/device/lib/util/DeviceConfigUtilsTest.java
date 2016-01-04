e1f5654f6645db6ec8bf3ffec91a07e45fc5838c
Divine Threepwood <DivineThreepwood@gmail.com>
2016-01-04 15:10:52 +0100

    applied some cleanup.

--- device-registry/lib/src/test/java/org/dc/bco/registry/device/lib/util/DeviceConfigUtilsTest.java
+++ device-registry/lib/src/test/java/org/dc/bco/registry/device/lib/util/DeviceConfigUtilsTest.java
@@ -1,42 +1,36 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package de.citec.dm.core.consistency;
+package org.dc.bco.registry.device.lib.util;
 
-import org.dc.bco.registry.device.lib.util.DeviceConfigUtils;
 import org.junit.After;
 import org.junit.AfterClass;
-import static org.junit.Assert.assertEquals;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import static org.junit.Assert.*;
 import rst.homeautomation.device.DeviceConfigType;
 import rst.homeautomation.unit.UnitConfigType;
 import rst.homeautomation.unit.UnitTemplateType;
 
 /**
  *
- * @author <a href="mailto:mpohling@cit-ec.uni-bielefeld.de">Divine Threepwood</a>
+ * @author * @author <a href="mailto:DivineThreepwood@gmail.com">Divine Threepwood</a>
  */
 public class DeviceConfigUtilsTest {
-    
+
     public DeviceConfigUtilsTest() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
     public void setUp() {
     }
-    
+
     @After
     public void tearDown() {
     }
@@ -49,11 +43,11 @@ public class DeviceConfigUtilsTest {
         System.out.println("checkDuplicatedUnitType");
         DeviceConfigType.DeviceConfig.Builder deviceConfig = DeviceConfigType.DeviceConfig.newBuilder();
         boolean expResult;
-        
+
         expResult = false;
         deviceConfig.addUnitConfig(UnitConfigType.UnitConfig.newBuilder().setType(UnitTemplateType.UnitTemplate.UnitType.LIGHT));
         assertEquals(expResult, DeviceConfigUtils.checkDuplicatedUnitType(deviceConfig.build()));
-        
+
         expResult = true;
         deviceConfig.addUnitConfig(UnitConfigType.UnitConfig.newBuilder().setType(UnitTemplateType.UnitTemplate.UnitType.LIGHT));
         assertEquals(expResult, DeviceConfigUtils.checkDuplicatedUnitType(deviceConfig.build()));
