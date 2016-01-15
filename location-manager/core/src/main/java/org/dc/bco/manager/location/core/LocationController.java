bab308642f0cf4969c27e63f71f32250ab3c3457
Divine Threepwood <DivineThreepwood@gmail.com>
2016-01-15 05:54:44 +0100

    implement configurable interface for location controller.

--- location-manager/core/src/main/java/org/dc/bco/manager/location/core/LocationController.java
+++ location-manager/core/src/main/java/org/dc/bco/manager/location/core/LocationController.java
@@ -29,6 +29,11 @@ public class LocationController extends AbstractUnitCollectionController<Generic
     }
 
     @Override
+    public LocationConfig getConfig() {
+        return config;
+    }
+
+    @Override
     public String getId() throws CouldNotPerformException {
         return config.getId();
     }
