c781757d62f6be07dd9ec2dd7ecb9c7bf2f07482
Tamino Huxohl <thuxohl@techfak.uni-bielefeld.de>
2016-05-06 12:12:48 +0200

    search for the unit template by type in the AbstractUnitController

--- lib/src/main/java/org/dc/bco/dal/lib/layer/unit/AbstractUnitController.java
+++ lib/src/main/java/org/dc/bco/dal/lib/layer/unit/AbstractUnitController.java
@@ -153,7 +153,7 @@ public abstract class AbstractUnitController<M extends GeneratedMessage, MB exte
 
     @Override
     public UnitConfig applyConfigUpdate(final UnitConfig config) throws CouldNotPerformException, InterruptedException {
-        unitTemplate = CachedDeviceRegistryRemote.getDeviceRegistry().getUnitTemplateById(config.getUnitTemplateConfigId());
+        unitTemplate = CachedDeviceRegistryRemote.getDeviceRegistry().getUnitTemplateByType(config.getType());
         return super.applyConfigUpdate(config);
     }
 
