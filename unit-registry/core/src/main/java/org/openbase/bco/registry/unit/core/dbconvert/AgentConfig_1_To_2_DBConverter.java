128453508287a4ebb85a29225bbdb8ee5e42b396
Tamino Huxohl <thuxohl@techfak.uni-bielefeld.de>
2016-10-12 13:52:37 +0200

    remove DeviceConfig and DalUnitConfig versionConsistencyHandler and moved their functionality to the DeviceConfig version 5 dbConverter

--- unit-registry/core/src/main/java/org/openbase/bco/registry/unit/core/dbconvert/AgentConfig_1_To_2_DBConverter.java
+++ unit-registry/core/src/main/java/org/openbase/bco/registry/unit/core/dbconvert/AgentConfig_1_To_2_DBConverter.java
@@ -24,6 +24,8 @@ package org.openbase.bco.registry.unit.core.dbconvert;
 import com.google.gson.JsonObject;
 import java.io.File;
 import java.util.Map;
+import org.openbase.bco.registry.unit.lib.generator.UnitConfigIdGenerator;
+import org.openbase.jul.exception.CouldNotPerformException;
 import org.openbase.jul.storage.registry.version.DBVersionConverter;
 import rst.domotic.unit.UnitTemplateType.UnitTemplate.UnitType;
 
@@ -32,35 +34,45 @@ import rst.domotic.unit.UnitTemplateType.UnitTemplate.UnitType;
  * @author <a href="mailto:pleminoq@openbase.org">Tamino Huxohl</a>
  */
 public class AgentConfig_1_To_2_DBConverter implements DBVersionConverter {
-
+    
     private static final String TYPE_FIELD = "type";
+    private static final String ID_FIELD = "id";
     private static final String AGENT_CLASS_ID_FIELD = "agent_class_id";
     private static final String AGENT_CONFIG_FIELD = "agent_config";
     private static final String LOCATION_ID_FIELD = "location_id";
     private static final String PLACEMENT_CONFIG_FIELD = "placement_config";
-
+    
+    private final UnitConfigIdGenerator idGenerator;
+    
+    public AgentConfig_1_To_2_DBConverter() {
+        this.idGenerator = new UnitConfigIdGenerator();
+    }
+    
     @Override
-    public JsonObject upgrade(JsonObject unitConfig, final Map<File, JsonObject> dbSnapshot) {
+    public JsonObject upgrade(JsonObject agentUnitConfig, final Map<File, JsonObject> dbSnapshot) throws CouldNotPerformException {
         // add type
-        unitConfig.addProperty(TYPE_FIELD, UnitType.AGENT.name());
+        agentUnitConfig.addProperty(TYPE_FIELD, UnitType.AGENT.name());
 
         // move class to agent config
-        if (unitConfig.has(AGENT_CLASS_ID_FIELD)) {
+        if (agentUnitConfig.has(AGENT_CLASS_ID_FIELD)) {
             JsonObject agentConfig = new JsonObject();
-            agentConfig.addProperty(AGENT_CLASS_ID_FIELD, unitConfig.get(AGENT_CLASS_ID_FIELD).getAsString());
-            unitConfig.remove(AGENT_CLASS_ID_FIELD);
-            unitConfig.add(AGENT_CONFIG_FIELD, agentConfig);
+            agentConfig.addProperty(AGENT_CLASS_ID_FIELD, agentUnitConfig.get(AGENT_CLASS_ID_FIELD).getAsString());
+            agentUnitConfig.remove(AGENT_CLASS_ID_FIELD);
+            agentUnitConfig.add(AGENT_CONFIG_FIELD, agentConfig);
         }
 
         // move location id to placement config
-        if (unitConfig.has(LOCATION_ID_FIELD)) {
+        if (agentUnitConfig.has(LOCATION_ID_FIELD)) {
             JsonObject placementConfig = new JsonObject();
-            placementConfig.addProperty(LOCATION_ID_FIELD, unitConfig.get(LOCATION_ID_FIELD).getAsString());
-            unitConfig.remove(LOCATION_ID_FIELD);
-            unitConfig.add(PLACEMENT_CONFIG_FIELD, placementConfig);
+            placementConfig.addProperty(LOCATION_ID_FIELD, agentUnitConfig.get(LOCATION_ID_FIELD).getAsString());
+            agentUnitConfig.remove(LOCATION_ID_FIELD);
+            agentUnitConfig.add(PLACEMENT_CONFIG_FIELD, placementConfig);
         }
-
-        return unitConfig;
+        
+        agentUnitConfig.remove(ID_FIELD);
+        agentUnitConfig.addProperty(ID_FIELD, idGenerator.generateId(null));
+        
+        return agentUnitConfig;
     }
-
+    
 }
--- unit-registry/core/src/main/java/org/openbase/bco/registry/unit/core/dbconvert/DeviceConfig_4_To_5_DBConverter.java
+++ unit-registry/core/src/main/java/org/openbase/bco/registry/unit/core/dbconvert/DeviceConfig_4_To_5_DBConverter.java
@@ -25,17 +25,26 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import java.io.File;
+import java.util.HashMap;
 import java.util.Map;
 import org.openbase.bco.registry.unit.lib.generator.UnitConfigIdGenerator;
+import org.openbase.bco.registry.unit.lib.jp.JPDalUnitConfigDatabaseDirectory;
+import org.openbase.jps.core.JPService;
+import org.openbase.jps.exception.JPNotAvailableException;
 import org.openbase.jul.exception.CouldNotPerformException;
-import org.openbase.jul.storage.registry.version.DBVersionConverter;
+import org.openbase.jul.exception.InvalidStateException;
+import org.openbase.jul.processing.StringProcessor;
+import org.openbase.jul.storage.registry.version.GlobalDbVersionConverter;
 import rst.domotic.unit.UnitTemplateType.UnitTemplate.UnitType;
 
 /**
+ * DBConverter that converts deviceConfigs to the everything is a unit update, replaces the deviceClass IDs
+ * with UUIDs and creates dalUnitConfigs.
+ *
  *
  * @author <a href="mailto:pleminoq@openbase.org">Tamino Huxohl</a>
  */
-public class DeviceConfig_4_To_5_DBConverter implements DBVersionConverter {
+public class DeviceConfig_4_To_5_DBConverter implements GlobalDbVersionConverter {
 
     private static final String TYPE_FIELD = "type";
     private static final String ID_FIELD = "id";
@@ -45,17 +54,64 @@ public class DeviceConfig_4_To_5_DBConverter implements DBVersionConverter {
     private static final String UNIT_ID_FIELD = "unit_id";
     private static final String UNIT_CONFIG_FIELD = "unit_config";
     private static final String DEVICE_CONFIG_FIELD = "device_config";
+    private static final String COMPANY_FIELD = "company";
+    private static final String PRODUCT_NUMBER_FIELD = "product_number";
+
+    private static final String SYSTEM_UNIT_ID_FIELD = "system_unit_id";
+    private static final String BOUND_TO_SYSTEM_UNIT_FIELD = "bound_to_system_unit";
+    private static final String UNIT_HOST_ID_FIELD = "unit_host_id";
+    private static final String BOUND_TO_UNIT_HOST_FIELD = "bound_to_unit_host";
 
     private final UnitConfigIdGenerator idGenerator;
+    private final Map<String, String> deviceClassIdMap;
 
     public DeviceConfig_4_To_5_DBConverter() {
         this.idGenerator = new UnitConfigIdGenerator();
+        this.deviceClassIdMap = new HashMap<>();
     }
 
     @Override
-    public JsonObject upgrade(JsonObject deviceUnitConfig, final Map<File, JsonObject> dbSnapshot) throws CouldNotPerformException {
-        // move all fields special for devices into the deviceConfig
+    public JsonObject upgrade(JsonObject deviceUnitConfig, Map<File, JsonObject> dbSnapshot, Map<String, Map<File, JsonObject>> globalDbSnapshots) throws CouldNotPerformException {
+        // replace the old id with a UUID
+        deviceUnitConfig.remove(ID_FIELD);
+        deviceUnitConfig.addProperty(ID_FIELD, idGenerator.generateId(null));
+
+        // add type
+        deviceUnitConfig.addProperty(TYPE_FIELD, UnitType.DEVICE.name());
+
+        // add deviceConfig
         JsonObject deviceConfig = new JsonObject();
+        // register all dal units from this device config and replace them with their ids in the device config
+        if (deviceUnitConfig.has(UNIT_CONFIG_FIELD)) {
+            JsonArray unitConfigs = deviceUnitConfig.getAsJsonArray(UNIT_CONFIG_FIELD);
+            JsonArray unitIds = new JsonArray();
+            for (JsonElement unitConfigElem : unitConfigs) {
+                JsonObject unitConfig = unitConfigElem.getAsJsonObject();
+
+                if (unitConfig.has(SYSTEM_UNIT_ID_FIELD)) {
+                    unitConfig.remove(SYSTEM_UNIT_ID_FIELD);
+                }
+                unitConfig.add(UNIT_HOST_ID_FIELD, deviceUnitConfig.get(ID_FIELD));
+
+                if (unitConfig.has(BOUND_TO_SYSTEM_UNIT_FIELD)) {
+                    unitConfig.add(BOUND_TO_UNIT_HOST_FIELD, unitConfig.get(BOUND_TO_SYSTEM_UNIT_FIELD));
+                    unitConfig.remove(BOUND_TO_SYSTEM_UNIT_FIELD);
+                }
+
+                try {
+                    File dalUnitConfigDir = JPService.getProperty(JPDalUnitConfigDatabaseDirectory.class).getValue();
+                    globalDbSnapshots.get("dal-unit-config").put(new File(dalUnitConfigDir, unitConfig.get(ID_FIELD).getAsString()), unitConfig);
+                } catch (JPNotAvailableException ex) {
+                    throw new CouldNotPerformException("Could not acces dal unit config database directory!", ex);
+                }
+                unitIds.add(unitConfig.get(ID_FIELD).getAsString());
+            }
+
+            deviceConfig.add(UNIT_ID_FIELD, unitIds);
+            deviceUnitConfig.remove(UNIT_CONFIG_FIELD);
+        }
+
+        // move all fields special for devices into the deviceConfig
         if (deviceUnitConfig.has(SERIAL_NUMBER_FIELD)) {
             deviceConfig.add(SERIAL_NUMBER_FIELD, deviceUnitConfig.get(SERIAL_NUMBER_FIELD));
             deviceUnitConfig.remove(SERIAL_NUMBER_FIELD);
@@ -64,28 +120,53 @@ public class DeviceConfig_4_To_5_DBConverter implements DBVersionConverter {
             deviceConfig.add(INVENTORY_STATE_FIELD, deviceUnitConfig.get(INVENTORY_STATE_FIELD));
             deviceUnitConfig.remove(INVENTORY_STATE_FIELD);
         }
-        if (deviceUnitConfig.has(DEVICE_CLASS_ID_FIELD)) {
-            deviceConfig.add(DEVICE_CLASS_ID_FIELD, deviceUnitConfig.get(DEVICE_CLASS_ID_FIELD));
-            deviceUnitConfig.remove(DEVICE_CLASS_ID_FIELD);
-        }
-        if (deviceUnitConfig.has(UNIT_CONFIG_FIELD)) {
-            JsonArray unitConfigs = deviceUnitConfig.getAsJsonArray(UNIT_CONFIG_FIELD);
-            JsonArray unitIds = new JsonArray();
-            for (JsonElement unitConfig : unitConfigs) {
-                unitIds.add(unitConfig.getAsJsonObject().get(ID_FIELD).getAsString());
+
+        //TODO:this step can only be executed if the ids in device classes have been updated which is the version update from 1 to 2
+        // update the device class id to its new UUID and put it in the device config
+        if (deviceClassIdMap.isEmpty()) {
+            try {
+                for (JsonObject deviceClass : globalDbSnapshots.get("device-class").values()) {
+                    deviceClassIdMap.put(getOldDeviceClassId(deviceClass), deviceClass.get(ID_FIELD).getAsString());
+                }
+            } catch (CouldNotPerformException ex) {
+                deviceClassIdMap.clear();
+                throw new CouldNotPerformException("Could not build deviceClass id map!", ex);
             }
-            deviceConfig.add(UNIT_ID_FIELD, unitIds);
-            deviceUnitConfig.remove(UNIT_CONFIG_FIELD);
+        }
+        if (deviceUnitConfig.has(DEVICE_CLASS_ID_FIELD)) {
+            deviceConfig.addProperty(DEVICE_CLASS_ID_FIELD, deviceClassIdMap.get(deviceUnitConfig.get(DEVICE_CLASS_ID_FIELD).getAsString()));
+            deviceUnitConfig.remove(DEVICE_CLASS_ID_FIELD);
         }
         deviceUnitConfig.add(DEVICE_CONFIG_FIELD, deviceConfig);
 
-        // add type
-        deviceUnitConfig.addProperty(TYPE_FIELD, UnitType.DEVICE.name());
-
-        // replace the old id with a UUID
-        deviceUnitConfig.remove(ID_FIELD);
-        deviceUnitConfig.addProperty(ID_FIELD, idGenerator.generateId(null));
-
         return deviceUnitConfig;
+    }
+
+    /**
+     * Get the id of a deviceClass before the change to UUIDs.
+     *
+     * @param deviceClass the JSON Object of a deviceClass of which the id is generated
+     * @return the old ID for the deviceClass
+     * @throws CouldNotPerformException if some fields in the deviceClass which are needed are not set
+     */
+    private String getOldDeviceClassId(JsonObject deviceClass) throws CouldNotPerformException {
+        String id;
+        try {
+            if (!deviceClass.has(PRODUCT_NUMBER_FIELD)) {
+                throw new InvalidStateException("Field [ProductNumber] is missing!");
+            }
+
+            if (!deviceClass.has(COMPANY_FIELD)) {
+                throw new InvalidStateException("Field [Company] is missing!");
+            }
+
+            id = deviceClass.get(COMPANY_FIELD).getAsString();
+            id += "_";
+            id += deviceClass.get(PRODUCT_NUMBER_FIELD).getAsString();
+
+            return StringProcessor.transformToIdString(id);
+        } catch (CouldNotPerformException ex) {
+            throw new CouldNotPerformException("Could not generate id!", ex);
+        }
     }
 }
