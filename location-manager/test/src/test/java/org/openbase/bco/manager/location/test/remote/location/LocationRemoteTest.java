ab5c7b2b6a2ea9c31535879d0749c4e8917b7a13
Tamino Huxohl <thuxohl@techfak.uni-bielefeld.de>
2016-11-25 13:17:52 +0100

    added locationRemote test

--- location-manager/test/src/test/java/org/openbase/bco/manager/location/test/remote/location/LocationRemoteTest.java
+++ location-manager/test/src/test/java/org/openbase/bco/manager/location/test/remote/location/LocationRemoteTest.java
@@ -21,17 +21,43 @@ package org.openbase.bco.manager.location.test.remote.location;
  * <http://www.gnu.org/licenses/gpl-3.0.html>.
  * #L%
  */
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 import org.junit.After;
 import org.junit.AfterClass;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openbase.bco.dal.lib.jp.JPHardwareSimulationMode;
+import org.openbase.bco.dal.lib.layer.service.operation.PowerStateOperationService;
+import org.openbase.bco.dal.lib.layer.unit.TemperatureControllerController;
+import org.openbase.bco.dal.lib.layer.unit.TemperatureSensorController;
+import org.openbase.bco.dal.lib.layer.unit.UnitController;
+import org.openbase.bco.dal.remote.unit.location.LocationRemote;
+import org.openbase.bco.manager.device.core.DeviceManagerLauncher;
+import org.openbase.bco.manager.location.core.LocationManagerLauncher;
+import org.openbase.bco.registry.location.lib.LocationRegistry;
+import org.openbase.bco.registry.location.remote.CachedLocationRegistryRemote;
+import org.openbase.bco.registry.mock.MockRegistry;
+import org.openbase.bco.registry.mock.MockRegistryHolder;
+import org.openbase.bco.registry.unit.lib.UnitRegistry;
+import org.openbase.bco.registry.unit.remote.CachedUnitRegistryRemote;
 import org.openbase.jps.core.JPService;
 import org.openbase.jps.exception.JPServiceException;
 import org.openbase.jul.exception.CouldNotPerformException;
 import org.openbase.jul.exception.InitializationException;
 import org.openbase.jul.exception.InstantiationException;
 import org.openbase.jul.exception.InvalidStateException;
+import org.openbase.jul.pattern.Remote;
 import org.slf4j.LoggerFactory;
+import rst.domotic.service.ServiceTemplateType.ServiceTemplate;
+import rst.domotic.service.ServiceTemplateType.ServiceTemplate.ServicePattern;
+import rst.domotic.service.ServiceTemplateType.ServiceTemplate.ServiceType;
+import rst.domotic.state.PowerStateType.PowerState;
+import rst.domotic.state.TemperatureStateType.TemperatureState;
+import rst.domotic.unit.UnitConfigType.UnitConfig;
 
 /**
  *
@@ -41,16 +67,49 @@ public class LocationRemoteTest {
 
     private static final org.slf4j.Logger logger = LoggerFactory.getLogger(LocationRemoteTest.class);
 
+    private static DeviceManagerLauncher deviceManagerLauncher;
+    private static LocationManagerLauncher locationManagerLauncher;
+    private static MockRegistry registry;
+
+    private static LocationRegistry locationRegistry;
+    private static UnitRegistry unitRegistry;
+
+    private static LocationRemote locationRemote;
+
     public LocationRemoteTest() {
     }
 
     @BeforeClass
     public static void setUpClass() throws InitializationException, InvalidStateException, InstantiationException, CouldNotPerformException, JPServiceException, InterruptedException {
         JPService.setupJUnitTestMode();
+        JPService.registerProperty(JPHardwareSimulationMode.class, true);
+        registry = MockRegistryHolder.newMockRegistry();
+
+        deviceManagerLauncher = new DeviceManagerLauncher();
+        deviceManagerLauncher.launch();
+        deviceManagerLauncher.getLaunchable().waitForInit(30, TimeUnit.SECONDS);
+
+        locationManagerLauncher = new LocationManagerLauncher();
+        locationManagerLauncher.launch();
+
+        locationRegistry = CachedLocationRegistryRemote.getRegistry();
+        unitRegistry = CachedUnitRegistryRemote.getRegistry();
+
+        locationRemote = new LocationRemote();
+        locationRemote.init(locationRegistry.getRootLocationConfig());
+        locationRemote.activate();
+        locationRemote.waitForConnectionState(Remote.ConnectionState.CONNECTED);
     }
 
     @AfterClass
     public static void tearDownClass() throws CouldNotPerformException, InterruptedException {
+        if (deviceManagerLauncher != null) {
+            deviceManagerLauncher.shutdown();
+        }
+        if (locationRemote != null) {
+            locationRemote.shutdown();
+        }
+        MockRegistryHolder.shutdownMockRegistry();
     }
 
     @Before
@@ -60,6 +119,72 @@ public class LocationRemoteTest {
 
     @After
     public void tearDown() throws CouldNotPerformException {
-
     }
+
+    @Test(timeout = 5000)
+    public void testLocationToUnitPipeline() throws Exception {
+        System.out.println("testLocationToUnitPipeline");
+
+        List<PowerStateOperationService> powerServiceList = new ArrayList<>();
+        for (UnitConfig dalUnitConfig : unitRegistry.getDalUnitConfigs()) {
+            if (unitHasService(dalUnitConfig, ServiceType.POWER_STATE_SERVICE, ServicePattern.OPERATION)) {
+                UnitController unitController = deviceManagerLauncher.getLaunchable().getUnitControllerRegistry().get(dalUnitConfig.getId());
+                powerServiceList.add((PowerStateOperationService) unitController);
+            }
+        }
+
+        PowerState powerOn = PowerState.newBuilder().setValue(PowerState.State.ON).build();
+        PowerState powerOff = PowerState.newBuilder().setValue(PowerState.State.OFF).build();
+
+        locationRemote.setPowerState(powerOn).get();
+        for (PowerStateOperationService powerStateService : powerServiceList) {
+            Assert.assertEquals("PowerState of unit [" + ((UnitController) powerStateService).getLabel() + "] has not been updated by the loationRemote!", powerOn.getValue(), powerStateService.getPowerState().getValue());
+        }
+
+        locationRemote.setPowerState(powerOff).get();
+        for (PowerStateOperationService powerStateService : powerServiceList) {
+            Assert.assertEquals("PowerState of unit [" + ((UnitController) powerStateService).getLabel() + "] has not been updated by the loationRemote!", powerOff.getValue(), powerStateService.getPowerState().getValue());
+        }
+    }
+
+    private boolean unitHasService(UnitConfig unitConfig, ServiceType serviceType, ServicePattern servicePattern) throws CouldNotPerformException {
+        for (ServiceTemplate serviceTemplate : unitRegistry.getUnitTemplateByType(unitConfig.getType()).getServiceTemplateList()) {
+            if (serviceTemplate.getType() == serviceType && serviceTemplate.getPattern() == servicePattern) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Test(timeout = 5000)
+    public void testUnitToLocationPipeline() throws Exception {
+        System.out.println("testUnitToLocationPipeline");
+
+        List<TemperatureSensorController> temperatureSensorList = new ArrayList<>();
+        List<TemperatureControllerController> temperatureControllerList = new ArrayList<>();
+        for (UnitConfig dalUnitConfig : unitRegistry.getDalUnitConfigs()) {
+            UnitController unitController = deviceManagerLauncher.getLaunchable().getUnitControllerRegistry().get(dalUnitConfig.getId());
+            if (unitController instanceof TemperatureSensorController) {
+                temperatureSensorList.add((TemperatureSensorController) unitController);
+            } else if (unitController instanceof TemperatureControllerController) {
+                temperatureControllerList.add((TemperatureControllerController) unitController);
+            }
+        }
+
+        double temperature = 21;
+        TemperatureState temperatureState = TemperatureState.newBuilder().setTemperature(temperature).build();
+        for (TemperatureSensorController temperatureSensor : temperatureSensorList) {
+            temperatureSensor.updateTemperatureStateProvider(temperatureState);
+        }
+        for (TemperatureControllerController temperatureController : temperatureControllerList) {
+            temperatureController.updateTemperatureStateProvider(temperatureState);
+        }
+
+        locationRemote.requestData().get();
+        while (locationRemote.getTemperatureState().getTemperature() != temperature) {
+            Thread.sleep(50);
+        }
+        Assert.assertEquals("Temperature of the location has not been updated!", temperature, locationRemote.getTemperatureState().getTemperature(), 0.01);
+    }
+
 }
