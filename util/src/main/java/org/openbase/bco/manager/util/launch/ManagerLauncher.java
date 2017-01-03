02e3efcf789bb778a834772b3fe848019c24026d
Marian Pohling <mpohling@cit-ec.uni-bielefeld.de>
2017-01-03 03:55:50 +0100

    License header fixed.

--- util/src/main/java/org/openbase/bco/manager/util/launch/ManagerLauncher.java
+++ util/src/main/java/org/openbase/bco/manager/util/launch/ManagerLauncher.java
@@ -1,6 +1,28 @@
 
 package org.openbase.bco.manager.util.launch;
 
+/*
+ * #%L
+ * BCO Manager Utility
+ * %%
+ * Copyright (C) 2015 - 2017 openbase.org
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+
 import org.openbase.bco.manager.agent.core.AgentManagerLauncher;
 import org.openbase.bco.manager.app.core.AppManagerLauncher;
 import org.openbase.bco.manager.device.core.DeviceManagerLauncher;
@@ -11,9 +33,6 @@ import org.openbase.bco.registry.lib.launch.AbstractLauncher;
 import org.openbase.jul.pattern.Manager;
 
 /**
- * #%L
- * #L%
- *
  * @author <a href="mailto:divine@openbase.org">Divine Threepwood</a>
  */
 public class ManagerLauncher {
