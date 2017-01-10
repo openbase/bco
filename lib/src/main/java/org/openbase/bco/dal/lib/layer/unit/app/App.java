44952213ebc97415dd4c58945fbf4beb418e5672
Marian Pohling <mpohling@cit-ec.uni-bielefeld.de>
2017-01-10 23:35:56 +0100

    License header 2017 upgrade.

--- lib/src/main/java/org/openbase/bco/dal/lib/layer/unit/app/App.java
+++ lib/src/main/java/org/openbase/bco/dal/lib/layer/unit/app/App.java
@@ -6,9 +6,9 @@ import rst.domotic.unit.UnitConfigType.UnitConfig;
 
 /*
  * #%L
- * COMA AppManager Library
+ * BCO DAL Library
  * %%
- * Copyright (C) 2015 - 2016 openbase.org
+ * Copyright (C) 2014 - 2017 openbase.org
  * %%
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
--- remote/src/main/java/org/openbase/bco/dal/remote/unit/UnitGroupRemote.java
+++ remote/src/main/java/org/openbase/bco/dal/remote/unit/UnitGroupRemote.java
@@ -5,7 +5,7 @@ package org.openbase.bco.dal.remote.unit;
  * #%L
  * BCO DAL Remote
  * %%
- * Copyright (C) 2014 - 2016 openbase.org
+ * Copyright (C) 2014 - 2017 openbase.org
  * %%
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
