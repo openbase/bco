3a1b0745593c531212ff1ae7de3a988c0de0c190
Marian Pohling <mpohling@cit-ec.uni-bielefeld.de>
2017-02-13 11:54:18 +0100

    mvn logging reduced.

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,1 +1,1 @@
-mvn clean deploy -Pdeploy,sonatype --settings .travis/settings.xml -DskipTests=true -B -U
+mvn clean deploy -Pdeploy,sonatype --settings .travis/settings.xml -DskipTests=true -B -U -q
