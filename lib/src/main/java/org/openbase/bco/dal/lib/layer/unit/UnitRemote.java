b890418e31f2c199203a000b0e9ea3ff062f89fb
Marian Pohling <mpohling@cit-ec.uni-bielefeld.de>
2017-02-27 18:19:39 +0100

    done todo removed

--- lib/src/main/java/org/openbase/bco/dal/lib/layer/unit/UnitRemote.java
+++ lib/src/main/java/org/openbase/bco/dal/lib/layer/unit/UnitRemote.java
@@ -33,7 +33,6 @@ import rst.domotic.unit.UnitConfigType.UnitConfig;
 import rst.rsb.ScopeType;
 
 /**
- * TODO Release: remove unused parameter CONFIG
  *
  * @author <a href="mailto:divine@openbase.org">Divine Threepwood</a>
  * @param <M> Message
