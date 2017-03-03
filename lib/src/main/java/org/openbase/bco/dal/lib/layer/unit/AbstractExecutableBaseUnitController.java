7b8af83a50ab7fcb86719f090e94b6be188768f7
Tamino Huxohl <thuxohl@techfak.uni-bielefeld.de>
2017-03-03 13:40:22 +0100

    fixed two exception messages in AbstractExecutableBaseUnitController

--- lib/src/main/java/org/openbase/bco/dal/lib/layer/unit/AbstractExecutableBaseUnitController.java
+++ lib/src/main/java/org/openbase/bco/dal/lib/layer/unit/AbstractExecutableBaseUnitController.java
@@ -10,12 +10,12 @@ package org.openbase.bco.dal.lib.layer.unit;
  * it under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Lesser Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Lesser Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/lgpl-3.0.html>.
@@ -138,7 +138,7 @@ public abstract class AbstractExecutableBaseUnitController<D extends GeneratedMe
                 }
             }
         } catch (ExecutionException ex) {
-            throw new CouldNotPerformException("Could not diable " + this, ex);
+            throw new CouldNotPerformException("Could not enable " + this, ex);
         }
     }
 
@@ -151,7 +151,7 @@ public abstract class AbstractExecutableBaseUnitController<D extends GeneratedMe
                 deactivate();
             }
         } catch (ExecutionException ex) {
-            throw new CouldNotPerformException("Could not diable " + this, ex);
+            throw new CouldNotPerformException("Could not disable " + this, ex);
         }
     }
 
