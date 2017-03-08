61e0025b6fb5e629a9d63fd66a98fbe43a70aeb7
Tamino Huxohl <thuxohl@techfak.uni-bielefeld.de>
2017-03-08 13:15:54 +0100

    added a test for setNeutralWhite of colorableLights

--- device-manager/test/src/test/java/org/openbase/bco/manager/device/test/remote/unit/ColorableLightRemoteTest.java
+++ device-manager/test/src/test/java/org/openbase/bco/manager/device/test/remote/unit/ColorableLightRemoteTest.java
@@ -10,12 +10,12 @@ package org.openbase.bco.manager.device.test.remote.unit;
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/gpl-3.0.html>.
@@ -31,6 +31,7 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openbase.bco.dal.lib.jp.JPHardwareSimulationMode;
+import org.openbase.bco.dal.lib.layer.service.operation.ColorStateOperationService;
 import org.openbase.bco.dal.lib.transform.HSBColorToRGBColorTransformer;
 import org.openbase.bco.dal.remote.unit.ColorableLightRemote;
 import org.openbase.bco.dal.remote.unit.LightRemote;
@@ -253,5 +254,13 @@ public class ColorableLightRemoteTest {
         colorableLightRemote.setBrightnessState(brightnessState).get();
         colorableLightRemote.requestData().get();
         assertEquals("Brightness has not been set in time or the return value from the getter is different!", brightnessState, colorableLightRemote.getBrightnessState());
+    }
+
+    @Test(timeout = 10000)
+    public void testSetNeutralWhite() throws Exception {
+        System.out.println("testSetNeutralWhite");
+        colorableLightRemote.setNeutralWhite().get();
+        colorableLightRemote.requestData().get();
+        assertEquals("Neutral white was not set to the default value!", ColorStateOperationService.DEFAULT_NEUTRAL_WHITE, colorableLightRemote.getColorState().getColor().getRgbColor());
     }
 }
