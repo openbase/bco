797edd27b2665419f580290419d29ff418b2c4bd
Marian Pohling <mpohling@cit-ec.uni-bielefeld.de>
2017-05-04 18:56:35 +0200

    used getUnitTransformation from LocationRegistryRemote.

--- remote/src/main/java/org/openbase/bco/dal/remote/unit/Units.java
+++ remote/src/main/java/org/openbase/bco/dal/remote/unit/Units.java
@@ -54,9 +54,9 @@ import rst.domotic.unit.UnitTemplateType.UnitTemplate.UnitType;
 import rst.rsb.ScopeType;
 import org.openbase.bco.dal.lib.layer.unit.UnitRemote;
 import org.openbase.bco.registry.remote.Registries;
-import org.openbase.jul.extension.rct.GlobalTransformReceiver;
 import org.openbase.jul.schedule.GlobalCachedExecutorService;
 import rct.Transform;
+import rst.domotic.registry.LocationRegistryDataType;
 
 /**
  * @author <a href="mailto:divine@openbase.org">Divine Threepwood</a>
@@ -877,12 +877,13 @@ public class Units {
      * @throws InterruptedException is thrown if the thread was externally interrupted.
      */
     public static Future<Transform> getUnitTransformation(final UnitConfig unitConfig) throws NotAvailableException, InterruptedException {
+        final Future<LocationRegistryDataType.LocationRegistryData> dataFuture;
         try {
-            Registries.getLocationRegistry().waitForData();
-            return getUnitTransformation(Registries.getLocationRegistry().getRootLocationConfig(), unitConfig);
+            dataFuture = Registries.getLocationRegistry().getDataFuture();
         } catch (CouldNotPerformException ex) {
             throw new NotAvailableException("UnitTransformation", ex);
         }
+        return GlobalCachedExecutorService.allOfInclusiveResultFuture(Registries.getLocationRegistry().getUnitTransformation(unitConfig), dataFuture);
     }
 
     /**
@@ -895,14 +896,12 @@ public class Units {
      * @throws InterruptedException is thrown if the thread was externally interrupted.
      */
     public static Future<Transform> getUnitTransformation(final UnitConfig unitConfigA, final UnitConfig unitConfigB) throws NotAvailableException, InterruptedException {
-        try {   
-            Future<Transform> transformationFuture = GlobalTransformReceiver.getInstance().requestTransform(
-                    unitConfigA.getPlacementConfig().getTransformationFrameId(),
-                    unitConfigB.getPlacementConfig().getTransformationFrameId(),
-                    System.currentTimeMillis());
-            return GlobalCachedExecutorService.allOfInclusiveResultFuture(transformationFuture, Registries.getLocationRegistry().getDataFuture());
+        final Future<LocationRegistryDataType.LocationRegistryData> dataFuture;
+        try {
+            dataFuture = Registries.getLocationRegistry().getDataFuture();
         } catch (CouldNotPerformException ex) {
             throw new NotAvailableException("UnitTransformation", ex);
         }
+        return GlobalCachedExecutorService.allOfInclusiveResultFuture(Registries.getLocationRegistry().getUnitTransformation(unitConfigA, unitConfigB), dataFuture);
     }
 }
