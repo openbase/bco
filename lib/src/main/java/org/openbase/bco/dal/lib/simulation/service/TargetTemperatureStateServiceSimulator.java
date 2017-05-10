3aa8de779426e98054499f2cbc586498c3aba14b
Tamino Huxohl <thuxohl@techfak.uni-bielefeld.de>
2017-05-10 11:50:55 +0200

    add some missing service simulator

---
+++ lib/src/main/java/org/openbase/bco/dal/lib/simulation/service/TargetTemperatureStateServiceSimulator.java
@@ -0,0 +1,14 @@
+package org.openbase.bco.dal.lib.simulation.service;
+
+import org.openbase.bco.dal.lib.layer.unit.UnitController;
+
+/**
+ *
+ * @author <a href="mailto:pleminoq@openbase.org">Tamino Huxohl</a>
+ */
+public class TargetTemperatureStateServiceSimulator extends TemperatureStateServiceSimulator {
+
+    public TargetTemperatureStateServiceSimulator(UnitController unitController) {
+        super(unitController);
+    }
+}
