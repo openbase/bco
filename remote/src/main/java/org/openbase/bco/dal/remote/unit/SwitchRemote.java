16f5f9bab8bd0671b5ed8ff936217cffb97359d5
Marian Pohling <mpohling@cit-ec.uni-bielefeld.de>
2017-05-24 16:02:33 +0200

    Switch Unit classes implemented.

---
+++ remote/src/main/java/org/openbase/bco/dal/remote/unit/SwitchRemote.java
@@ -0,0 +1,59 @@
+package org.openbase.bco.dal.remote.unit;
+
+/*
+ * #%L
+ * BCO DAL Remote
+ * %%
+ * Copyright (C) 2014 - 2017 openbase.org
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Lesser Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-3.0.html>.
+ * #L%
+ */
+import org.openbase.jul.exception.CouldNotPerformException;
+import org.openbase.jul.exception.NotAvailableException;
+import rsb.converter.DefaultConverterRepository;
+import rsb.converter.ProtocolBufferConverter;
+import rst.domotic.state.SwitchStateType.SwitchState;
+import rst.domotic.unit.dal.SwitchDataType.SwitchData;
+import org.openbase.bco.dal.lib.layer.unit.Switch;
+
+/**
+ *
+ * * @author <a href="mailto:pleminoq@openbase.org">Tamino Huxohl</a>
+ */
+public class SwitchRemote extends AbstractUnitRemote<SwitchData> implements Switch {
+
+    static {
+        DefaultConverterRepository.getDefaultConverterRepository().addConverter(new ProtocolBufferConverter<>(SwitchData.getDefaultInstance()));
+        DefaultConverterRepository.getDefaultConverterRepository().addConverter(new ProtocolBufferConverter<>(SwitchState.getDefaultInstance()));
+    }
+
+    public SwitchRemote() {
+        super(SwitchData.class);
+    }
+
+    @Override
+    public void notifyDataUpdate(SwitchData data) {
+    }
+
+    @Override
+    public SwitchState getSwitchState() throws NotAvailableException {
+        try {
+            return getData().getSwitchState();
+        } catch (CouldNotPerformException ex) {
+            throw new NotAvailableException("SwitchState", ex);
+        }
+    }
+}
