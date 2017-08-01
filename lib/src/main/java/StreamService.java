36c8b2f4129af10eb19a5f83ee42caa3cf94ddb8
Marian Pohling <mpohling@cit-ec.uni-bielefeld.de>
2017-08-01 07:58:15 -0600

    Consumer implementation started.

---
+++ lib/src/main/java/StreamService.java
@@ -0,0 +1,40 @@
+/*-
+ * #%L
+ * BCO DAL Library
+ * %%
+ * Copyright (C) 2014 - 2017 openbase.org
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Lesser Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-3.0.html>.
+ * #L%
+ */
+
+import java.util.concurrent.Future;
+import org.openbase.bco.dal.lib.layer.service.Service;
+import org.openbase.jul.exception.CouldNotPerformException;
+import org.openbase.jul.exception.NotSupportedException;
+import rst.domotic.action.ActionDescriptionType;
+import rst.domotic.action.ActionFutureType;
+
+
+/**
+ * @author <a href="mailto:divine@openbase.org">Divine Threepwood</a>
+ */
+public interface StreamService extends Service {
+
+    @Override
+    public default Future<ActionFutureType.ActionFuture> applyAction(ActionDescriptionType.ActionDescription actionConfig) throws CouldNotPerformException, InterruptedException {
+        throw new NotSupportedException("actions", StreamService.class);
+    }
+}
