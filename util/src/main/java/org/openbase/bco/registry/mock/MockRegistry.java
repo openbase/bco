73b6d6a468e3c7c43da3b36a4794e88b5f0d00a1
Tamino Huxohl <thuxohl@techfak.uni-bielefeld.de>
2017-09-01 11:01:20 +0200

    Removed setting password of a user in the mock registry

--- util/src/main/java/org/openbase/bco/registry/mock/MockRegistry.java
+++ util/src/main/java/org/openbase/bco/registry/mock/MockRegistry.java
@@ -21,7 +21,6 @@ package org.openbase.bco.registry.mock;
  * <http://www.gnu.org/licenses/gpl-3.0.html>.
  * #L%
  */
-import com.google.protobuf.ByteString;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -603,8 +602,7 @@ public class MockRegistry {
     }
 
     private void registerUser() throws CouldNotPerformException, InterruptedException {
-        byte[] password = Arrays.copyOf("12345678Password1234".getBytes(), 16);
-        UserConfig.Builder config = UserConfig.newBuilder().setFirstName("Max").setLastName("Mustermann").setUserName(USER_NAME).setPassword(ByteString.copyFrom(password));
+        UserConfig.Builder config = UserConfig.newBuilder().setFirstName("Max").setLastName("Mustermann").setUserName(USER_NAME);
         UnitConfig userUnitConfig = UnitConfig.newBuilder().setType(UnitType.USER).setUserConfig(config).setEnablingState(EnablingState.newBuilder().setValue(EnablingState.State.ENABLED)).build();
         try {
             testUser = userRegisty.registerUserConfig(userUnitConfig).get();
