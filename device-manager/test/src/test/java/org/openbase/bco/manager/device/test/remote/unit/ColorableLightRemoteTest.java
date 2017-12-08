d346b4782124a1c1e6a76e4158c1ffa7af9b38df
Tamino Huxohl <thuxohl@techfak.uni-bielefeld.de>
2017-12-08 17:48:40 +0100

    Reenabled PowerStateObserver

--- device-manager/test/src/test/java/org/openbase/bco/manager/device/test/remote/unit/ColorableLightRemoteTest.java
+++ device-manager/test/src/test/java/org/openbase/bco/manager/device/test/remote/unit/ColorableLightRemoteTest.java
@@ -273,7 +273,7 @@ public class ColorableLightRemoteTest extends AbstractBCODeviceManagerTest {
 
     private int powerStateObserverUpdateNumber = 0;
 
-//    @Test(timeout = 10000)
+    @Test(timeout = 10000)
     public void testPowerStateObserver() throws Exception {
         System.out.println("testPowerStateObserver");
 
