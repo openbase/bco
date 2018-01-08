a49be00ffdcdb1db97383a0f02d7eed762225b6e
Divine Threepwood <divine@openbase.org>
2018-01-08 14:25:12 +0100

    travis config added.

---
+++ .travis/deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -ev
+#echo ### start deployment...
+#mvn deploy -Pdeploy,sonatype --settings .travis/settings.xml -DskipTests=true -B
+#echo ### deployment successfully finished
+echo ### No deployment needed for this project
