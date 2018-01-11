2ca6a21539921b39aaa8b8396bda79f3920c5cb2
Marian Pohling <mpohling@cit-ec.uni-bielefeld.de>
2018-01-11 18:27:11 +0100

    branch name detection implemented.

--- workspace-update.sh
+++ workspace-update.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
+
+# update top level project
+git pull
+
+# make sure submodule projects are initiated and updated
 git $@ submodule init
 git $@ submodule update
 
+# detect project branch
+BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
+
 # switch to bco master versions
-git submodule foreach git checkout master
+git submodule foreach git checkout ${BRANCH_NAME}
 git submodule foreach git pull
 
