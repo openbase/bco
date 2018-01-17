74471bd61ab52b668b6f0acec01faa709dbfaccf
Tamino Huxohl <thuxohl@techfak.uni-bielefeld.de>
2018-01-17 18:36:54 +0100

    EXPERIMENTEL: fixed error in reset of Units

--- remote/src/main/java/org/openbase/bco/dal/remote/unit/Units.java
+++ remote/src/main/java/org/openbase/bco/dal/remote/unit/Units.java
@@ -10,12 +10,12 @@ package org.openbase.bco.dal.remote.unit;
  * it under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- *
+ * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Lesser Public License for more details.
- *
+ * 
  * You should have received a copy of the GNU General Lesser Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/lgpl-3.0.html>.
@@ -291,7 +291,7 @@ public class Units {
 
             UNIT_REMOTE_REGISTRY_LOCK.writeLock().lock();
             try {
-                unitRemoteRegistry.getEntries().stream().parallel().forEach(((org.openbase.bco.dal.lib.layer.unit.UnitRemote unitRemote) -> {
+                for (UnitRemote unitRemote : new ArrayList<>(unitRemoteRegistry.getEntries())) {
                     try {
                         unitRemoteRegistry.remove(unitRemote);
                         unitRemote.unlock(unitRemoteRegistry);
@@ -299,7 +299,7 @@ public class Units {
                     } catch (CouldNotPerformException ex) {
                         ExceptionPrinter.printHistory("Could not properly shutdown " + unitRemote, ex, LOGGER);
                     }
-                }));
+                }
             } finally {
                 UNIT_REMOTE_REGISTRY_LOCK.writeLock().unlock();
             }
