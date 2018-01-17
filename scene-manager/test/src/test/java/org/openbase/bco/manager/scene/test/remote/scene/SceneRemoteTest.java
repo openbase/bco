97814e9eb215acdd4e15c8e96523b88277f6c411
Tamino Huxohl <thuxohl@techfak.uni-bielefeld.de>
2018-01-17 17:24:19 +0100

    add debug log for a problem in SceneRemoteTest

--- scene-manager/test/src/test/java/org/openbase/bco/manager/scene/test/remote/scene/SceneRemoteTest.java
+++ scene-manager/test/src/test/java/org/openbase/bco/manager/scene/test/remote/scene/SceneRemoteTest.java
@@ -10,12 +10,12 @@ package org.openbase.bco.manager.scene.test.remote.scene;
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/gpl-3.0.html>.
@@ -221,7 +221,10 @@ public class SceneRemoteTest {
             unitRegistry.registerUnitConfig(unitConfig).get();
 
             label = SCENE_ROOT_LOCATION_ALL_DEVICES_ON;
-            LocationRemote locationRemote = Units.getUnit(Registries.getLocationRegistry().getRootLocationConfig(), true, LocationRemote.class);
+            LocationRemote locationRemote = Units.getUnit(Registries.getLocationRegistry().getRootLocationConfig(), false, LocationRemote.class);
+            locationRemote.addConnectionStateObserver((source, data) -> System.out.println("ConnectionState update of root location remote[" + data + "] isActive[" + locationRemote.isActive() + "]"));
+            System.out.println("LocationRemote connectionState[" + locationRemote.getConnectionState().name() + "] isActive[" + locationRemote.isActive() + "]");
+            locationRemote.waitForData();
             locationRemote.setPowerState(POWER_ON).get();
             locationRemote.requestData().get();
             sceneConfig = SceneConfig.newBuilder().addAllRequiredServiceStateDescription(locationRemote.recordSnapshot().get().getServiceStateDescriptionList()).build();
