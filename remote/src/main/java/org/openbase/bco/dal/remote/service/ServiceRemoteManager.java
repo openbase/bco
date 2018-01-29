1d60578c80f142338bd8391a5a26e80422788f5c
Marian Pohling <mpohling@cit-ec.uni-bielefeld.de>
2018-01-29 17:59:21 +0100

    synchronize service remote activation

--- remote/src/main/java/org/openbase/bco/dal/remote/service/ServiceRemoteManager.java
+++ remote/src/main/java/org/openbase/bco/dal/remote/service/ServiceRemoteManager.java
@@ -10,12 +10,12 @@ package org.openbase.bco.dal.remote.service;
  * it under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Lesser Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Lesser Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/lgpl-3.0.html>.
@@ -143,9 +143,11 @@ public abstract class ServiceRemoteManager<D> implements Activatable, Snapshotab
                 serviceRemoteMap.put(serviceType, serviceRemote);
 
                 // if already active than update the current location state.
-                if (isActive()) {
-                    serviceRemote.addDataObserver(serviceDataObserver);
-                    serviceRemote.activate();
+                synchronized (serviceRemoteMapLock) {
+                    if (isActive()) {
+                        serviceRemote.addDataObserver(serviceDataObserver);
+                        serviceRemote.activate();
+                    }
                 }
             }
         }
