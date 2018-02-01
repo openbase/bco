b7105a6e5382be9f55d211f152d5cff6c0a23e0f
Divine Threepwood <divine@openbase.org>
2018-02-01 17:56:13 +0100

    unit alias feature implementation finished

--- unit-registry/core/src/main/java/org/openbase/bco/registry/unit/core/consistency/UnitAliasUniqueVerificationConsistencyHandler.java
+++ unit-registry/core/src/main/java/org/openbase/bco/registry/unit/core/consistency/UnitAliasUniqueVerificationConsistencyHandler.java
@@ -1,10 +1,29 @@
 package org.openbase.bco.registry.unit.core.consistency;
 
+/*-
+ * #%L
+ * BCO Registry Unit Core
+ * %%
+ * Copyright (C) 2014 - 2018 openbase.org
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+
 import org.openbase.bco.registry.unit.lib.UnitRegistry;
-import org.openbase.jul.exception.CouldNotPerformException;
-import org.openbase.jul.exception.InitializationException;
-import org.openbase.jul.exception.InvalidStateException;
-import org.openbase.jul.exception.RejectedException;
+import org.openbase.jul.exception.*;
 import org.openbase.jul.extension.protobuf.IdentifiableMessage;
 import org.openbase.jul.extension.protobuf.container.ProtoBufMessageMap;
 import org.openbase.jul.extension.rsb.scope.ScopeGenerator;
@@ -34,13 +53,13 @@ public class UnitAliasUniqueVerificationConsistencyHandler extends AbstractProto
     public void processData(String id, IdentifiableMessage<String, UnitConfig, Builder> entry, ProtoBufMessageMap<String, UnitConfig, Builder> entryMap, ProtoBufRegistry<String, UnitConfig, Builder> registry) throws CouldNotPerformException, EntryModification {
         final UnitConfig.Builder unitConfig = entry.getMessage().toBuilder();
 
-        for(String alias : unitConfig.getAliasList()) {
-            if(!aliasUnitIdMap.containsKey(alias.toLowerCase())) {
-                aliasUnitIdMap.put(alias, unitConfig.getId());
+        for (final String alias : unitConfig.getAliasList()) {
+            if (!aliasUnitIdMap.containsKey(alias.toLowerCase())) {
+                aliasUnitIdMap.put(alias.toLowerCase(), unitConfig.getId());
             } else {
                 // if already known check if this unit is owning the alias otherwise throw invalid state
-                if(!aliasUnitIdMap.get(alias).equals(unitConfig.getId())) {
-                    throw new RejectedException("Alias[" + alias.toLowerCase() + "] of Unit[" + ScopeGenerator.generateStringRep(unitConfig.getScope()) + ", " + unitConfig.getId() + "] is already used by Uit[" + aliasUnitIdMap.get(alias.toLowerCase()) + "]");
+                if (!aliasUnitIdMap.get(alias.toLowerCase()).equals(unitConfig.getId())) {
+                    throw new RejectedException("Alias[" + alias.toLowerCase() + "] of Unit[" + ScopeGenerator.generateStringRep(unitConfig.getScope()) + ", " + unitConfig.getId() + "] is already used by Unit[" + aliasUnitIdMap.get(alias.toLowerCase()) + "]");
                 }
             }
         }
@@ -155,16 +174,23 @@ public class UnitAliasUniqueVerificationConsistencyHandler extends AbstractProto
     public void reset() {
 
         // validate known aliases
-        for(String alias : new ArrayList<>(aliasUnitIdMap.keySet())) {
+        for (String alias : new ArrayList<>(aliasUnitIdMap.keySet())) {
             try {
                 // remove alias entry if alias is globally unknown.
-                if(!unitRegistry.containsUnitConfigByAlias(alias)) {
+                if (!unitRegistry.containsUnitConfigByAlias(alias)) {
+                    logger.error("remove alias: " + alias);
                     aliasUnitIdMap.remove(alias);
                 }
-            } catch(final CouldNotPerformException ex) {
+            } catch (final CouldNotPerformException ex) {
                 logger.debug("Could not validate alias!", ex);
             }
         }
         super.reset();
+    }
+
+    @Override
+    public void shutdown() {
+        aliasUnitIdMap.clear();
+        // super call is not performed because those would only call reset() which fails because the unit registry is not responding during shutdown.
     }
 }
