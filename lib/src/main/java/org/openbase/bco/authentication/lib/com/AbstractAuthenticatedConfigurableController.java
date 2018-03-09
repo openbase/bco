b6daa2c342e69b30bd13982f813372512a7c8ccb
Tamino Huxohl <thuxohl@techfak.uni-bielefeld.de>
2018-03-09 14:18:44 +0100

    add authenticated configurable controller and remote

--- lib/src/main/java/org/openbase/bco/authentication/lib/com/AbstractAuthenticatedConfigurableController.java
+++ lib/src/main/java/org/openbase/bco/authentication/lib/com/AbstractAuthenticatedConfigurableController.java
@@ -1,27 +1,5 @@
 package org.openbase.bco.authentication.lib.com;
 
-/*-
- * #%L
- * BCO Authentication Library
- * %%
- * Copyright (C) 2017 - 2018 openbase.org
- * %%
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Lesser Public License for more details.
- *
- * You should have received a copy of the GNU General Lesser Public
- * License along with this program.  If not, see
- * <http://www.gnu.org/licenses/lgpl-3.0.html>.
- * #L%
- */
-
 import com.google.protobuf.GeneratedMessage;
 import org.openbase.bco.authentication.lib.AuthenticatedServerManager;
 import org.openbase.bco.authentication.lib.EncryptionHelper;
@@ -31,8 +9,8 @@ import org.openbase.jul.exception.InitializationException;
 import org.openbase.jul.exception.InstantiationException;
 import org.openbase.jul.exception.printer.ExceptionPrinter;
 import org.openbase.jul.exception.printer.LogLevel;
+import org.openbase.jul.extension.rsb.com.AbstractConfigurableController;
 import org.openbase.jul.extension.rsb.com.RPCHelper;
-import org.openbase.jul.extension.rsb.com.RSBCommunicationService;
 import rsb.config.ParticipantConfig;
 import rst.domotic.authentication.AuthenticatedValueType.AuthenticatedValue;
 import rst.domotic.authentication.TicketAuthenticatorWrapperType.TicketAuthenticatorWrapper;
@@ -40,16 +18,9 @@ import rst.rsb.ScopeType.Scope;
 
 import java.io.IOException;
 
+public abstract class AbstractAuthenticatedConfigurableController<M extends GeneratedMessage, MB extends M.Builder<MB>, CONFIG extends GeneratedMessage> extends AbstractConfigurableController<M, MB, CONFIG> implements AuthenticatedRequestable<M> {
 
-public abstract class AbstractAuthenticatedCommunicationService<M extends GeneratedMessage, MB extends M.Builder<MB>> extends RSBCommunicationService<M, MB> implements AuthenticatedRequestable<M> {
-
-    /**
-     * Create a communication service.
-     *
-     * @param builder the initial data builder
-     * @throws InstantiationException if the creation fails
-     */
-    public AbstractAuthenticatedCommunicationService(final MB builder) throws InstantiationException {
+    public AbstractAuthenticatedConfigurableController(MB builder) throws InstantiationException {
         super(builder);
     }
 
--- lib/src/main/java/org/openbase/bco/authentication/lib/com/AbstractAuthenticatedConfigurableRemote.java
+++ lib/src/main/java/org/openbase/bco/authentication/lib/com/AbstractAuthenticatedConfigurableRemote.java
@@ -1,33 +1,11 @@
 package org.openbase.bco.authentication.lib.com;
 
-/*-
- * #%L
- * BCO Authentication Library
- * %%
- * Copyright (C) 2017 - 2018 openbase.org
- * %%
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Lesser Public License for more details.
- * 
- * You should have received a copy of the GNU General Lesser Public
- * License along with this program.  If not, see
- * <http://www.gnu.org/licenses/lgpl-3.0.html>.
- * #L%
- */
-
 import com.google.protobuf.GeneratedMessage;
 import org.openbase.bco.authentication.lib.SessionManager;
 import org.openbase.bco.authentication.lib.iface.AuthenticatedRequestable;
 import org.openbase.jul.exception.CouldNotPerformException;
 import org.openbase.jul.exception.printer.ExceptionPrinter;
-import org.openbase.jul.extension.rsb.com.RSBRemoteService;
+import org.openbase.jul.extension.rsb.com.AbstractConfigurableRemote;
 import org.openbase.jul.pattern.Observer;
 import rsb.Event;
 import rsb.Handler;
@@ -35,12 +13,12 @@ import rst.domotic.authentication.TicketAuthenticatorWrapperType.TicketAuthentic
 
 import java.util.concurrent.Future;
 
-public abstract class AbstractAuthenticatedRemoteService<M extends GeneratedMessage> extends RSBRemoteService<M> {
+public class AbstractAuthenticatedConfigurableRemote<M extends GeneratedMessage, CONFIG extends GeneratedMessage> extends AbstractConfigurableRemote<M, CONFIG> {
 
     private final Observer<String> loginObserver;
 
-    public AbstractAuthenticatedRemoteService(Class<M> dataClass) {
-        super(dataClass);
+    public AbstractAuthenticatedConfigurableRemote(final Class<M> dataClass, final Class<CONFIG> configClass) {
+        super(dataClass, configClass);
         this.setMessageProcessor(new AuthenticatedMessageProcessor<>(dataClass));
 
         this.loginObserver = (source, data) -> requestData();
