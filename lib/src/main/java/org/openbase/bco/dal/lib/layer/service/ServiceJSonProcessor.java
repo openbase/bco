557872fec88b4413a0a36ea15eefd3cae5ffab61
Tamino Huxohl <thuxohl@techfak.uni-bielefeld.de>
2018-03-21 15:35:15 +0100

    ServiceJsonProcessor extends ProtoBufJSonProcessor and adds method to filter fields

--- lib/src/main/java/org/openbase/bco/dal/lib/layer/service/ServiceJSonProcessor.java
+++ lib/src/main/java/org/openbase/bco/dal/lib/layer/service/ServiceJSonProcessor.java
@@ -10,204 +10,75 @@ package org.openbase.bco.dal.lib.layer.service;
  * it under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Lesser Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Lesser Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/lgpl-3.0.html>.
  * #L%
  */
-import com.google.protobuf.Descriptors;
+
+import com.google.protobuf.Descriptors.FieldDescriptor;
 import com.google.protobuf.Message;
-import com.googlecode.protobuf.format.JsonFormat;
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
-import java.nio.charset.Charset;
 import org.openbase.jul.exception.CouldNotPerformException;
-import org.openbase.jul.exception.InvalidStateException;
-import org.openbase.jul.exception.NotSupportedException;
-import org.openbase.jul.processing.StringProcessor;
-import org.slf4j.LoggerFactory;
+import org.openbase.jul.extension.protobuf.processing.ProtoBufFieldProcessor;
+import org.openbase.jul.extension.protobuf.processing.ProtoBufJSonProcessor;
+import org.openbase.jul.extension.rst.processing.TimestampProcessor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
- *
  * @author <a href="mailto:divine@openbase.org">Divine Threepwood</a>
  */
-public class ServiceJSonProcessor {
-
-    private static final String UTF8 = "UTF8";
-    private static final String EMPTY_MESSAGE = "{}";
-    private static final org.slf4j.Logger logger = LoggerFactory.getLogger(ServiceJSonProcessor.class);
-    private static final String javaPrimitvePrefix = "java.lang.";
-    private final JsonFormat jsonFormat;
-
-    /**
-     * Enumeration that maps from java primitives to proto field descriptor
-     * types.
-     */
-    public enum JavaTypeToProto {
-
-        BOOLEAN(Descriptors.FieldDescriptor.Type.BOOL),
-        INTEGER(Descriptors.FieldDescriptor.Type.INT32),
-        FLOAT(Descriptors.FieldDescriptor.Type.FLOAT),
-        DOUBLE(Descriptors.FieldDescriptor.Type.DOUBLE),
-        LONG(Descriptors.FieldDescriptor.Type.INT64),
-        STRING(Descriptors.FieldDescriptor.Type.STRING),
-        ENUM(Descriptors.FieldDescriptor.Type.ENUM);
-
-        private final Descriptors.FieldDescriptor.Type protoType;
-
-        private JavaTypeToProto(final Descriptors.FieldDescriptor.Type protoType) {
-            this.protoType = protoType;
-        }
-
-        public Descriptors.FieldDescriptor.Type getProtoType() {
-            return protoType;
-        }
-    }
+public class ServiceJSonProcessor extends ProtoBufJSonProcessor {
 
     public ServiceJSonProcessor() {
-        this.jsonFormat = new JsonFormat();
+        super();
     }
 
     /**
-     * Serialize a serviceAttribute which can be a proto message, enumeration or
-     * a java primitive to string. If its a primitive toString is called while
-     * messages or enumerations will be serialized into JSon
+     * Serialize a serviceAttribute which is a protoBuf message.
+     * This method removes timestamp and responsible action fields.
      *
-     * @param serviceAttribute
-     * @return
-     * @throws org.openbase.jul.exception.InvalidStateException in case the given service argument does not contain any context to serialize.
-     * @throws CouldNotPerformException in case the serialization failed.
+     * @param serviceAttribute the service attribute which is serialized
+     * @return a jSon string representation of the service attribute
+     * @throws CouldNotPerformException if the serialization fails or the service attribute does not contain any context
+     *                                  <p>
+     *                                  TODO: release: change parameter type to message since java primitives cannot be de-/serialized anymore anyway
      */
-    public String serialize(final Object serviceAttribute) throws InvalidStateException, CouldNotPerformException {
-        String jsonStringRep;
-        if (serviceAttribute instanceof Message) {
-            try {
-                jsonStringRep = jsonFormat.printToString((Message) serviceAttribute);
-            } catch (Exception ex) {
-                throw new CouldNotPerformException("Could not serialize service argument to string!", ex);
+    @Override
+    public String serialize(final Object serviceAttribute) throws CouldNotPerformException {
+        final List<String> filteredFieldList = new ArrayList<>();
+        filteredFieldList.add(TimestampProcessor.TIMESTEMP_FIELD.toLowerCase());
+        filteredFieldList.add(Service.RESPONSIBLE_ACTION_FIELD_NAME);
+        return serialize((Message) serviceAttribute, filteredFieldList);
+    }
+
+    /**
+     * Serialize a serviceAttribute and filter a list of fields beforehand.
+     *
+     * @param serviceAttribute  the service attribute which is serialized
+     * @param filteredFieldList a list of field names which shall be filtered before serialization
+     * @return a jSon string representation of the service attribute without the filtered fields
+     * @throws CouldNotPerformException if the serialization fails or the service attribute does not contain any context
+     */
+    public String serialize(final Message serviceAttribute, final List<String> filteredFieldList) throws CouldNotPerformException {
+        final Message.Builder builder = serviceAttribute.toBuilder();
+
+        for (String filteredField : filteredFieldList) {
+            FieldDescriptor fieldDescriptor = ProtoBufFieldProcessor.getFieldDescriptor(builder, filteredField);
+            if (!builder.hasField(fieldDescriptor)) {
+                continue;
             }
-        } else {
-            jsonStringRep = serviceAttribute.toString();
+
+            builder.clearField(fieldDescriptor);
         }
 
-        if (jsonStringRep.isEmpty() || jsonStringRep.equals(EMPTY_MESSAGE)) {
-            throw new InvalidStateException("Service attribute does not contain any context!");
-        }
-
-        return jsonStringRep;
-    }
-
-    /**
-     * Get the string representation for a given serviceAttribute which can be a
-     * proto message, enumeration or a java primitive.
-     *
-     * @param serviceAttribute the serviceAttribute
-     * @return a string representation of the serviceAttribute type
-     * @throws CouldNotPerformException
-     */
-    public String getServiceAttributeType(final Object serviceAttribute) throws CouldNotPerformException {
-        if (serviceAttribute.getClass().getName().startsWith("rst")) {
-            return serviceAttribute.getClass().getName();
-        }
-
-        if (serviceAttribute.getClass().isEnum()) {
-            logger.info(serviceAttribute.getClass().getName());
-            return serviceAttribute.getClass().getName();
-        }
-
-        logger.debug("Simple class name of attribute to upper case [" + serviceAttribute.getClass().getSimpleName().toUpperCase() + "]");
-        JavaTypeToProto javaToProto;
-        try {
-            javaToProto = JavaTypeToProto.valueOf(serviceAttribute.getClass().getSimpleName().toUpperCase());
-        } catch (IllegalArgumentException ex) {
-            throw new CouldNotPerformException("ServiceAttribute is not a supported java primitive nor a supported rst type", ex);
-        }
-        logger.debug("According proto type [" + javaToProto.getProtoType().name() + "]");
-        return javaToProto.getProtoType().name();
-    }
-
-    public <SAT> SAT deserialize(String jsonStringRep, Class<SAT> serviceAttributeTypeClass) throws CouldNotPerformException {
-        return (SAT) deserialize(jsonStringRep, serviceAttributeTypeClass.getSimpleName());
-    }
-
-    /**
-     * Deserialise a JSon string representation for an rst value given the class
-     * name for the value or the type if its a primitive.
-     *
-     * @param jsonStringRep the string representation of the rst value
-     * @param serviceAttributeType the class name or the type of the value
-     * @return the deserialized message
-     * @throws org.openbase.jul.exception.CouldNotPerformException
-     */
-    public Message deserialize(String jsonStringRep, String serviceAttributeType) throws CouldNotPerformException {
-        try {
-            if (serviceAttributeType.startsWith("rst")) {
-                try {
-                    Class attibuteClass = Class.forName(serviceAttributeType);
-                    if (attibuteClass.isEnum()) {
-                        throw new NotSupportedException(serviceAttributeType, this, "Service arguments must be a protobuf message!");
-                        //return attibuteClass.getMethod("valueOf", String.class).invoke(null, jsonStringRep);
-                    }
-                    Message.Builder builder = (Message.Builder) attibuteClass.getMethod("newBuilder").invoke(null);
-                    jsonFormat.merge(new ByteArrayInputStream(jsonStringRep.getBytes(Charset.forName(UTF8))), builder);
-                    return builder.build();
-                } catch (ClassNotFoundException ex) {
-                    throw new CouldNotPerformException("Could not find class for serviceAttributeType [" + serviceAttributeType + "]", ex);
-                } catch (IOException ex) {
-                    throw new CouldNotPerformException("Could not merge [" + jsonStringRep + "] into builder", ex);
-                } catch (NoSuchMethodException | SecurityException ex) {
-                    throw new CouldNotPerformException("Could not find or acces newBuilder method for rst type [" + serviceAttributeType + "]", ex);
-                } catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException ex) {
-                    throw new CouldNotPerformException("Could not invoke newBuilder method for rst type [" + serviceAttributeType + "]", ex);
-                }
-            } else {
-                throw new NotSupportedException(serviceAttributeType, this, "Service arguments must be a protobuf message!");
-//                try {
-//                    if (serviceAttributeType.split("\\.").length > 1) {
-//                        Class attibuteClass = Class.forName(serviceAttributeType);
-//                        if (attibuteClass.isEnum()) {
-//                            return attibuteClass.getMethod("valueOf", String.class).invoke(null, jsonStringRep);
-//                        }
-//                    }
-//                } catch (ClassNotFoundException ex) {
-//                    throw new CouldNotPerformException("Could not find class [" + serviceAttributeType + "]", ex);
-//                } catch (NoSuchMethodException ex) {
-//                    throw new CouldNotPerformException("Java primitive [" + serviceAttributeType + "] has no valueOf method", ex);
-//                } catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException ex) {
-//                    throw new CouldNotPerformException("Could not invoke valueOf method of class [" + serviceAttributeType + "] with [" + jsonStringRep + "] as the argument", ex);
-//                }
-//                String className = getJavaPrimitiveClassName(Descriptors.FieldDescriptor.Type.valueOf(serviceAttributeType));
-//                try {
-//                    Class attibuteClass = Class.forName(className);
-//                    // The simple types often offer a constructor by string
-//                    Constructor constructor = attibuteClass.getConstructor(String.class);
-//                    return constructor.newInstance(jsonStringRep);
-//                } catch (ClassNotFoundException ex) {
-//                    throw new CouldNotPerformException("Could not find class [" + className + "]", ex);
-//                } catch (NoSuchMethodException ex) {
-//                    throw new CouldNotPerformException("Java primitive [" + className + "] has no string constructor", ex);
-//                } catch (InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException ex) {
-//                    throw new CouldNotPerformException("Could not invoke constructor of class [" + className + "] with [" + jsonStringRep + "] as the argument", ex);
-//                }
-            }
-        } catch (CouldNotPerformException | NullPointerException ex) {
-            throw new CouldNotPerformException("Could not deserialize json String[" + jsonStringRep + "] into ServiceAttributeType[" + serviceAttributeType + "]!", ex);
-        }
-    }
-
-    public String getJavaPrimitiveClassName(Descriptors.FieldDescriptor.Type protoType) {
-        switch (protoType.getJavaType()) {
-            case INT:
-                return javaPrimitvePrefix + "Integer";
-            default:
-                return javaPrimitvePrefix + StringProcessor.transformUpperCaseToCamelCase(protoType.getJavaType().name());
-        }
+        return super.serialize(builder.build());
     }
 }
