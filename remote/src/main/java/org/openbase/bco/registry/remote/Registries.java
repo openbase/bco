c5c707892384296175455493dd6d76dd241a5c7d
Tamino Huxohl <thuxohl@techfak.uni-bielefeld.de>
2018-05-25 15:12:18 +0200

    fix appassembled registry names, move registry editor remote pool methods to registries

--- remote/src/main/java/org/openbase/bco/registry/remote/Registries.java
+++ remote/src/main/java/org/openbase/bco/registry/remote/Registries.java
@@ -22,6 +22,8 @@ package org.openbase.bco.registry.remote;
  * #L%
  */
 
+import com.google.protobuf.Message;
+import com.google.protobuf.MessageOrBuilder;
 import org.openbase.bco.registry.activity.remote.ActivityRegistryRemote;
 import org.openbase.bco.registry.activity.remote.CachedActivityRegistryRemote;
 import org.openbase.bco.registry.clazz.remote.CachedClassRegistryRemote;
@@ -30,12 +32,27 @@ import org.openbase.bco.registry.template.remote.CachedTemplateRegistryRemote;
 import org.openbase.bco.registry.template.remote.TemplateRegistryRemote;
 import org.openbase.bco.registry.unit.remote.CachedUnitRegistryRemote;
 import org.openbase.bco.registry.unit.remote.UnitRegistryRemote;
+import org.openbase.jul.annotation.Experimental;
 import org.openbase.jul.exception.CouldNotPerformException;
+import org.openbase.jul.exception.MultiException;
+import org.openbase.jul.exception.MultiException.ExceptionStack;
 import org.openbase.jul.exception.NotAvailableException;
+import org.openbase.jul.extension.rsb.com.RSBRemoteService;
 import org.openbase.jul.storage.registry.RegistryRemote;
+import rst.domotic.activity.ActivityConfigType.ActivityConfig;
+import rst.domotic.activity.ActivityTemplateType.ActivityTemplate;
+import rst.domotic.service.ServiceTemplateType.ServiceTemplate;
+import rst.domotic.unit.UnitConfigType.UnitConfig;
+import rst.domotic.unit.UnitTemplateType.UnitTemplate;
+import rst.domotic.unit.agent.AgentClassType.AgentClass;
+import rst.domotic.unit.app.AppClassType.AppClass;
+import rst.domotic.unit.device.DeviceClassType.DeviceClass;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Future;
 
 /**
  * @author <a href="mailto:divine@openbase.org">Divine Threepwood</a>
@@ -57,6 +74,21 @@ public class Registries {
         registryList.add(getActivityRegistry(waitForData));
         registryList.add(getUnitRegistry(waitForData));
         return registryList;
+    }
+
+    /**
+     * Returns a list of all available bco registries without waiting for data.
+     *
+     * @return a list of all available bco registries
+     * @throws CouldNotPerformException is throw if at least one registry is not available.
+     */
+    public static List<RegistryRemote> getRegistries() throws CouldNotPerformException {
+        try {
+            return getRegistries(false);
+        } catch (InterruptedException ex) {
+            Thread.currentThread().interrupt();
+            throw new CouldNotPerformException("Interrupted while creating registry list", ex);
+        }
     }
 
     /**
@@ -226,5 +258,237 @@ public class Registries {
         CachedClassRegistryRemote.waitUntilReady();
         CachedActivityRegistryRemote.waitUntilReady();
         CachedUnitRegistryRemote.waitUntilReady();
+    }
+
+    /**
+     * An array of all types of messages in registries.
+     * Can be used to iterate over all registries.
+     */
+    private static final Message[] MESSAGE_TYPES = {
+            UnitConfig.getDefaultInstance(),
+            DeviceClass.getDefaultInstance(),
+            AppClass.getDefaultInstance(),
+            AgentClass.getDefaultInstance(),
+            UnitTemplate.getDefaultInstance(),
+            ServiceTemplate.getDefaultInstance(),
+            ActivityTemplate.getDefaultInstance(),
+            ActivityConfig.getDefaultInstance()
+    };
+
+    /**
+     * Generic method to register a message in a registry.
+     * The given message will be used to select the correct registry.
+     *
+     * @param message the message that will be registered
+     * @param <M>     the type of message registered
+     * @return the registered message
+     * @throws CouldNotPerformException if registration fails or messages of this type cannot be registered
+     */
+    @Experimental
+    public static <M extends Message> Future<M> register(final M message) throws CouldNotPerformException {
+        return (Future<M>) invokeMethod("register", message);
+    }
+
+    /**
+     * Generic method to update a message in a registry.
+     * The given message will be used to select the correct registry.
+     *
+     * @param message the message that will be updated
+     * @param <M>     the type of message updates
+     * @return the updated message
+     * @throws CouldNotPerformException if updating fails or messages of this type cannot be updated
+     */
+    @Experimental
+    public static <M extends Message> Future<M> update(final M message) throws CouldNotPerformException {
+        return (Future<M>) invokeMethod("update", message);
+    }
+
+    /**
+     * Generic method to remove a message in a registry.
+     * The given message will be used to select the correct registry.
+     *
+     * @param message the message that will be removed
+     * @param <M>     the type of message removed
+     * @return the removed message
+     * @throws CouldNotPerformException if removing fails or messages of this type cannot be removed
+     */
+    @Experimental
+    public static <M extends Message> Future<M> remove(final M message) throws CouldNotPerformException {
+        return (Future<M>) invokeMethod("remove", message);
+    }
+
+    /**
+     * Test if a given message is contained in a registry.
+     * The given message will be used to select the correct registry.
+     *
+     * @param message the message that will be tested
+     * @return true if the message is contained in a registry, else false
+     * @throws CouldNotPerformException if it could not be checked for the message type or the check itself fails
+     */
+    @Experimental
+    public static Boolean contains(final Message message) throws CouldNotPerformException {
+        return (Boolean) invokeMethod("contains", message);
+    }
+
+    /**
+     * Test if a given message is contained in a registry.
+     *
+     * @param id the id which is checked
+     * @return true if the message is contained in a registry, false if it is not contained in any and no exception is thrown
+     * @throws CouldNotPerformException if the check fails for at least one registry and the id is not contained in any
+     */
+    @Experimental
+    public static Boolean containsById(final String id) throws CouldNotPerformException {
+        ExceptionStack exceptionStack = null;
+        for (final Message message : MESSAGE_TYPES) {
+            try {
+                if (containsById(id, message)) {
+                    return true;
+                }
+            } catch (CouldNotPerformException ex) {
+                exceptionStack = MultiException.push(Registries.class, ex, exceptionStack);
+            }
+        }
+        MultiException.checkAndThrow("Could not check if a message with id[" + id + "] is contained", exceptionStack);
+
+        return false;
+    }
+
+    /**
+     * Test if a given message is contained in  a specific registry.
+     *
+     * @param id               the id that is checked
+     * @param messageOrBuilder message or builder defining which registry is checked
+     * @return true if the message is contained
+     * @throws CouldNotPerformException if the check fails
+     */
+    @Experimental
+    public static Boolean containsById(final String id, final MessageOrBuilder messageOrBuilder) throws CouldNotPerformException {
+        return (Boolean) invokeMethod(getMethodName("contains", "ById", messageOrBuilder), messageOrBuilder);
+    }
+
+    /**
+     * Get a message from a registry by id.
+     *
+     * @param id the id that is checked
+     * @return a message with the id
+     * @throws CouldNotPerformException if no message with the id could be found
+     */
+    @Experimental
+    public static Message getById(final String id) throws CouldNotPerformException {
+        ExceptionStack exceptionStack = null;
+        for (final Message message : MESSAGE_TYPES) {
+            try {
+                return getById(id, message);
+            } catch (CouldNotPerformException ex) {
+                exceptionStack = MultiException.push(Registries.class, ex, exceptionStack);
+            }
+        }
+        MultiException.checkAndThrow("Could not check for id[" + id + "]", exceptionStack);
+
+        throw new NotAvailableException("Could not find a message with id[" + id + "]");
+    }
+
+    /**
+     * Get a message from a registry by id from a specific registry.
+     *
+     * @param id               the id that is checked
+     * @param messageOrBuilder message or builder defining which registry is checked
+     * @return a message with the id
+     * @throws CouldNotPerformException if no message with the id could be found
+     */
+    @Experimental
+    public static Message getById(final String id, final MessageOrBuilder messageOrBuilder) throws CouldNotPerformException {
+        return (Message) invokeMethod(getMethodName("get", "ById", messageOrBuilder), messageOrBuilder);
+    }
+
+    /**
+     * Get the list of all messages of a registry.
+     *
+     * @param messageOrBuilder type used to identify the registry whose list is returned
+     * @param <M>              the type of message in the list
+     * @return a list of message in a registry
+     * @throws CouldNotPerformException if the list could not be retrieved for the type
+     */
+    @Experimental
+    public static <M extends Message> List<M> getMessageList(MessageOrBuilder messageOrBuilder) throws CouldNotPerformException {
+        String methodName;
+        if (messageOrBuilder.getDescriptorForType().getName().endsWith("Class")) {
+            methodName = getMethodName("get", "s", messageOrBuilder);
+        } else {
+            methodName = getMethodName("get", "s", messageOrBuilder);
+        }
+
+        return (List<M>) invokeMethod(methodName, messageOrBuilder);
+    }
+
+    /**
+     * Test if a registry is read only.
+     *
+     * @param messageOrBuilder type to identify the registry checked.
+     * @return true if the registry is read only
+     * @throws CouldNotPerformException if the check fails
+     */
+    @Experimental
+    public static Boolean isReadOnly(MessageOrBuilder messageOrBuilder) throws CouldNotPerformException {
+        return (Boolean) invokeMethod(getMethodName("is", "RegistryReadOnly", messageOrBuilder), messageOrBuilder);
+    }
+
+    /**
+     * Test if a registry is consistent.
+     *
+     * @param messageOrBuilder type to identify the registry checked.
+     * @return true if the registry is consistent
+     * @throws CouldNotPerformException if the check fails
+     */
+    @Experimental
+    public static Boolean isConsistent(MessageOrBuilder messageOrBuilder) throws CouldNotPerformException {
+        return (Boolean) invokeMethod(getMethodName("is", "RegistryConsistent", messageOrBuilder), messageOrBuilder);
+    }
+
+    private static Object invokeMethod(final String methodPrefix, final Message message) throws CouldNotPerformException {
+        return invokeMethod(getMethodName(methodPrefix, "", message), message, message);
+    }
+
+    private static Object invokeMethod(final String methodName, final MessageOrBuilder messageOrBuilder, final Object... parameters) throws CouldNotPerformException {
+        final Class[] classes = new Class[parameters.length];
+        for (int i = 0; i < parameters.length; i++) {
+            classes[i] = parameters[i].getClass();
+        }
+
+        try {
+            final RSBRemoteService remote = getRegistryRemoteByType(messageOrBuilder);
+            Method method = remote.getClass().getMethod(methodName, classes);
+            return method.invoke(remote, parameters);
+        } catch (NoSuchMethodException | SecurityException | IllegalAccessException | IllegalArgumentException | InvocationTargetException ex) {
+            throw new CouldNotPerformException("Could not invoke method[" + methodName + "] for type[" + messageOrBuilder.getDescriptorForType().getName() + "]", ex);
+        }
+    }
+
+    private static String getMethodName(final String prefix, final String suffix, final MessageOrBuilder messageOrBuilder) {
+        return prefix + getMethodNameForType(messageOrBuilder) + suffix;
+    }
+
+    private static String getMethodNameForType(final MessageOrBuilder messageOrBuilder) {
+        return messageOrBuilder.getDescriptorForType().getName();
+    }
+
+    private static RSBRemoteService getRegistryRemoteByType(final MessageOrBuilder messageOrBuilder) throws CouldNotPerformException {
+        switch (messageOrBuilder.getDescriptorForType().getName()) {
+            case "UnitConfig":
+                return getUnitRegistry();
+            case "DeviceClass":
+            case "AgentClass":
+            case "AppClass":
+                return getClassRegistry();
+            case "UnitTemplate":
+            case "ServiceTemplate":
+            case "ActivityTemplate":
+                return getTemplateRegistry();
+            case "ActivityConfig":
+            default:
+                throw new NotAvailableException("Registry remote for type [" + messageOrBuilder.getDescriptorForType().getName() + "]");
+
+        }
     }
 }
