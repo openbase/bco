690cf512e4fc79b134c16ab42a86e819270fffa0
Tamino Huxohl <thuxohl@techfak.uni-bielefeld.de>
2018-06-28 16:50:23 +0200

    actiavte authentication per default

--- lib/src/main/java/org/openbase/bco/authentication/lib/jp/JPAuthentication.java
+++ lib/src/main/java/org/openbase/bco/authentication/lib/jp/JPAuthentication.java
@@ -10,23 +10,22 @@ package org.openbase.bco.authentication.lib.jp;
  * it under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Lesser Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Lesser Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/lgpl-3.0.html>.
  * #L%
  */
-import org.openbase.jps.core.JPService;
+
 import org.openbase.jps.exception.JPNotAvailableException;
 import org.openbase.jps.preset.AbstractJPBoolean;
 
 /**
- *
  * @author <a href="mailto:thuxohl@techfak.uni-bielefeld.de">Tamino Huxohl</a>
  */
 public class JPAuthentication extends AbstractJPBoolean {
@@ -39,7 +38,7 @@ public class JPAuthentication extends AbstractJPBoolean {
 
     @Override
     protected Boolean getPropertyDefaultValue() throws JPNotAvailableException {
-        return JPService.testMode();
+        return true;
     }
 
     @Override
