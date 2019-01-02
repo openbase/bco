12a71c2b544bd92e1dbfe361db3e19e91149e713
Divine Threepwood <divine@openbase.org>
2019-01-02 16:12:47 +0100

    licence header 2019 update.

--- openhab/src/main/java/org/openbase/bco/device/openhab/manager/transform/IlluminanceStateDecimalTypeTransformer.java
+++ openhab/src/main/java/org/openbase/bco/device/openhab/manager/transform/IlluminanceStateDecimalTypeTransformer.java
@@ -7,7 +7,7 @@ import org.openbase.type.domotic.state.IlluminanceStateType.IlluminanceState;
  * #%L
  * BCO Openhab Device Manager
  * %%
- * Copyright (C) 2015 - 2018 openbase.org
+ * Copyright (C) 2015 - 2019 openbase.org
  * %%
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as
