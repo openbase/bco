9bdeab2cfbf7a382d3ec0b3345ff1c9a416b3779
Divine Threepwood <divine@openbase.org>
2019-01-18 22:51:48 +0100

    continue on sitemap generation

--- openhab/src/main/java/org/openbase/bco/device/openhab/sitemap/element/GenericUnitSitemapElement.java
+++ openhab/src/main/java/org/openbase/bco/device/openhab/sitemap/element/GenericUnitSitemapElement.java
@@ -26,6 +26,8 @@ import org.openbase.bco.device.openhab.sitemap.SitemapBuilder;
 import org.openbase.bco.device.openhab.sitemap.SitemapBuilder.SitemapIconType;
 import org.openbase.jul.exception.CouldNotPerformException;
 import org.openbase.jul.exception.InstantiationException;
+import org.openbase.type.domotic.service.ServiceConfigType.ServiceConfig;
+import org.openbase.type.domotic.service.ServiceTemplateType.ServiceTemplate.ServicePattern;
 import org.openbase.type.domotic.service.ServiceTemplateType.ServiceTemplate.ServiceType;
 import org.openbase.type.domotic.unit.UnitConfigType.UnitConfig;
 
@@ -78,13 +80,13 @@ public class GenericUnitSitemapElement extends AbstractUnitSitemapElement {
                 sitemap.addSwitchElement(getItem(ServiceType.POWER_STATE_SERVICE), getLabel(), SitemapIconType.SWITCH);
                 break;
             case TAMPER_DETECTOR:
-                sitemap.addTextElement(getItem(ServiceType.TAMPER_STATE_SERVICE), getLabel() + "[]", SitemapIconType.ERROR);
+                sitemap.addTextElement(getItem(ServiceType.TAMPER_STATE_SERVICE), getLabel() + "[%s]", SitemapIconType.ERROR);
                 break;
             case REED_CONTACT:
-                sitemap.addTextElement(getItem(ServiceType.CONTACT_STATE_SERVICE), getLabel() + "[]", SitemapIconType.CONTACT);
+                sitemap.addTextElement(getItem(ServiceType.CONTACT_STATE_SERVICE), getLabel() + "[%s]", SitemapIconType.CONTACT);
                 break;
             case MOTION_DETECTOR:
-                sitemap.addTextElement(getItem(ServiceType.MOTION_STATE_SERVICE), getLabel(), SitemapIconType.MOTION);
+                sitemap.addTextElement(getItem(ServiceType.MOTION_STATE_SERVICE), getLabel() + "[%s]", SitemapIconType.MOTION);
                 break;
             case TEMPERATURE_SENSOR:
                 sitemap.addTextElement(getItem(ServiceType.TEMPERATURE_STATE_SERVICE), getLabel() + " Temperatur [%.1f °C]");
@@ -109,13 +111,13 @@ public class GenericUnitSitemapElement extends AbstractUnitSitemapElement {
                 sitemap.addTextElement(getItem(ServiceType.ILLUMINANCE_STATE_SERVICE), getLabel() + " Helligkeit [%.1f Lux]", SitemapIconType.SUN);
                 break;
             case BUTTON:
-                sitemap.addTextElement(getItem(ServiceType.BUTTON_STATE_SERVICE), getLabel(), SitemapIconType.WALLSWITCH);
+                sitemap.addTextElement(getItem(ServiceType.BUTTON_STATE_SERVICE), getLabel() + "[%s]", SitemapIconType.WALLSWITCH);
                 break;
             case POWER_CONSUMPTION_SENSOR:
                 sitemap.addTextElement(getItem(ServiceType.POWER_CONSUMPTION_STATE_SERVICE), getLabel() + " Vebrauch [%.1f Watt]", SitemapIconType.ENERGY);
                 break;
             case USER:
-                sitemap.addTextElement(getItem(ServiceType.PRESENCE_STATE_SERVICE), getLabel(), SitemapIconType.MOTION);
+                sitemap.addTextElement(getItem(ServiceType.PRESENCE_STATE_SERVICE), unitConfig.getUserConfig().getFirstName() + " " + unitConfig.getUserConfig().getLastName()  + "[%s]", SitemapIconType.MOTION);
                 break;
             case ROLLER_SHUTTER:
                 sitemap.addDefaultElement(getItem(ServiceType.BLIND_STATE_SERVICE), getLabel());
@@ -124,26 +126,52 @@ public class GenericUnitSitemapElement extends AbstractUnitSitemapElement {
                 sitemap.addDefaultElement(getItem(ServiceType.SWITCH_STATE_SERVICE), getLabel());
                 break;
             case SMOKE_DETECTOR:
-                sitemap.addTextElement(getItem(ServiceType.SMOKE_ALARM_STATE_SERVICE), getLabel(), SitemapIconType.SIREN);
-                sitemap.addTextElement(getItem(ServiceType.SMOKE_STATE_SERVICE), getLabel(), SitemapIconType.FIRE);
+                sitemap.addTextElement(getItem(ServiceType.SMOKE_ALARM_STATE_SERVICE), getLabel() + "[%s]", SitemapIconType.SIREN);
+                sitemap.addTextElement(getItem(ServiceType.SMOKE_STATE_SERVICE), getLabel() + "[%s]", SitemapIconType.FIRE);
                 break;
             case CONNECTION:
                 switch (unitConfig.getConnectionConfig().getType()) {
                     case DOOR:
-                        sitemap.addTextElement(getItem(ServiceType.DOOR_STATE_SERVICE), getLabel(), SitemapIconType.DOOR);
+                        sitemap.addTextElement(getItem(ServiceType.DOOR_STATE_SERVICE), getLabel() + "[%s]", SitemapIconType.DOOR);
                         break;
                     case WINDOW:
-                        sitemap.addTextElement(getItem(ServiceType.WINDOW_STATE_SERVICE), getLabel(), SitemapIconType.WINDOW);
+                        sitemap.addTextElement(getItem(ServiceType.WINDOW_STATE_SERVICE), getLabel() + "[%s]", SitemapIconType.WINDOW);
                         break;
                     case PASSAGE:
-                        sitemap.addDefaultElement(getItem(ServiceType.PASSAGE_STATE_SERVICE), getLabel());
+                        sitemap.addDefaultElement(getItem(ServiceType.PASSAGE_STATE_SERVICE), getLabel() + "[%s]");
                         break;
                 }
-                sitemap.addDefaultElement(getItem(ServiceType.BLIND_STATE_SERVICE), getLabel());
+                break;
+            case UNIT_GROUP:
+                for (ServiceConfig serviceConfig : unitConfig.getServiceConfigList()) {
+
+                    // filter non operation services
+                    if(serviceConfig.getServiceDescription().getPattern() != ServicePattern.OPERATION) {
+                        continue;
+                    }
+
+                    switch (serviceConfig.getServiceDescription().getServiceType()) {
+                        case POWER_STATE_SERVICE:
+                            sitemap.addSwitchElement(getItem(ServiceType.POWER_STATE_SERVICE), getLabel(), SitemapIconType.SWITCH);
+                            break;
+                        case COLOR_STATE_SERVICE:
+                            sitemap.addColorpickerElement(getItem(ServiceType.COLOR_STATE_SERVICE), getLabel(), SitemapIconType.COLORWHEEL);
+                            break;
+                        case BRIGHTNESS_STATE_SERVICE:
+                            sitemap.addDefaultElement(getItem(ServiceType.BRIGHTNESS_STATE_SERVICE), getLabel());
+                            break;
+                    }
+                }
+            case LOCATION:
+                sitemap.addDefaultElement(getItem(ServiceType.POWER_STATE_SERVICE), getLabel());
+                break;
+            case DEVICE:
+                break;
+            case AUTHORIZATION_GROUP:
                 break;
             case UNKNOWN:
             default:
-                sitemap.addTextElement("", getLabel());
+                sitemap.addTextElement("", getLabel() + "[%s]");
         }
     }
 }
