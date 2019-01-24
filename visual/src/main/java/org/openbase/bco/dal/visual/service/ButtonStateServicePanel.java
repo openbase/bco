617a1febfaaeb3f9b548d28161e471ada54dc5c6
Divine Threepwood <divine@openbase.org>
2019-01-24 18:09:00 +0100

    implement button provider control elements

--- visual/src/main/java/org/openbase/bco/dal/visual/service/ButtonStateServicePanel.java
+++ visual/src/main/java/org/openbase/bco/dal/visual/service/ButtonStateServicePanel.java
@@ -28,8 +28,21 @@ import org.openbase.jul.exception.InvalidStateException;
 import org.openbase.jul.exception.printer.LogLevel;
 import org.openbase.jul.processing.StringProcessor;
 import java.awt.Color;
+import java.text.DateFormat;
+import java.util.Date;
+import org.openbase.bco.dal.lib.action.ActionDescriptionProcessor;
+import org.openbase.bco.dal.lib.jp.JPProviderControlMode;
+import org.openbase.bco.dal.lib.layer.service.Services;
 import org.openbase.bco.dal.lib.layer.service.consumer.ConsumerService;
 import org.openbase.bco.dal.lib.layer.service.operation.OperationService;
+import org.openbase.jps.core.JPService;
+import org.openbase.jps.exception.JPNotAvailableException;
+import org.openbase.jul.exception.NotAvailableException;
+import org.openbase.jul.extension.type.processing.TimestampJavaTimeTransform;
+import org.openbase.type.domotic.action.ActionDescriptionType.ActionDescription;
+import org.openbase.type.domotic.service.ServiceTemplateType;
+import org.openbase.type.domotic.state.ButtonStateType;
+import org.openbase.type.domotic.state.ButtonStateType.ButtonState;
 
 /**
  *
@@ -37,6 +50,8 @@ import org.openbase.bco.dal.lib.layer.service.operation.OperationService;
  */
 public class ButtonStateServicePanel extends AbstractServicePanel<ButtonStateProviderService, ConsumerService, OperationService> {
 
+    private final DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.FULL, DateFormat.MEDIUM);
+    
     /**
      * Creates new form BrightnessService
      *
@@ -44,6 +59,13 @@ public class ButtonStateServicePanel extends AbstractServicePanel<ButtonStatePro
      */
     public ButtonStateServicePanel() throws org.openbase.jul.exception.InstantiationException {
         initComponents();
+        
+        try {
+            applyButtonStateButton.setVisible(JPService.getProperty(JPProviderControlMode.class).getValue());
+        } catch (JPNotAvailableException ex) {
+            applyButtonStateButton.setVisible(false);
+            ExceptionPrinter.printHistory(ex, logger, LogLevel.ERROR);
+        }
     }
 
     /**
@@ -55,6 +77,9 @@ public class ButtonStateServicePanel extends AbstractServicePanel<ButtonStatePro
 
         standbyStatePanel = new javax.swing.JPanel();
         buttonStatusLabel = new javax.swing.JLabel();
+        applyButtonStateButton = new javax.swing.JButton();
+        lastButtonPressLabel = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
 
         standbyStatePanel.setBackground(new java.awt.Color(204, 204, 204));
         standbyStatePanel.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(0, 0, 0), 5, true));
@@ -77,27 +102,70 @@ public class ButtonStateServicePanel extends AbstractServicePanel<ButtonStatePro
             .addComponent(buttonStatusLabel, javax.swing.GroupLayout.DEFAULT_SIZE, 23, Short.MAX_VALUE)
         );
 
+        applyButtonStateButton.setText("Press the Button");
+        applyButtonStateButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                applyButtonStateButtonActionPerformed(evt);
+            }
+        });
+
+        lastButtonPressLabel.setText("N/A");
+
+        jLabel2.setText("Last Press:");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(standbyStatePanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(standbyStatePanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jLabel2)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(lastButtonPressLabel, javax.swing.GroupLayout.DEFAULT_SIZE, 214, Short.MAX_VALUE)))
                 .addContainerGap())
+            .addComponent(applyButtonStateButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(standbyStatePanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(lastButtonPressLabel)
+                    .addComponent(jLabel2))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(applyButtonStateButton))
         );
     }// </editor-fold>//GEN-END:initComponents
 
+    private void applyButtonStateButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_applyButtonStateButtonActionPerformed
+        try {
+            ButtonState buttonState;
+            switch (getProviderService().getButtonState().getValue()) {
+                case PRESSED:
+                buttonState = ButtonState.newBuilder().setValue(ButtonState.State.RELEASED).build();
+                break;
+                default:
+                buttonState = ButtonState.newBuilder().setValue(ButtonState.State.PRESSED).build();
+                break;
+            }
+            ActionDescription action = ActionDescriptionProcessor.generateActionDescriptionBuilder(buttonState, ServiceTemplateType.ServiceTemplate.ServiceType.BUTTON_STATE_SERVICE, getUnitRemote()).build();
+            notifyActionProcessing(getUnitRemote().applyAction(action));
+        } catch (CouldNotPerformException ex) {
+            ExceptionPrinter.printHistory(ex, logger, LogLevel.ERROR);
+        }
+    }//GEN-LAST:event_applyButtonStateButtonActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton applyButtonStateButton;
     private javax.swing.JLabel buttonStatusLabel;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel lastButtonPressLabel;
     private javax.swing.JPanel standbyStatePanel;
     // End of variables declaration//GEN-END:variables
 
@@ -107,6 +175,7 @@ public class ButtonStateServicePanel extends AbstractServicePanel<ButtonStatePro
             switch (getProviderService().getButtonState().getValue()) {
                 case PRESSED:
                     buttonStatusLabel.setForeground(Color.BLACK);
+                    applyButtonStateButton.setText("Release the Button");
                     standbyStatePanel.setBackground(Color.GREEN.darker());
                     break;
                 case DOUBLE_PRESSED:
@@ -115,6 +184,7 @@ public class ButtonStateServicePanel extends AbstractServicePanel<ButtonStatePro
                     break;
                 case RELEASED:
                     buttonStatusLabel.setForeground(Color.BLACK);
+                    applyButtonStateButton.setText("Press the Button");
                     standbyStatePanel.setBackground(Color.LIGHT_GRAY);
                     break;
                 case UNKNOWN:
@@ -125,6 +195,17 @@ public class ButtonStateServicePanel extends AbstractServicePanel<ButtonStatePro
                     throw new InvalidStateException("State[" + getProviderService().getButtonState().getValue() + "] is unknown.");
             }
             buttonStatusLabel.setText("Current ButtonState = " + StringProcessor.transformUpperCaseToPascalCase(getProviderService().getButtonState().getValue().name()));
+            
+            try {
+                try {
+                    lastButtonPressLabel.setText(dateFormat.format(new Date(TimestampJavaTimeTransform.transform(Services.getLatestValueOccurrence(ButtonStateType.ButtonState.State.PRESSED, getProviderService().getButtonState())))));
+                } catch (NotAvailableException ex) {
+                    lastButtonPressLabel.setText("Never");
+                }
+            } catch (Exception ex) {
+                lastButtonPressLabel.setText("N/A");
+                ExceptionPrinter.printHistory(new CouldNotPerformException("Could not format: [" + Services.getLatestValueOccurrence(ButtonStateType.ButtonState.State.PRESSED, getProviderService().getButtonState()).getTime() + "]!", ex), logger, LogLevel.ERROR);
+            }
         } catch (CouldNotPerformException ex) {
             ExceptionPrinter.printHistory(ex, logger, LogLevel.ERROR);
         }
