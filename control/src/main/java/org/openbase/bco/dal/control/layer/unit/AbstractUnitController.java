18fb5aad87ca2794f9859c2025fdc20a2c3b5774
Tamino Huxohl <thuxohl@techfak.uni-bielefeld.de>
2019-04-13 15:17:18 +0200

    readd missing file check

--- control/src/main/java/org/openbase/bco/dal/control/layer/unit/AbstractUnitController.java
+++ control/src/main/java/org/openbase/bco/dal/control/layer/unit/AbstractUnitController.java
@@ -10,12 +10,12 @@ package org.openbase.bco.dal.control.layer.unit;
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- *
+ * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *
+ * 
  * You should have received a copy of the GNU General Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/gpl-3.0.html>.
@@ -970,7 +970,7 @@ public abstract class AbstractUnitController<D extends AbstractMessage & Seriali
                 actionDescriptionBuilder.getActionInitiatorBuilder().clear();
 
                 // if an authentication token is send replace the initiator in any case
-                if (authenticationBaseData.getAuthenticationToken() != null) {
+                if (authenticationBaseData != null && authenticationBaseData.getAuthenticationToken() != null) {
                     actionDescriptionBuilder.getActionInitiatorBuilder().setInitiatorId(authenticationBaseData.getAuthenticationToken().getUserId());
                 }
 
