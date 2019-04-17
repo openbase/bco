8e8d7680bf4774b556975454dc66bd0fc000b54f
Divine Threepwood <divine@openbase.org>
2019-04-17 14:54:23 +0200

    fix deploment

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -4,6 +4,6 @@ echo '### start deployment...'
 if [ "${AUTO_DEPLOY}" = False ] ; then
     echo '### Skip deloyment because auto deployment is disabled.'
 else 
-    ${AUTO_DEPLOY} && mvn deploy -Pdeploy,sonatype --settings .travis/settings.xml -DskipTests=true -B
+    mvn deploy -Pdeploy,sonatype --settings .travis/settings.xml -DskipTests=true -B
     echo '### deployment successfully finished'
 fi
