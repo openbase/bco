a4db5d30198b42187e5a1fe74fe0b5e77069d787
Divine Threepwood <divine@openbase.org>
2019-04-26 17:14:56 +0200

    Implement BCOAppManagerTestLauncher

---
+++ util/src/main/java/org/openbase/bco/app/util/launch/BCOAppManagerTestLauncher.java
@@ -0,0 +1,10 @@
+package org.openbase.bco.app.util.launch;
+
+import org.openbase.bco.dal.control.layer.unit.app.AppManagerLauncher;
+
+public class BCOAppManagerTestLauncher {
+
+    public static void main(String[] args) throws Throwable {
+        AppManagerLauncher.main(args);
+    }
+}
