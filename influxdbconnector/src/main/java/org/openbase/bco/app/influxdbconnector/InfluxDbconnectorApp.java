a6f1cd97c6f67142316e0d00e277c6747d165cb0
Divine Threepwood <divine@openbase.org>
2019-04-29 11:17:26 +0200

    fix header

--- influxdbconnector/src/main/java/org/openbase/bco/app/influxdbconnector/InfluxDbconnectorApp.java
+++ influxdbconnector/src/main/java/org/openbase/bco/app/influxdbconnector/InfluxDbconnectorApp.java
@@ -1,5 +1,27 @@
 package org.openbase.bco.app.influxdbconnector;
 
+/*-
+ * #%L
+ * BCO InfluxDB Connector
+ * %%
+ * Copyright (C) 2018 - 2019 openbase.org
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+
 import org.openbase.bco.dal.control.layer.unit.app.AbstractAppController;
 import org.openbase.jul.exception.CouldNotPerformException;
 import org.openbase.jul.exception.InstantiationException;
