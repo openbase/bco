7551b9c180acf647d65185a4b1c8fb8709373f58
David Weinholz <david-weinholz@web.de>
2019-06-07 17:23:29 +0200

    fixed exception

--- influxdbconnector/src/main/java/org/openbase/bco/app/influxdbconnector/InfluxDbconnectorApp.java
+++ influxdbconnector/src/main/java/org/openbase/bco/app/influxdbconnector/InfluxDbconnectorApp.java
@@ -10,12 +10,12 @@ package org.openbase.bco.app.influxdbconnector;
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/gpl-3.0.html>.
@@ -276,24 +276,14 @@ public class InfluxDbconnectorApp extends AbstractAppController {
 
             lastServiceState = TimestampProcessor.updateTimestamp(serviceStateTimestamp, lastServiceState, TimeUnit.MILLISECONDS);
             storeServiceState(unit, serviceType, currentServiceState);
-            try {
-                storeServiceState(unit, serviceType, lastServiceState);
-            } catch (NotAvailableException ex) {
-                logger.warn("\nUnitType: " + unit.getUnitType().toString() + "\n" +
-                        "ServiceType: " + serviceType.toString() + "\n" +
-                        "CurrentServiceState: " + currentServiceState.toString() + "\n" +
-                        "LastServiceState: " + lastServiceState.toString() + "\n" +
-                        "LastServiceState: " + ex);
-
-
-            }
-        } catch (NotAvailableException ex) {
-
-            logger.warn("\nUnitType: " + unit.getUnitType().toString() + "\n" +
-                    "ServiceType: " + serviceType.toString() + "\n" +
-                    "CurrentServiceState: " + currentServiceState.toString() + "\n" +
-                    "LastServiceState: " + lastServiceState.toString() + "\n" +
-                    "CurrentServiceState: " + ex);
+            storeServiceState(unit, serviceType, lastServiceState);
+        } catch (CouldNotPerformException ex) {
+            ExceptionPrinter.printHistory(
+                    "UnitType[" + unit.getUnitType().toString() + "]" +
+                            "ServiceType[" + serviceType.toString() + "]" +
+                            "CurrentServiceState[" + currentServiceState.toString() + "]" +
+                            "LastServiceState[" + lastServiceState.toString() + "]"
+                    , ex, logger, LogLevel.WARN);
 
         }
 
