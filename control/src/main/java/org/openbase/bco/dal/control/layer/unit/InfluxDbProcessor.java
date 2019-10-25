99f80509596d22c6f0405f11e620f5ba228e7448
Divine Threepwood <divine@openbase.org>
2019-10-25 13:14:39 +0200

    clean imports

--- control/src/main/java/org/openbase/bco/dal/control/layer/unit/InfluxDbProcessor.java
+++ control/src/main/java/org/openbase/bco/dal/control/layer/unit/InfluxDbProcessor.java
@@ -10,12 +10,12 @@ package org.openbase.bco.dal.control.layer.unit;
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/gpl-3.0.html>.
@@ -26,10 +26,8 @@ import com.google.protobuf.Descriptors;
 import com.google.protobuf.Message;
 import com.influxdb.client.InfluxDBClient;
 import com.influxdb.client.InfluxDBClientFactory;
-import com.influxdb.client.QueryApi;
 import com.influxdb.query.FluxRecord;
 import com.influxdb.query.FluxTable;
-import com.sun.tools.jdi.LockObject;
 import org.openbase.bco.dal.lib.layer.service.Services;
 import org.openbase.bco.registry.remote.Registries;
 import org.openbase.jul.exception.CouldNotPerformException;
