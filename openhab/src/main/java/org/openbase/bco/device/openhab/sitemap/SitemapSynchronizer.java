c2ac6104b3f6dfb7083622514222933b667b51b0
Divine Threepwood <divine@openbase.org>
2020-01-02 22:04:42 +0100

    update year in license header.

--- openhab/src/main/java/org/openbase/bco/device/openhab/sitemap/SitemapSynchronizer.java
+++ openhab/src/main/java/org/openbase/bco/device/openhab/sitemap/SitemapSynchronizer.java
@@ -23,7 +23,7 @@ import java.io.File;
  * #%L
  * BCO Openhab Device Manager
  * %%
- * Copyright (C) 2015 - 2019 openbase.org
+ * Copyright (C) 2015 - 2020 openbase.org
  * %%
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as
