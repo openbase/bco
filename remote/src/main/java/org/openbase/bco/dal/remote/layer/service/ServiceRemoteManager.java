a37c28d368aad737dfe2e45046d817e804974765
Divine Threepwood <divine@openbase.org>
2020-01-31 00:34:42 +0100

    split timeout

--- remote/src/main/java/org/openbase/bco/dal/remote/layer/service/ServiceRemoteManager.java
+++ remote/src/main/java/org/openbase/bco/dal/remote/layer/service/ServiceRemoteManager.java
@@ -538,10 +538,10 @@ public abstract class ServiceRemoteManager<D extends Message> implements Activat
 
     @Override
     public void waitForData(long timeout, TimeUnit timeUnit) throws CouldNotPerformException, InterruptedException {
+        final TimeoutSplitter timeSplit = new TimeoutSplitter(timeout, timeUnit);
         try (final CloseableReadLockWrapper ignored = lockProvider.getCloseableReadLock(this)) {
             for (final Remote<D> remote : serviceRemoteMap.values()) {
-                // todo: split timeout
-                remote.waitForData(timeout, timeUnit);
+                remote.waitForData(timeSplit.getTime(), TimeUnit.MILLISECONDS);
             }
         }
     }
