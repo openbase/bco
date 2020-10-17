98cf2a4f56a8ae1d5cb52177a0e4d3cdbe11bfaa
Divine Threepwood <divine@openbase.org>
2020-10-17 17:29:03 +0200

    Login to bco and implement mutation

--- src/main/java/org/openbase/bco/api/graphql/RegistrySchemaModule.java
+++ src/main/java/org/openbase/bco/api/graphql/RegistrySchemaModule.java
@@ -1,13 +1,22 @@
 package org.openbase.bco.api.graphql;
 
 import com.google.api.graphql.rejoiner.Arg;
+import com.google.api.graphql.rejoiner.Mutation;
 import com.google.api.graphql.rejoiner.Query;
 import com.google.api.graphql.rejoiner.SchemaModule;
-import graphql.schema.GraphQLArgument;
-import graphql.schema.GraphQLTypeReference;
+import com.google.common.util.concurrent.Futures;
+import org.openbase.bco.dal.remote.layer.unit.Units;
 import org.openbase.bco.registry.remote.Registries;
+import org.openbase.bco.registry.remote.login.BCOLogin;
 import org.openbase.jul.exception.CouldNotPerformException;
+import org.openbase.jul.exception.NotAvailableException;
+import org.openbase.jul.schedule.FutureProcessor;
+import org.openbase.type.domotic.action.ActionDescriptionType.ActionDescription;
+import org.openbase.type.domotic.state.PowerStateType.PowerState;
+import org.openbase.type.domotic.state.PowerStateType.PowerState.State;
 import org.openbase.type.domotic.unit.UnitConfigType.UnitConfig;
+
+import java.util.concurrent.*;
 
 public class RegistrySchemaModule extends SchemaModule {
 
@@ -22,4 +31,28 @@ public class RegistrySchemaModule extends SchemaModule {
         }
         return UnitConfig.getDefaultInstance();
     }
-}
+
+    @Mutation("lamp")
+    ActionDescription getLamp(@Arg("alias") String alias, @Arg("power") String state) throws InterruptedException, NotAvailableException, TimeoutException, ExecutionException {
+
+//        final State state1 = State.valueOf(state);
+//        try {
+//            Futures.
+//            return CompleFutureProcessor.toCompletableFuture(Units.getUnitByAlias(alias, true, Units.COLORABLE_LIGHT).setPowerState(state1));
+//        } catch (NotAvailableException | ExecutionException | TimeoutException e) {
+//            throw e;
+//        } catch (InterruptedException e) {
+//            throw e;
+//        }
+//    }
+
+        final State state1 = State.valueOf(state);
+            try {
+                return Units.getUnitByAlias(alias, true, Units.COLORABLE_LIGHT).setPowerState(state1).get(5, TimeUnit.SECONDS);
+            } catch (NotAvailableException | ExecutionException | TimeoutException e) {
+                throw e;
+            } catch (InterruptedException e) {
+               throw e;
+            }
+        }
+    }
