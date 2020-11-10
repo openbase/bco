83e44f1456b429f400787bb95b93c6784a21fbb3
Divine Threepwood <divine@openbase.org>
2020-11-11 00:11:18 +0100

    fix shutdown log

--- src/main/java/org/openbase/bco/api/graphql/BcoApiGraphQlSpringLaunchable.java
+++ src/main/java/org/openbase/bco/api/graphql/BcoApiGraphQlSpringLaunchable.java
@@ -1,5 +1,27 @@
 package org.openbase.bco.api.graphql;
 
+/*-
+ * #%L
+ * BCO GraphQL API
+ * %%
+ * Copyright (C) 2020 openbase.org
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+
 import org.openbase.bco.registry.remote.Registries;
 import org.openbase.bco.registry.remote.login.BCOLogin;
 import org.openbase.jul.exception.CouldNotPerformException;
@@ -30,11 +52,10 @@ public class BcoApiGraphQlSpringLaunchable implements Launchable<Void>, VoidInit
 
     @Override
     public void deactivate() {
-
-        LOGGER.info("Login to bco...");
+        LOGGER.info("Logout...");
         BCOLogin.getSession().logout();
 
-        LOGGER.info("Start webserver...");
+        LOGGER.info("Shutdown spring application...");
         SpringApplication.exit(context);
         context = null;
     }
