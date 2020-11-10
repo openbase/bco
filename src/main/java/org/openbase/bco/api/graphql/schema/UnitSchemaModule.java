a591fef0127b31b76c60ff8fb29857276278758b
Tamino Huxohl <thuxohl@techfak.uni-bielefeld.de>
2020-11-10 22:22:18 +0100

    implement setting power state for lamp with token

--- src/main/java/org/openbase/bco/api/graphql/schema/UnitSchemaModule.java
+++ src/main/java/org/openbase/bco/api/graphql/schema/UnitSchemaModule.java
@@ -1,11 +1,19 @@
 package org.openbase.bco.api.graphql.schema;
 
 import com.google.api.graphql.rejoiner.*;
+import graphql.schema.DataFetchingEnvironment;
 import graphql.schema.GraphQLFieldDefinition;
+import org.openbase.bco.api.graphql.AuthorizationContext;
 import org.openbase.bco.api.graphql.coercing.GraphQLScalars;
+import org.openbase.bco.dal.lib.action.ActionDescriptionProcessor;
+import org.openbase.bco.dal.remote.layer.unit.ColorableLightRemote;
 import org.openbase.bco.dal.remote.layer.unit.Units;
-import org.openbase.jul.exception.NotAvailableException;
+import org.openbase.jul.exception.CouldNotPerformException;
 import org.openbase.type.domotic.action.ActionDescriptionType.ActionDescription;
+import org.openbase.type.domotic.action.ActionParameterType;
+import org.openbase.type.domotic.authentication.AuthTokenType;
+import org.openbase.type.domotic.service.ServiceTemplateType;
+import org.openbase.type.domotic.state.PowerStateType;
 import org.openbase.type.domotic.state.PowerStateType.PowerState.State;
 import org.openbase.type.domotic.unit.UnitConfigType.UnitConfig;
 
@@ -23,7 +31,7 @@ public class UnitSchemaModule extends SchemaModule {
                     .build());
 
     @Mutation("unit")
-    ActionDescription getLamp(@Arg("alias") String alias, @Arg("power") String state) throws InterruptedException, NotAvailableException, TimeoutException, ExecutionException {
+    ActionDescription getLamp(@Arg("alias") String alias, @Arg("power") String state, DataFetchingEnvironment env) throws InterruptedException, CouldNotPerformException, TimeoutException, ExecutionException {
 
 //        final State state1 = State.valueOf(state);
 //        try {
@@ -35,8 +43,12 @@ public class UnitSchemaModule extends SchemaModule {
 //            throw e;
 //        }
 //    }
+        final String token = ((AuthorizationContext) env.getContext()).getToken();
 
-        final State state1 = State.valueOf(state);
-        return Units.getUnitByAlias(alias, true, Units.COLORABLE_LIGHT).setPowerState(state1).get(5, TimeUnit.SECONDS);
+        ColorableLightRemote unit = Units.getUnitByAlias(alias, true, Units.COLORABLE_LIGHT);
+        PowerStateType.PowerState powerState = PowerStateType.PowerState.newBuilder().setValue(State.valueOf(state)).build();
+        ActionParameterType.ActionParameter.Builder builder = ActionDescriptionProcessor.generateDefaultActionParameter(powerState, ServiceTemplateType.ServiceTemplate.ServiceType.POWER_STATE_SERVICE, unit);
+        builder.setAuthToken(AuthTokenType.AuthToken.newBuilder().setAuthenticationToken(token).build());
+        return unit.applyAction(builder).get(5, TimeUnit.SECONDS);
     }
 }
