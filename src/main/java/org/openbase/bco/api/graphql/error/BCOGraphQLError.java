2c85305d699a3b7f2f8e8f9f5b0065193bac48e2
Tamino Huxohl <thuxohl@techfak.uni-bielefeld.de>
2020-11-16 13:31:46 +0100

    add missing license headers

--- src/main/java/org/openbase/bco/api/graphql/error/BCOGraphQLError.java
+++ src/main/java/org/openbase/bco/api/graphql/error/BCOGraphQLError.java
@@ -1,11 +1,33 @@
 package org.openbase.bco.api.graphql.error;
 
+/*-
+ * #%L
+ * BCO GraphQL API
+ * %%
+ * Copyright (C) 2020 openbase.org
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+
 import graphql.GraphQLError;
 import graphql.language.SourceLocation;
 
 import java.util.List;
 
-public abstract class BCOGraphQLError extends Throwable implements GraphQLError {
+public abstract class BCOGraphQLError extends Exception implements GraphQLError {
 
     private List<Object> path = null;
     private List<SourceLocation> locations = null;
