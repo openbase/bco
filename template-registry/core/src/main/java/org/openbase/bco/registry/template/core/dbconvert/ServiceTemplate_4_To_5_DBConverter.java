035066404ba422e5cb76a1f39840a3acadde69f8
Tamino Huxohl <thuxohl@techfak.uni-bielefeld.de>
2020-11-16 16:08:29 +0100

    add new db converter

---
+++ template-registry/core/src/main/java/org/openbase/bco/registry/template/core/dbconvert/ServiceTemplate_4_To_5_DBConverter.java
@@ -0,0 +1,46 @@
+package org.openbase.bco.registry.template.core.dbconvert;
+
+/*-
+ * #%L
+ * BCO Registry Template Core
+ * %%
+ * Copyright (C) 2014 - 2020 openbase.org
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+
+import com.google.gson.JsonObject;
+import org.openbase.jul.exception.CouldNotPerformException;
+import org.openbase.jul.storage.registry.version.AbstractDBVersionConverter;
+import org.openbase.jul.storage.registry.version.DBVersionControl;
+
+import java.io.File;
+import java.util.Map;
+
+/**
+ * Empty db converter to indicate the addition of the discovery service.
+ */
+public class ServiceTemplate_4_To_5_DBConverter extends AbstractDBVersionConverter {
+
+    public ServiceTemplate_4_To_5_DBConverter(final DBVersionControl versionControl) {
+        super(versionControl);
+    }
+
+    @Override
+    public JsonObject upgrade(JsonObject outdatedDBEntry, Map<File, JsonObject> dbSnapshot) throws CouldNotPerformException {
+        return outdatedDBEntry;
+    }
+}
