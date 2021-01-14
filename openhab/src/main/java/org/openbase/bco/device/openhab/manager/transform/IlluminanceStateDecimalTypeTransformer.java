56b1065e4dbd07e38ee53f44130dd1ea720c1e31
Divine Threepwood <divine@openbase.org>
2021-01-14 19:34:35 +0100

    update licence header to 2021

--- openhab/src/main/java/org/openbase/bco/device/openhab/manager/transform/IlluminanceStateDecimalTypeTransformer.java
+++ openhab/src/main/java/org/openbase/bco/device/openhab/manager/transform/IlluminanceStateDecimalTypeTransformer.java
@@ -8,7 +8,7 @@ import org.openbase.type.domotic.state.IlluminanceStateType.IlluminanceState;
  * #%L
  * BCO Openhab Device Manager
  * %%
- * Copyright (C) 2015 - 2020 openbase.org
+ * Copyright (C) 2015 - 2021 openbase.org
  * %%
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as
--- openhab/src/main/java/org/openbase/bco/device/openhab/registry/OpenHABConfigSynchronizerLauncher.java
+++ openhab/src/main/java/org/openbase/bco/device/openhab/registry/OpenHABConfigSynchronizerLauncher.java
@@ -4,7 +4,7 @@ package org.openbase.bco.device.openhab.registry;
  * #%L
  * BCO Openhab Device Manager
  * %%
- * Copyright (C) 2015 - 2020 openbase.org
+ * Copyright (C) 2015 - 2021 openbase.org
  * %%
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as
