6a212a3408ec130866b2e704694de131eaf5a393
Divine Threepwood <divine@openbase.org>
2021-01-15 00:43:20 +0100

    move unit filter service methods to registry and used unit filter operation from there.

--- src/main/java/org/openbase/bco/api/graphql/schema/UnitSchemaModule.java
+++ src/main/java/org/openbase/bco/api/graphql/schema/UnitSchemaModule.java
@@ -36,6 +36,7 @@ import org.openbase.bco.dal.lib.layer.unit.UnitRemote;
 import org.openbase.bco.dal.remote.action.RemoteAction;
 import org.openbase.bco.dal.remote.layer.unit.Units;
 import org.openbase.bco.registry.remote.Registries;
+import org.openbase.bco.registry.unit.lib.filter.UnitConfigFilterImpl;
 import org.openbase.jul.exception.CouldNotPerformException;
 import org.openbase.jul.exception.NotAvailableException;
 import org.openbase.jul.extension.protobuf.ProtoBufBuilderProcessor;
@@ -54,10 +55,14 @@ import java.util.concurrent.TimeUnit;
 
 public class UnitSchemaModule extends SchemaModule {
 
+    // ===================================== Schema Modifications ======================================================
+
     @SchemaModification(addField = "config", onType = UnitData.class)
     UnitConfig addConfigToData(UnitData data) throws CouldNotPerformException, InterruptedException {
         return Registries.getUnitRegistry(ServerError.BCO_TIMEOUT_SHORT, ServerError.BCO_TIMEOUT_TIME_UNIT).getUnitConfigById(data.getId());
     }
+
+    // ===================================== Queries ===================================================================
 
     @Query("unit")
     UnitData unit(@Arg("unitId") String id) throws BCOGraphQLError {
@@ -73,7 +78,7 @@ public class UnitSchemaModule extends SchemaModule {
     ImmutableList<UnitData> units(@Arg("filter") UnitFilter unitFilter) throws BCOGraphQLError {
         try {
             final List<UnitData> dataList = new ArrayList<>();
-            for (UnitConfig unitConfig : getUnitConfigsByFilter(unitFilter)) {
+            for (UnitConfig unitConfig : Registries.getUnitRegistry(ServerError.BCO_TIMEOUT_SHORT, ServerError.BCO_TIMEOUT_TIME_UNIT).getUnitConfigs(unitFilter)) {
                 UnitRemote<?> unit = Units.getUnit(unitConfig, ServerError.BCO_TIMEOUT_SHORT, ServerError.BCO_TIMEOUT_TIME_UNIT);
                 dataList.add((UnitData) ProtoBufBuilderProcessor.merge(UnitData.newBuilder(), unit.getData()).build());
             }
@@ -83,7 +88,7 @@ public class UnitSchemaModule extends SchemaModule {
         }
     }
 
-
+    // ===================================== Queries ===================================================================
 
     @Mutation("unit")
     UnitData unit(@Arg("unitId") String unitId, @Arg("data") UnitData data, DataFetchingEnvironment env) throws BCOGraphQLError {
@@ -95,10 +100,10 @@ public class UnitSchemaModule extends SchemaModule {
     }
 
     @Mutation("units")
-    ImmutableList<UnitData> units(@Arg("filter") UnitFilter filter, @Arg("data") UnitData data, DataFetchingEnvironment env) throws BCOGraphQLError {
+    ImmutableList<UnitData> units(@Arg("filter") UnitFilter unitFilter, @Arg("data") UnitData data, DataFetchingEnvironment env) throws BCOGraphQLError {
         try {
             final List<UnitData> dataList = new ArrayList<>();
-            for (UnitConfig unitConfig : getUnitConfigsByFilter(filter)) {
+            for (UnitConfig unitConfig : Registries.getUnitRegistry(ServerError.BCO_TIMEOUT_SHORT, ServerError.BCO_TIMEOUT_TIME_UNIT).getUnitConfigs(unitFilter)) {
                 dataList.add(setServiceStates(unitConfig, data, env, ServerError.BCO_TIMEOUT_SHORT, ServerError.BCO_TIMEOUT_TIME_UNIT));
             }
             return ImmutableList.copyOf(dataList);
@@ -107,17 +112,7 @@ public class UnitSchemaModule extends SchemaModule {
         }
     }
 
-    private List<UnitConfig> getUnitConfigsByFilter(UnitFilter unitFilter) throws CouldNotPerformException, InterruptedException {
-        return buildUnitConfigFilter(unitFilter).pass(Registries.getUnitRegistry(ServerError.BCO_TIMEOUT_SHORT, ServerError.BCO_TIMEOUT_TIME_UNIT).getUnitConfigsFiltered(true));
-    }
-
-    private ListFilter<UnitConfig> buildUnitConfigFilter(UnitFilter unitFilter) throws CouldNotPerformException, InterruptedException {
-        // setup default values
-        if ((unitFilter == null)) {
-            unitFilter = UnitFilter.getDefaultInstance();
-        }
-        return new RegistrySchemaModule.UnitFilterImpl(unitFilter);
-    }
+    // ===================================== Service Methods ===========================================================
 
     private UnitData setServiceStates(final UnitConfig unitConfig, final UnitData data, DataFetchingEnvironment env, final long timeout, final TimeUnit timeUnit) throws CouldNotPerformException, InterruptedException {
         return setServiceStates(unitConfig.getId(), data, env, timeout, timeUnit);
