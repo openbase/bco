1eab7c0bdf215f01997f84b8020c0c24e72eea01
Divine Threepwood <divine@openbase.org>
2021-01-14 19:34:35 +0100

    update licence header to 2021

--- cloudconnector/src/main/java/org/openbase/bco/app/cloudconnector/CloudConnectorRemote.java
+++ cloudconnector/src/main/java/org/openbase/bco/app/cloudconnector/CloudConnectorRemote.java
@@ -4,7 +4,7 @@ package org.openbase.bco.app.cloudconnector;
  * #%L
  * BCO Cloud Connector
  * %%
- * Copyright (C) 2018 - 2020 openbase.org
+ * Copyright (C) 2018 - 2021 openbase.org
  * %%
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as
--- openapiserver/src/test/java/org/openbase/app/openapiserver/OpenApiServerAppTest.java
+++ openapiserver/src/test/java/org/openbase/app/openapiserver/OpenApiServerAppTest.java
@@ -4,7 +4,7 @@ package org.openbase.app.openapiserver;
  * #%L
  * BCO OpenAPI Server
  * %%
- * Copyright (C) 2018 - 2020 openbase.org
+ * Copyright (C) 2018 - 2021 openbase.org
  * %%
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as
