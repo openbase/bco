c258f06881bbafa6b793e2a08ab7d2122c213713
Divine Threepwood <divine@openbase.org>
2021-02-20 18:43:21 +0100

    refactor train data generation to generate iluminance value as well

--- util/src/main/java/org/openbase/bco/app/util/launch/BCOTrainDataGeneratorLauncher.java
+++ util/src/main/java/org/openbase/bco/app/util/launch/BCOTrainDataGeneratorLauncher.java
@@ -22,31 +22,33 @@ package org.openbase.bco.app.util.launch;
  * #L%
  */
 
+import org.openbase.bco.authentication.lib.BCO;
 import org.openbase.bco.dal.lib.action.ActionDescriptionProcessor;
 import org.openbase.bco.dal.lib.jp.JPProviderControlMode;
 import org.openbase.bco.dal.remote.action.Actions;
 import org.openbase.bco.dal.remote.layer.unit.ColorableLightRemote;
 import org.openbase.bco.dal.remote.layer.unit.Units;
 import org.openbase.bco.dal.remote.layer.unit.location.LocationRemote;
-import org.openbase.bco.authentication.lib.BCO;
 import org.openbase.bco.registry.remote.Registries;
 import org.openbase.bco.registry.remote.login.BCOLogin;
 import org.openbase.jps.core.JPService;
+import org.openbase.jps.exception.JPServiceException;
 import org.openbase.jul.exception.CouldNotPerformException;
+import org.openbase.jul.exception.ExceptionProcessor;
 import org.openbase.jul.exception.printer.ExceptionPrinter;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.openbase.type.domotic.action.ActionDescriptionType.ActionDescription;
 import org.openbase.type.domotic.service.ServiceTemplateType.ServiceTemplate.ServiceType;
+import org.openbase.type.domotic.state.IlluminanceStateType.IlluminanceState;
 import org.openbase.type.domotic.state.PowerStateType.PowerState;
 import org.openbase.type.domotic.state.PresenceStateType.PresenceState;
-import org.openbase.type.domotic.state.PresenceStateType.PresenceState.State;
 import org.openbase.type.vision.HSBColorType.HSBColor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.util.Random;
+import java.util.*;
 import java.util.concurrent.CancellationException;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 /**
  * @author <a href="mailto:divine@openbase.org">Divine Threepwood</a>
@@ -55,13 +57,24 @@ public class BCOTrainDataGeneratorLauncher {
 
     public static final Logger LOGGER = LoggerFactory.getLogger(BCOTrainDataGeneratorLauncher.class);
     private static final Random random = new Random(System.currentTimeMillis());
+    public static final long TIMEOUT = 5000;
+
+    public enum TrainCondition {
+        PRESENCE_DARK_ON,
+        ABSENCE_DARK_OFF,
+        PRESENCE_SUNNY_OFF,
+        ABSENCE_SUNNY_OFF
+    }
+
+    final static List<TrainCondition> trainConditions = new ArrayList<>(Arrays.asList(TrainCondition.values()));
 
     /**
      * @param args the command line arguments
      */
-    public static void main(final String[] args) {
+    public static void main(final String[] args) throws JPServiceException {
         BCO.printLogo();
         JPService.registerProperty(JPProviderControlMode.class, true);
+        JPService.parse(args);
 
         BCOLogin.getSession().autoLogin(true);
 
@@ -72,86 +85,172 @@ public class BCOTrainDataGeneratorLauncher {
 
             LOGGER.info("init simulation setup");
 
+
             // init
+//            final int trainingSetCounter = 2;
             final int trainingSetCounter = 10;
-            int conditionCounter = 0;
-            LocationRemote location = Units.getUnit(Registries.getUnitRegistry(true).getUnitConfigByAlias("Location-8"), true, Units.LOCATION);
-            ColorableLightRemote light = Units.getUnit(Registries.getUnitRegistry(true).getUnitConfigByAlias("ColorableLight-15"), true, Units.COLORABLE_LIGHT);
+
+            LocationRemote location = Units.getUnit(Registries.getUnitRegistry(true).getUnitConfigByAlias("Location-Adhoc"), true, Units.LOCATION);
+            ColorableLightRemote light = Units.getUnit(Registries.getUnitRegistry(true).getUnitConfigByAlias("ColorableLight-Adhoc"), true, Units.COLORABLE_LIGHT);
+
             final ActionDescription absentState = ActionDescriptionProcessor.generateActionDescriptionBuilder(
-                    PresenceState.newBuilder().setValue(State.ABSENT).build(),
+                    PresenceState.newBuilder().setValue(PresenceState.State.ABSENT).build(),
                     ServiceType.PRESENCE_STATE_SERVICE,
                     location).build();
             final ActionDescription presentState = ActionDescriptionProcessor.generateActionDescriptionBuilder(
-                    PresenceState.newBuilder().setValue(State.PRESENT).build(),
+                    PresenceState.newBuilder().setValue(PresenceState.State.PRESENT).build(),
                     ServiceType.PRESENCE_STATE_SERVICE,
                     location).build();
-            //
-            location.setColor(HSBColor.newBuilder().setBrightness(1).setSaturation(0).build());
-            boolean present = false;
 
+            final ActionDescription darkState = ActionDescriptionProcessor.generateActionDescriptionBuilder(
+                    IlluminanceState.newBuilder().setValue(IlluminanceState.State.DARK).build(),
+                    ServiceType.ILLUMINANCE_STATE_SERVICE,
+                    location).build();
+            final ActionDescription sunnyState = ActionDescriptionProcessor.generateActionDescriptionBuilder(
+                    IlluminanceState.newBuilder().setValue(IlluminanceState.State.SUNNY).build(),
+                    ServiceType.ILLUMINANCE_STATE_SERVICE,
+                    location).build();
 
-            LOGGER.info("generate "+ trainingSetCounter + " training sets.");
+            LOGGER.info("prepare setup...");
+            Actions.waitForExecution(location.applyAction(presentState), TIMEOUT, TimeUnit.MILLISECONDS);
+            waitBetweenActions();
+            Actions.waitForExecution(location.setColor(HSBColor.newBuilder().setBrightness(1).setSaturation(0).build()), TIMEOUT, TimeUnit.MILLISECONDS);
+            waitUntilNextAction();
 
-            while (!Thread.interrupted() && conditionCounter <= trainingSetCounter) {
+            LOGGER.info("generate " + trainingSetCounter + " training sets.");
 
-                LOGGER.info("=== generate condition {} for training set {} ===", (present ? "PRESENT" : "ABSENT"), (conditionCounter + 1));
+            for (int i = 0; i < trainingSetCounter; i++) {
 
-                try {
-                    // check condition
-                    if (present) {
+                Collections.shuffle(trainConditions);
 
-                        // random order
-                        if (random.nextBoolean()) {
-                            // human leaving the room
-                            Actions.waitForExecution(location.applyAction(absentState));
-                            // and after a while
-                            waitBetweenActions();
-                            // they is switching the light off.
-//                            Actions.waitForExecution(location.setPowerState(PowerState.State.OFF, UnitType.LIGHT));
-                            Actions.waitForExecution(light.setPowerState(PowerState.State.OFF));
-                        } else {
+                for (TrainCondition trainCondition : trainConditions) {
 
-                            // human is switching the light off.
-//                            Actions.waitForExecution(location.setPowerState(PowerState.State.OFF, UnitType.LIGHT));
-                            Actions.waitForExecution(light.setPowerState(PowerState.State.OFF));
-                            // and after a while
-                            waitBetweenActions();
-                            // they is leaving the room
-                            Actions.waitForExecution(location.applyAction(absentState));
-                        }
-                        conditionCounter++;
-                    } else {
-                        // random order
-                        if (random.nextBoolean()) {
-                            // human entering the room
-                            Actions.waitForExecution(location.applyAction(presentState));
-                            // and after a while
-                            waitBetweenActions();
-                            // they is switching the light on.
-//                            Actions.waitForExecution(location.setPowerState(PowerState.State.ON, UnitType.LIGHT));
-                            Actions.waitForExecution(light.setPowerState(PowerState.State.ON));
-                        } else {
-                            // human is switching the light on
-//                            Actions.waitForExecution(location.setPowerState(PowerState.State.ON, UnitType.LIGHT));
-                            Actions.waitForExecution(light.setPowerState(PowerState.State.ON));
-                            // and after a while
-                            waitBetweenActions();
-                            // they is entering the room
-                            Actions.waitForExecution(location.applyAction(presentState));
-                        }
+                    // skip on manual cancel
+                    if (Thread.interrupted()) {
+                        throw new InterruptedException();
                     }
-                } catch (CancellationException ex) {
-                    ExceptionPrinter.printHistory("generator run skipped!", ex, LOGGER);
+
+                    LOGGER.info("=== generate condition {} for training set {} ===", trainCondition.name(), (i + 1));
+
+                    try {
+                        // check condition
+                        switch (trainCondition) {
+                            case ABSENCE_DARK_OFF:
+//                                if (random.nextBoolean()) {
+                                    Actions.waitForExecution(location.applyAction(absentState), TIMEOUT, TimeUnit.MILLISECONDS);
+                                    Actions.waitForExecution(location.applyAction(darkState), TIMEOUT, TimeUnit.MILLISECONDS);
+                                    waitBetweenActions();
+                                    Actions.waitForExecution(light.setPowerState(PowerState.State.OFF), TIMEOUT, TimeUnit.MILLISECONDS);
+//                                } else {
+//                                    Actions.waitForExecution(light.setPowerState(PowerState.State.OFF), TIMEOUT, TimeUnit.MILLISECONDS);
+//                                    waitBetweenActions();
+//                                    Actions.waitForExecution(location.applyAction(absentState), TIMEOUT, TimeUnit.MILLISECONDS);
+//                                    Actions.waitForExecution(location.applyAction(darkState), TIMEOUT, TimeUnit.MILLISECONDS);
+//                                }
+                                break;
+                            case PRESENCE_DARK_ON:
+//                                if (random.nextBoolean()) {
+                                    Actions.waitForExecution(location.applyAction(presentState), TIMEOUT, TimeUnit.MILLISECONDS);
+                                    Actions.waitForExecution(location.applyAction(darkState), TIMEOUT, TimeUnit.MILLISECONDS);
+                                    waitBetweenActions();
+                                    Actions.waitForExecution(light.setPowerState(PowerState.State.ON), TIMEOUT, TimeUnit.MILLISECONDS);
+//                                } else {
+//                                    Actions.waitForExecution(light.setPowerState(PowerState.State.ON), TIMEOUT, TimeUnit.MILLISECONDS);
+//                                    waitBetweenActions();
+//                                    Actions.waitForExecution(location.applyAction(presentState), TIMEOUT, TimeUnit.MILLISECONDS);
+//                                    Actions.waitForExecution(location.applyAction(darkState), TIMEOUT, TimeUnit.MILLISECONDS);
+//                                }
+                                break;
+                            case ABSENCE_SUNNY_OFF:
+//                                if (random.nextBoolean()) {
+                                    Actions.waitForExecution(location.applyAction(absentState), TIMEOUT, TimeUnit.MILLISECONDS);
+                                    Actions.waitForExecution(location.applyAction(sunnyState), TIMEOUT, TimeUnit.MILLISECONDS);
+                                    waitBetweenActions();
+                                    Actions.waitForExecution(light.setPowerState(PowerState.State.OFF), TIMEOUT, TimeUnit.MILLISECONDS);
+//                                } else {
+//                                    Actions.waitForExecution(light.setPowerState(PowerState.State.OFF), TIMEOUT, TimeUnit.MILLISECONDS);
+//                                    waitBetweenActions();
+//                                    Actions.waitForExecution(location.applyAction(absentState), TIMEOUT, TimeUnit.MILLISECONDS);
+//                                    Actions.waitForExecution(location.applyAction(sunnyState), TIMEOUT, TimeUnit.MILLISECONDS);
+//                                }
+                                break;
+                            case PRESENCE_SUNNY_OFF:
+//                                if (random.nextBoolean()) {
+                                    Actions.waitForExecution(location.applyAction(presentState), TIMEOUT, TimeUnit.MILLISECONDS);
+                                    Actions.waitForExecution(location.applyAction(sunnyState), TIMEOUT, TimeUnit.MILLISECONDS);
+                                    waitBetweenActions();
+                                    Actions.waitForExecution(light.setPowerState(PowerState.State.OFF), TIMEOUT, TimeUnit.MILLISECONDS);
+//                                } else {
+//                                    Actions.waitForExecution(light.setPowerState(PowerState.State.OFF), TIMEOUT, TimeUnit.MILLISECONDS);
+//                                    waitBetweenActions();
+//                                    Actions.waitForExecution(location.applyAction(presentState), TIMEOUT, TimeUnit.MILLISECONDS);
+//                                    Actions.waitForExecution(location.applyAction(sunnyState), TIMEOUT, TimeUnit.MILLISECONDS);
+//                                }
+                                break;
+                        }
+                        waitUntilNextAction();
+
+                        // random order
+//                        if (random.nextBoolean()) {
+                        // human leaving the room
+//                        LOGGER.info("absent send " + System.currentTimeMillis());
+//                        Actions.waitForExecution(location.applyAction(darkState), TIMEOUT, TimeUnit.MILLISECONDS);
+//                        Actions.waitForExecution(location.applyAction(lightState), TIMEOUT, TimeUnit.MILLISECONDS);
+//                        LOGGER.info("absent done " + System.currentTimeMillis());
+                        // and after a while
+
+                        // they is switching the light off.
+//                            Actions.waitForExecution(location.setPowerState(PowerState.State.OFF, UnitType.LIGHT));
+//                        LOGGER.info("off send " + System.currentTimeMillis());
+//                        Actions.waitForExecution(light.setPowerState(PowerState.State.OFF), TIMEOUT, TimeUnit.MILLISECONDS);
+//                        LOGGER.info("off done " + System.currentTimeMillis());
+//                        } else {
+//
+//                            // human is switching the light off.
+////                            Actions.waitForExecution(location.setPowerState(PowerState.State.OFF, UnitType.LIGHT));
+//                            Actions.waitForExecution(light.setPowerState(PowerState.State.OFF));
+//                            // and after a while
+//                            waitBetweenActions();
+//                            // they is leaving the room
+//                            Actions.waitForExecution(location.applyAction(absentState));
+//                        }
+
+//                        } else {
+//                            // random order
+////                        if (random.nextBoolean()) {
+//                            // human entering the room
+//                            LOGGER.info("present send "+System.currentTimeMillis());
+//                            Actions.waitForExecution(location.applyAction(presentState), TIMEOUT, TimeUnit.MILLISECONDS);
+//                            Actions.waitForExecution(location.applyAction(darkState), TIMEOUT, TimeUnit.MILLISECONDS);
+//                            LOGGER.info("present done "+System.currentTimeMillis());
+//                            // and after a while
+//                            waitBetweenActions();
+//                            // they is switching the light on.
+////                            Actions.waitForExecution(location.setPowerState(PowerState.State.ON, UnitType.LIGHT));
+//                            LOGGER.info("on send "+System.currentTimeMillis());
+//                            Actions.waitForExecution(light.setPowerState(PowerState.State.ON), TIMEOUT, TimeUnit.MILLISECONDS);
+//                            LOGGER.info("on done "+System.currentTimeMillis());
+//                        } else {
+//                            // human is switching the light on
+////                            Actions.waitForExecution(location.setPowerState(PowerState.State.ON, UnitType.LIGHT));
+//                            Actions.waitForExecution(light.setPowerState(PowerState.State.ON));
+//                            // and after a while
+//                            waitBetweenActions();
+//                            // they is entering the room
+//                            Actions.waitForExecution(location.applyAction(presentState));
+//                        }
+                    } catch (CancellationException ex) {
+                        ExceptionPrinter.printHistory("generator run skipped!", ex, LOGGER);
+                    }
                 }
-                // toggle condition
-                present = !present;
-                waitUntilNextAction();
             }
             LOGGER.info("generate finished.");
         } catch (InterruptedException e) {
             LOGGER.info("generate canceled by user.");
-        } catch (CouldNotPerformException ex) {
-            ExceptionPrinter.printHistory("Training data generation failed!", ex, LOGGER);
+        } catch (CouldNotPerformException | TimeoutException ex) {
+            if(!ExceptionProcessor.isCausedBySystemShutdown(ex)) {
+                ExceptionPrinter.printHistory("Training data generation failed!", ex, LOGGER);
+            }
             System.exit(1);
         }
         System.exit(0);
@@ -159,10 +258,8 @@ public class BCOTrainDataGeneratorLauncher {
 
     private static void waitBetweenActions() throws InterruptedException {
         TimeUnit timeUnit = TimeUnit.MILLISECONDS;
-//        long staticDelay = 1000;
-        long staticDelay = 10;
-//        int maxRandomOffset = 5000;
-        int maxRandomOffset = 100;
+        long staticDelay = 5000;
+        int maxRandomOffset = 2500;
 
         long delay = staticDelay + random.nextInt(maxRandomOffset);
         LOGGER.info("wait {} sec between actions.", timeUnit.toSeconds(delay));
