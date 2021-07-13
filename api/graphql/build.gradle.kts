ebd3c3606d0a31af9821de7e56ebc15846daab93
Tamino Huxohl <thuxohl@techfak.uni-bielefeld.de>
2021-07-13 22:32:36 +0200

    set graphql dependencies to api

--- api/graphql/build.gradle.kts
+++ api/graphql/build.gradle.kts
@@ -6,20 +6,20 @@ plugins {
 }
 
 dependencies {
-    implementation(project(":bco.dal.remote"))
-    implementation("com.graphql-java-kickstart:graphql-spring-boot-starter:8.0.0") {
+    api(project(":bco.dal.remote"))
+    api("com.graphql-java-kickstart:graphql-spring-boot-starter:8.0.0") {
         exclude(group = "org.springframework.boot", module = "spring-boot-starter-tomcat")
     }
-    implementation("io.reactivex:rxjava:1.3.8")
-    implementation("org.springframework.boot:spring-boot-starter-jetty:2.4.0")
-    implementation("org.springframework:spring-webflux:5.3.1")
-    implementation("com.google.api.graphql:rejoiner:0.3.1") {
+    api("io.reactivex:rxjava:1.3.8")
+    api("org.springframework.boot:spring-boot-starter-jetty:2.4.0")
+    api("org.springframework:spring-webflux:5.3.1")
+    api("com.google.api.graphql:rejoiner:0.3.1") {
         exclude(group = "com.graphql-java", module = "graphql-java")
     }
-    implementation("com.google.guava:guava:28.0-jre")
-    implementation("net.javacrumbs.future-converter:future-converter-java8-guava:1.2.0")
-    implementation("org.jmdns:jmdns:3.5.6")
-    implementation("io.reactivex.rxjava2:rxjava:2.2.20")
+    api("com.google.guava:guava:28.0-jre")
+    api("net.javacrumbs.future-converter:future-converter-java8-guava:1.2.0")
+    api("org.jmdns:jmdns:3.5.6")
+    api("io.reactivex.rxjava2:rxjava:2.2.20")
 }
 
 description = "BCO GraphQL API"
