c642a581f7df8401ec22cf8ccfe48d59b4592414
Divine Threepwood <divine@openbase.org>
2021-07-18 22:26:57 +0200

    move modules

---
+++ init-lib.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+git $@ submodule init
+git $@ submodule update --remote
+
+# link to branch
+git submodule foreach -q --recursive 'git checkout $(git config -f $toplevel/.gitmodules submodule.$name.branch || echo master)'
+
