537fcb4d15a4945b14cd87547a3fa649a1e19fd0
Divine Threepwood <divine@openbase.org>
2021-10-04 21:21:53 +0200

    implement install script

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+NC='\033[0m'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+WHITE='\033[0;37m'
+
+APP_NAME='bco'
+APP_NAME=${BLUE}${APP_NAME}${NC}
+echo -e "=== ${APP_NAME} project ${WHITE}cleanup${NC}" &&
+gradle clean --quiet $@ &&
+echo -e "=== ${APP_NAME} project ${WHITE}installation${NC}" &&
+./gradlew \
+    build \
+    testDeploy \
+    --exclude-task test \
+    --exclude-task javaDoc \
+    --parallel \
+    --quiet \
+    $@ &&
+echo -e "=== ${APP_NAME} was ${GREEN}successfully${NC} installed to ${WHITE}${prefix}${NC}"
