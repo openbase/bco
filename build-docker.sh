2532123c4c0aca52d0d3a380f20f0bca61e55d18
Divine Threepwood <divine@openbase.org>
2021-10-06 02:40:50 +0200

    fix docker build script.

--- build-docker.sh
+++ build-docker.sh
@@ -12,8 +12,8 @@ APP_NAME='docker images'
 APP_NAME=${BLUE}${APP_NAME}${NC}
 echo -e "=== ${APP_NAME} are building${WHITE}${NC}" &&
 docker build -f docker/Dockerfile .
-docker build -f docker/device-manager/openhab/Dockerfile .
-docker build -f docker/bco-demo/Dockerfile .
+docker build -f docker/device-manager/openhab/Dockerfile docker/device-manager/openhab
+docker build -f docker/bco-demo/Dockerfile docker/bco-demo
 
 # use this for debugging purpose: DOCKER_BUILDKIT=0 docker build -f docker/Dockerfile --progress=plain .
 echo -e "=== ${APP_NAME} were ${GREEN}successfully${NC} build.${NC}"
