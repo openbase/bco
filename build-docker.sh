635a36a6bb232401efd0114674de802e5c03e567
Divine Threepwood <divine@openbase.org>
2021-10-06 02:14:16 +0200

    fix core docker file and setup docker build script.

---
+++ build-docker.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+NC='\033[0m'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+WHITE='\033[0;37m'
+
+APP_NAME='docker images'
+APP_NAME=${BLUE}${APP_NAME}${NC}
+echo -e "=== ${APP_NAME} are building${WHITE}${NC}" &&
+docker build -f docker/Dockerfile .
+docker build -f docker/device-manager/openhab/Dockerfile .
+docker build -f docker/bco-demo/Dockerfile .
+
+# use this for debugging purpose: DOCKER_BUILDKIT=0 docker build -f docker/Dockerfile --progress=plain .
+echo -e "=== ${APP_NAME} were ${GREEN}successfully${NC} build.${NC}"
