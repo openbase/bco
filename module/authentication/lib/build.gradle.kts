7f6961ecb1e7b603f4aec336321c6f2497b030ac
Divine Threepwood <divine@openbase.org>
2021-11-10 01:45:58 +0100

    add mqtt dep

--- module/authentication/lib/build.gradle.kts
+++ module/authentication/lib/build.gradle.kts
@@ -10,6 +10,7 @@ description = "BCO Authentication Library"
 
 
 dependencies {
+    api("org.openbase:jul.communication.mqtt:_")
     api("org.openbase:jul.extension.type.processing:_")
     api("org.openbase:jul.extension.protobuf:_")
     api("org.openbase:jul.pattern.launch:_")
