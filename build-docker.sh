3b44684363e84e762676874be65992adf3f2db54
Divine Threepwood <divine@openbase.org>
2021-12-14 19:02:07 +0100

    fix demo server

--- build-docker.sh
+++ build-docker.sh
@@ -11,9 +11,9 @@ WHITE='\033[0;37m'
 APP_NAME='docker images'
 APP_NAME=${BLUE}${APP_NAME}${NC}
 echo -e "=== ${APP_NAME} are building${WHITE}${NC}" &&
-docker build -f docker/Dockerfile .
-docker build -f docker/device-manager/openhab/Dockerfile docker/device-manager/openhab
-docker build -f docker/bco-demo/Dockerfile docker/bco-demo
+docker build -f docker/Dockerfile -t openbaseorg/bco:local .
+docker build -f docker/device-manager/openhab/Dockerfile -t openbaseorg/bco-device-manager-openhab:local --build-arg BCO_BASE_IMAGE_VERSION=local docker/device-manager/openhab
+docker build -f docker/bco-demo/Dockerfile -t openbaseorg/bco-demo:local --build-arg BCO_BASE_IMAGE_VERSION=local docker/bco-demo
 
 # use this for debugging purpose: DOCKER_BUILDKIT=0 docker build -f docker/Dockerfile --progress=plain .
 echo -e "=== ${APP_NAME} were ${GREEN}successfully${NC} build.${NC}"
--- docker/bco-demo/bco-module-prepare.sh
+++ docker/bco-demo/bco-module-prepare.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-echo "nothing to prepare."
+echo "reset db"
+rm -rf /tmp/bco
+ls -la /usr/share/bco/ 
+cp -r /usr/share/bco /tmp/
+chmod -R 777 /tmp/bco
