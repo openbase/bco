d0c3c7bf3d3f7e9da8e0d2caac5cd455044c7210
Divine Threepwood <divine@openbase.org>
2022-05-25 02:12:09 +0200

    fix prepare script by using the repo linked jul version.

--- prepare.sh
+++ prepare.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 git $@ submodule init
 git $@ submodule update --remote
-
-# link to branch
-git submodule foreach -q --recursive 'git checkout $(git config -f $toplevel/.gitmodules submodule.$name.branch || echo stable)'
-
