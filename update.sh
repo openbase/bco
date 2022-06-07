c3a82f594676c38cdf99ea54a7729eb0d08bdbb4
Divine Threepwood <divine@openbase.org>
2022-06-07 20:50:51 +0200

    fix update script

--- update.sh
+++ update.sh
@@ -7,9 +7,6 @@ git pull
 git $@ submodule init
 git $@ submodule update --remote
 
-# detect project branch
-BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
-
 # switch to bco master versions
 git submodule foreach git checkout ${BRANCH_NAME}
 git submodule foreach git pull
