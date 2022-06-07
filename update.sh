445f4391d3f5aa8a419cd03bee9a781026e90ee9
Divine Threepwood <divine@openbase.org>
2022-06-07 20:55:08 +0200

    remove manual submodule updates

--- update.sh
+++ update.sh
@@ -6,8 +6,3 @@ git pull
 # make sure submodule projects are initiated and updated
 git $@ submodule init
 git $@ submodule update --remote
-
-# switch to bco master versions
-git submodule foreach git checkout ${BRANCH_NAME}
-git submodule foreach git pull
-
