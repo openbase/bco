acddddf229c85714c207585b71ac37f92fed197b
Tamino Huxohl <thuxohl@techfak.uni-bielefeld.de>
2022-08-02 21:25:23 +0200

    convert ServicesTest to kotlin and fix #67 by correcting the expectations

---
+++ module/dal/lib/src/test/java/org/openbase/bco/dal/lib/layer/service/provider/ServicesTest.kt
@@ -0,0 +1,70 @@
+package org.openbase.bco.dal.lib.layer.service.provider
+
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Timeout
+import org.openbase.bco.dal.lib.action.Action
+import org.openbase.bco.dal.lib.layer.service.Services
+import org.openbase.bco.dal.lib.state.States
+import org.openbase.bco.registry.remote.Registries
+import org.openbase.bco.registry.unit.test.AbstractBCORegistryTest
+import org.openbase.type.domotic.service.ServiceStateDescriptionType.ServiceStateDescription
+import org.openbase.type.domotic.service.ServiceTemplateType.ServiceTemplate
+import java.util.function.Consumer
+
+/*-
+ * #%L
+ * BCO DAL Library
+ * %%
+ * Copyright (C) 2014 - 2021 openbase.org
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU General Lesser Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-3.0.html>.
+ * #L%
+ */
+class ServicesTest : AbstractBCORegistryTest() {
+
+    @Test
+    @Timeout(value = 30)
+    fun testComputeActionImpact() {
+        val unitRegistry = Registries.getUnitRegistry(true)
+        val serviceStateBuilder = ServiceStateDescription.newBuilder()
+        serviceStateBuilder.unitId = unitRegistry.rootLocationConfig.id
+        serviceStateBuilder.serviceState = Services.serializeServiceState(
+            States.Power.ON,
+            true
+        )
+        serviceStateBuilder.serviceType = ServiceTemplate.ServiceType.POWER_STATE_SERVICE
+        val impact = Services.computeActionImpact(serviceStateBuilder.build())
+        impact.forEach(
+            Consumer {
+                it.actionId shouldBe Action.PRECOMPUTED_ACTION_ID
+            }
+        )
+        val impactedUnitIdList = impact
+            .map { it.serviceStateDescription.unitId }
+            .distinct()
+            .sorted()
+        val affectedUnitsUnsorted = unitRegistry.getUnitConfigsByLocationIdAndServiceType(
+            unitRegistry.rootLocationConfig.id,
+            serviceStateBuilder.serviceType
+        )
+            .map { it.id }
+            .plus(unitRegistry.rootLocationConfig.id) // by design the action impacts also contain the location itself
+            .distinct()
+            .sorted()
+
+        impactedUnitIdList shouldBe affectedUnitsUnsorted
+    }
+}
