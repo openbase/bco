780ba61e750f759b5933adb54d7f363abc27abd9
Divine Threepwood <divine@openbase.org>
2022-08-03 13:38:11 +0200

    make sure lib repos are properly initialized.

--- update.sh
+++ update.sh
@@ -3,8 +3,9 @@
 # update top level project
 git pull
 
-# make sure submodule projects are initiated and linked to its compatible hashes.
+# make sure submodule projects are initiated and linked to its compatible hashes
 git $@ submodule init
+git $@ submodule update
 
 git submodule foreach ./install.sh
 ./install.sh
