b41e1d4ae9c50492bebdbcf9c1b0ef7c064c4a7f
Divine Threepwood <divine@openbase.org>
2022-08-07 14:43:34 +0200

    lock property manipulation for mqtt tests to fix issue #71

---
+++ module/authentication/test/src/main/java/org/openbase/bco/authentication/mock/MqttIntegrationTest.kt
@@ -0,0 +1,100 @@
+package org.openbase.bco.authentication.mock
+
+import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.BeforeEach
+import org.openbase.jps.core.JPService
+import org.openbase.jps.exception.JPServiceException
+import org.openbase.jul.communication.jp.JPComHost
+import org.openbase.jul.communication.jp.JPComPort
+import org.openbase.jul.communication.mqtt.SharedMqttClient.waitForShutdown
+import org.testcontainers.containers.BindMode
+import org.testcontainers.containers.GenericContainer
+import org.testcontainers.utility.DockerImageName
+import java.nio.file.Files
+import java.nio.file.Path
+import java.time.Duration
+import java.util.*
+
+/*-
+ * #%L
+ * JUL Extension Controller
+ * %%
+ * Copyright (C) 2015 - 2021 openbase.org
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU General Lesser Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-3.0.html>.
+ * #L%
+ */ open class MqttIntegrationTest {
+
+    /** Overwrite method to set custom test properties. */
+    open fun setupTestProperties() {}
+
+    @BeforeEach
+    @Throws(JPServiceException::class)
+    fun customSetup() {
+        synchronized(configLock) {
+            resetBrokerProperties()
+            setupTestProperties()
+        }
+    }
+
+    companion object {
+        const val port = 1884
+        var mosquittoConfig: Path? = null
+        var broker: GenericContainer<*>? = null
+        val configLock = Any()
+
+        @BeforeAll
+        @Throws(Throwable::class)
+        fun setupMqtt() {
+            synchronized(configLock) {
+                mosquittoConfig = Files.createTempFile("mosquitto_", ".conf")
+                Files.write(
+                    mosquittoConfig, Arrays.asList(
+                        "allow_anonymous true",
+                        "listener " + port
+                    )
+                )
+                broker = GenericContainer(DockerImageName.parse("eclipse-mosquitto"))
+                    .withExposedPorts(port)
+                    .withFileSystemBind(
+                        mosquittoConfig.toString(),
+                        "/mosquitto/config/mosquitto.conf",
+                        BindMode.READ_ONLY
+                    )
+                    .apply { withStartupTimeout(Duration.ofSeconds(30)).start() }
+                    .also { resetBrokerProperties() }
+            }
+        }
+
+        @AfterAll
+        @Throws(Throwable::class)
+        fun tearDownMQTT() {
+            synchronized(configLock) {
+                waitForShutdown()
+                broker?.stop()
+                Files.delete(mosquittoConfig)
+            }
+        }
+
+        @Throws(JPServiceException::class)
+        private fun resetBrokerProperties() {
+            JPService.reset()
+            JPService.registerProperty(JPComPort::class.java, broker!!.firstMappedPort)
+            JPService.registerProperty(JPComHost::class.java, broker!!.host)
+            JPService.setupJUnitTestMode()
+        }
+    }
+}
