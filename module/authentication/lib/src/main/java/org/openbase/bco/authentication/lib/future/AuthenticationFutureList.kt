7ffb5e1685bec55b3edc65ed93f9f0346178b31f
Tamino Huxohl <thuxohl@techfak.uni-bielefeld.de>
2022-10-04 21:34:00 +0200

    move AuthenticationFutureList into its on file and fix #73

---
+++ module/authentication/lib/src/main/java/org/openbase/bco/authentication/lib/future/AuthenticationFutureList.kt
@@ -0,0 +1,55 @@
+package org.openbase.bco.authentication.lib.future
+
+import org.openbase.jul.iface.Shutdownable
+import org.openbase.jul.schedule.GlobalScheduledExecutorService
+import org.openbase.jul.schedule.SyncObject
+import org.slf4j.LoggerFactory
+import java.util.concurrent.ExecutionException
+import java.util.concurrent.TimeUnit
+
+object AuthenticationFutureList {
+
+    private const val SCHEDULE_RATE_IN_S = 5L
+    private const val INITIAL_DELAY_IN_S = 1L
+    private const val FUTURE_TIMEOUT_IN_MS = 1L
+
+    private val listSync = SyncObject("AuthenticatedFutureListSync")
+    private val authenticatedFutureList: MutableList<AbstractAuthenticationFuture<*, *>> = ArrayList()
+    private val logger = LoggerFactory.getLogger("AuthenticationFuture")
+
+    private fun isFailed(future: AbstractAuthenticationFuture<*, *>): Boolean {
+        try {
+            // Note: the abstract authentication future will remove itself from this list if
+            //       get finished successfully.
+            future.get(FUTURE_TIMEOUT_IN_MS, TimeUnit.MILLISECONDS)
+        } catch (ex: ExecutionException) {
+            return true
+        }
+        return false
+    }
+
+    init {
+        synchronized(listSync) {
+            // create a task which makes sure that get is called on all of these futures so that tickets are renewed
+            val responseVerificationFuture = GlobalScheduledExecutorService.scheduleAtFixedRate(
+                {
+                    synchronized(listSync) {
+                        authenticatedFutureList
+                            .filter { it.isCancelled || isFailed(it) }
+                            .let { authenticatedFutureList.removeAll(it) }
+
+                    }
+                }, INITIAL_DELAY_IN_S, SCHEDULE_RATE_IN_S, TimeUnit.SECONDS
+            )
+            Shutdownable.registerShutdownHook { responseVerificationFuture.cancel(true) }
+        }
+    }
+
+    fun addFuture(authenticationFuture: AbstractAuthenticationFuture<*, *>) {
+        synchronized(listSync) { authenticatedFutureList.add(authenticationFuture) }
+    }
+
+    fun removeFuture(authenticationFuture: AbstractAuthenticationFuture<*, *>) {
+        synchronized(listSync) { authenticatedFutureList.remove(authenticationFuture) }
+    }
+}
