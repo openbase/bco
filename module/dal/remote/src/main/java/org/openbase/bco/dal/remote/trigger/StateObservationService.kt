1a248347c5071e33e007f66e8417fd43bfb67c36
Divine Threepwood <divine@openbase.org>
2022-11-09 02:47:20 +0100

    finish StateObservationService

--- module/dal/remote/src/main/java/org/openbase/bco/dal/remote/trigger/StateObservationService.kt
+++ module/dal/remote/src/main/java/org/openbase/bco/dal/remote/trigger/StateObservationService.kt
@@ -4,32 +4,65 @@ import com.google.protobuf.Message
 import org.openbase.bco.dal.lib.layer.unit.UnitRemote
 import org.openbase.jul.pattern.Observer
 import org.openbase.jul.pattern.provider.DataProvider
+import org.openbase.jul.pattern.trigger.Trigger
 import org.openbase.jul.pattern.trigger.TriggerPriority
 
 object StateObservationService {
 
-    private val observerMap = mutableMapOf<UnitRemote<*>, InternalObserver<*>>()
+    private val observerMap = mutableMapOf<String, InternalObserver<*>>()
+    private val lock = Any()
 
-    fun <DT : Message> registerTrigger(dataObserver: Observer<DataProvider<DT>, DT>, unitRemote: UnitRemote<DT>, priority: TriggerPriority) {
-        observerMap.getOrPut(unitRemote) {
-            InternalObserver<DT>().also { unitRemote.addDataObserver(it) }
+    fun <DT : Message> registerTrigger(
+        dataObserver: Observer<DataProvider<DT>, DT>,
+        unitRemote: UnitRemote<DT>,
+        trigger: Trigger,
+    ) {
+        synchronized(lock) {
+            observerMap
+                .getOrPut(unitRemote.id) { InternalObserver<DT>().also { unitRemote.addDataObserver(it) } }
+                .let { it as InternalObserver<DT> }
+                .also { it.registerTrigger(trigger, dataObserver) }
         }
-            .let { it as InternalObserver<DT> }
-            .let { internalObserver ->
-                when(priority) {
-                    TriggerPriority.HIGH -> internalObserver.highPrioDataObserver.add(dataObserver)
-                    TriggerPriority.LOW -> internalObserver.lowPrioDataObserver.add(dataObserver)
-                }
-            }
     }
 
-    class InternalObserver<DT>: Observer<DataProvider<DT>, DT> {
-        val highPrioDataObserver = mutableListOf<Observer<DataProvider<DT>, DT>>()
-        val lowPrioDataObserver=  mutableListOf<Observer<DataProvider<DT>, DT>>()
+    fun <DT : Message> removeTrigger(
+        dataObserver: Observer<DataProvider<DT>, DT>,
+        unitRemote: UnitRemote<DT>,
+        trigger: Trigger,
+    ) {
+        synchronized(lock) {
+            observerMap[unitRemote.id]
+                .let { it as InternalObserver<DT> }
+                .also { it.removeTrigger(trigger, dataObserver) }
+                .takeIf { it.noSubscription }
+                ?.also { unitRemote.removeDataObserver(it) }
+                ?.also { observerMap.remove(unitRemote.id) }
+        }
+    }
+
+    private class InternalObserver<DT> : Observer<DataProvider<DT>, DT> {
+        private var highPrioDataObserver = listOf<Observer<DataProvider<DT>, DT>>()
+        private var lowPrioDataObserver = listOf<Observer<DataProvider<DT>, DT>>()
 
         override fun update(source: DataProvider<DT>, data: DT) {
             highPrioDataObserver.forEach { it.update(source, data) }
             lowPrioDataObserver.forEach { it.update(source, data) }
         }
+
+        fun registerTrigger(trigger: Trigger, observer: Observer<DataProvider<DT>, DT>) {
+            when (trigger.priority) {
+                TriggerPriority.HIGH -> highPrioDataObserver = highPrioDataObserver.plus(observer)
+                TriggerPriority.LOW -> lowPrioDataObserver = lowPrioDataObserver.plus(observer)
+            }
+        }
+
+        fun removeTrigger(trigger: Trigger, observer: Observer<DataProvider<DT>, DT>) {
+            when (trigger.priority) {
+                TriggerPriority.HIGH -> highPrioDataObserver = highPrioDataObserver.minus(observer)
+                TriggerPriority.LOW -> lowPrioDataObserver = lowPrioDataObserver.minus(observer)
+            }
+        }
+
+        val noSubscription = highPrioDataObserver.isEmpty() && lowPrioDataObserver.isEmpty()
     }
 }
