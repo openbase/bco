760840b928fd16bb09dc253548d5535121ef0b29
Tamino Huxohl <thuxohl@techfak.uni-bielefeld.de>
2022-11-17 21:10:26 +0100

    UnitConfigFilterImpl now filter by username

--- module/registry/unit-registry/lib/src/main/java/org/openbase/bco/registry/unit/lib/filter/UnitConfigFilterImpl.java
+++ module/registry/unit-registry/lib/src/main/java/org/openbase/bco/registry/unit/lib/filter/UnitConfigFilterImpl.java
@@ -10,12 +10,12 @@ package org.openbase.bco.registry.unit.lib.filter;
  * it under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Lesser Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Lesser Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/lgpl-3.0.html>.
@@ -100,6 +100,11 @@ public class UnitConfigFilterImpl implements ListFilter<UnitConfig>, UnitConfigF
 
         // filter by location type
         if (properties.getLocationConfig().hasLocationType() && !(properties.getLocationConfig().getLocationType() == (unitConfig.getLocationConfig().getLocationType()))) {
+            return false;
+        }
+
+        // filter by username
+        if (properties.hasUserConfig() && unitConfig.hasUserConfig() && !(properties.getUserConfig().getUserName().equals(unitConfig.getUserConfig().getUserName()))) {
             return false;
         }
 
