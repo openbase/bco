611a7ffbdc1a10cdd5f7e1093c50ebd4bf3ce1ef
Divine Threepwood <divine@openbase.org>
2023-01-21 17:48:31 +0100

    apply jul api changes by cleaning leagacy flag.

--- module/device/openhab/src/main/java/org/openbase/bco/device/openhab/OpenHABDeviceManagerLauncher.java
+++ module/device/openhab/src/main/java/org/openbase/bco/device/openhab/OpenHABDeviceManagerLauncher.java
@@ -10,28 +10,27 @@ package org.openbase.bco.device.openhab;
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/gpl-3.0.html>.
  * #L%
  */
 
+import org.openbase.bco.authentication.lib.BCO;
 import org.openbase.bco.authentication.lib.jp.JPCredentialsDirectory;
 import org.openbase.bco.device.openhab.jp.JPOpenHABURI;
 import org.openbase.bco.device.openhab.manager.OpenHABDeviceManager;
-import org.openbase.bco.authentication.lib.BCO;
 import org.openbase.bco.device.openhab.registry.OpenHABConfigSynchronizerLauncher;
 import org.openbase.bco.device.openhab.sitemap.OpenHABSitemapSynchronizerLauncher;
 import org.openbase.jps.core.JPService;
 import org.openbase.jps.preset.JPDebugMode;
 import org.openbase.jul.communication.jp.JPComHost;
-import org.openbase.jul.communication.jp.JPComLegacyMode;
 import org.openbase.jul.communication.jp.JPComPort;
 import org.openbase.jul.pattern.launch.AbstractLauncher;
 
@@ -39,16 +38,6 @@ public class OpenHABDeviceManagerLauncher extends AbstractLauncher<OpenHABDevice
 
     public OpenHABDeviceManagerLauncher() throws org.openbase.jul.exception.InstantiationException {
         super(OpenHABDeviceManagerLauncher.class, OpenHABDeviceManager.class);
-    }
-
-    @Override
-    protected void loadProperties() {
-        JPService.registerProperty(JPOpenHABURI.class);
-        JPService.registerProperty(JPDebugMode.class);
-        JPService.registerProperty(JPCredentialsDirectory.class);
-        JPService.registerProperty(JPComHost.class);
-        JPService.registerProperty(JPComPort.class);
-        JPService.registerProperty(JPComLegacyMode.class);
     }
 
     /**
@@ -59,5 +48,14 @@ public class OpenHABDeviceManagerLauncher extends AbstractLauncher<OpenHABDevice
         AbstractLauncher.main(BCO.class, OpenHABDeviceManagerLauncher.class, args, OpenHABDeviceManagerLauncher.class,
                 OpenHABConfigSynchronizerLauncher.class,
                 OpenHABSitemapSynchronizerLauncher.class);
+    }
+
+    @Override
+    protected void loadProperties() {
+        JPService.registerProperty(JPOpenHABURI.class);
+        JPService.registerProperty(JPDebugMode.class);
+        JPService.registerProperty(JPCredentialsDirectory.class);
+        JPService.registerProperty(JPComHost.class);
+        JPService.registerProperty(JPComPort.class);
     }
 }
--- module/registry/message-registry/core/src/main/java/org/openbase/bco/registry/message/core/MessageRegistryLauncher.java
+++ module/registry/message-registry/core/src/main/java/org/openbase/bco/registry/message/core/MessageRegistryLauncher.java
@@ -10,12 +10,12 @@ package org.openbase.bco.registry.message.core;
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/gpl-3.0.html>.
@@ -28,14 +28,14 @@ package org.openbase.bco.registry.message.core;
 import org.openbase.bco.authentication.lib.BCO;
 import org.openbase.bco.registry.lib.launch.AbstractRegistryLauncher;
 import org.openbase.bco.registry.message.lib.MessageRegistry;
-import org.openbase.bco.registry.message.lib.jp.*;
+import org.openbase.bco.registry.message.lib.jp.JPMessageRegistryScope;
+import org.openbase.bco.registry.message.lib.jp.JPUserMessageDatabaseDirectory;
 import org.openbase.jps.core.JPService;
 import org.openbase.jps.preset.JPDebugMode;
 import org.openbase.jps.preset.JPForce;
 import org.openbase.jps.preset.JPLocale;
 import org.openbase.jps.preset.JPReadOnly;
 import org.openbase.jul.communication.jp.JPComHost;
-import org.openbase.jul.communication.jp.JPComLegacyMode;
 import org.openbase.jul.communication.jp.JPComPort;
 import org.openbase.jul.storage.registry.jp.JPRecoverDB;
 
@@ -43,6 +43,11 @@ public class MessageRegistryLauncher extends AbstractRegistryLauncher<MessageReg
 
     public MessageRegistryLauncher() throws org.openbase.jul.exception.InstantiationException {
         super(MessageRegistry.class, MessageRegistryController.class);
+    }
+
+    public static void main(String[] args) throws Throwable {
+        BCO.printLogo();
+        main(BCO.class, MessageRegistry.class, args, MessageRegistryLauncher.class);
     }
 
     @Override
@@ -62,11 +67,5 @@ public class MessageRegistryLauncher extends AbstractRegistryLauncher<MessageReg
 
         JPService.registerProperty(JPComHost.class);
         JPService.registerProperty(JPComPort.class);
-        JPService.registerProperty(JPComLegacyMode.class);
-    }
-
-    public static void main(String[] args) throws Throwable {
-        BCO.printLogo();
-        main(BCO.class, MessageRegistry.class, args, MessageRegistryLauncher.class);
     }
 }
