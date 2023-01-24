33755309704f970669127aee78273eddadc13562
Tamino Huxohl <thuxohl@techfak.uni-bielefeld.de>
2023-01-24 21:29:07 +0100

    make AuthenticationFutureList testable by reducing timeouts in test mode, setting types to basic futures, allowing to wait for an iterations and implementing getters for list sizes

--- module/authentication/lib/src/main/java/org/openbase/bco/authentication/lib/future/AuthenticationFutureList.kt
+++ module/authentication/lib/src/main/java/org/openbase/bco/authentication/lib/future/AuthenticationFutureList.kt
@@ -3,26 +3,33 @@ package org.openbase.bco.authentication.lib.future
 import kotlinx.coroutines.async
 import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.runBlocking
+import org.openbase.jps.core.JPService
 import org.openbase.jul.iface.Shutdownable
 import org.openbase.jul.schedule.GlobalScheduledExecutorService
 import org.openbase.jul.schedule.SyncObject
-import java.util.concurrent.CancellationException
-import java.util.concurrent.ExecutionException
-import java.util.concurrent.TimeUnit
-import java.util.concurrent.TimeoutException
+import java.util.concurrent.*
+import java.util.concurrent.locks.Condition
+import java.util.concurrent.locks.ReentrantLock
+import kotlin.concurrent.withLock
 
 object AuthenticationFutureList {
 
-    private const val SCHEDULE_RATE_IN_S = 5L
+    private val SCHEDULE_RATE_IN_S = if (JPService.testMode()) 1L else 5L
     private const val INITIAL_DELAY_IN_S = 1L
     private const val FUTURE_TIMEOUT_IN_MS = 1L
 
     private val authenticatedFuturesLock = SyncObject("AuthenticatedFutureListSync")
     private val incomingFuturesLock = SyncObject("IncomingFuturesLock")
-    private val incomingFutures: MutableList<AbstractAuthenticationFuture<*, *>> = ArrayList()
-    private val authenticatedFutures: MutableList<AbstractAuthenticationFuture<*, *>> = ArrayList()
+    private val incomingFutures: MutableList<Future<*>> = ArrayList()
+    private val authenticatedFutures: MutableList<Future<*>> = ArrayList()
 
-    fun <F : AbstractAuthenticationFuture<*, *>> takeIfDone(future: F): F? = try {
+    private val notificationLock: ReentrantLock = ReentrantLock()
+    private val notificationCondition: Condition = notificationLock.newCondition()
+
+    val size get() = authenticatedFutures.size
+    val incomingSize get() = incomingFutures.size
+
+    fun <F : Future<*>> takeIfDone(future: F): F? = try {
         future.apply { get(FUTURE_TIMEOUT_IN_MS, TimeUnit.MILLISECONDS) }
     } catch (e: TimeoutException) {
         null
@@ -46,11 +53,16 @@ object AuthenticationFutureList {
 
                     runBlocking {
                         authenticatedFutures
-                            .filter { it.isCancelled }
                             .map { async { takeIfDone(it) } }
                             .awaitAll()
                             .filterNotNull()
-                            .let { authenticatedFutures.removeAll(it) }
+                            .let {
+                                authenticatedFutures.removeAll(it)
+                            }
+                    }
+
+                    notificationLock.withLock {
+                        notificationCondition.signalAll()
                     }
                 }
             }, INITIAL_DELAY_IN_S, SCHEDULE_RATE_IN_S, TimeUnit.SECONDS
@@ -58,7 +70,13 @@ object AuthenticationFutureList {
         Shutdownable.registerShutdownHook { responseVerificationFuture.cancel(true) }
     }
 
-    fun addFuture(authenticationFuture: AbstractAuthenticationFuture<*, *>) {
+    fun addFuture(authenticationFuture: Future<*>) {
         synchronized(incomingFuturesLock) { incomingFutures.add(authenticationFuture) }
+    }
+
+    fun waitForIteration() {
+        notificationLock.withLock {
+            notificationCondition.await()
+        }
     }
 }
