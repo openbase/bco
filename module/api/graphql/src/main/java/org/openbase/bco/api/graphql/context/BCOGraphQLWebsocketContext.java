d46309dee6455363e66e3235170dacf2f8a10972
Tamino Huxohl <thuxohl@techfak.uni-bielefeld.de>
2023-02-06 22:26:03 +0100

    update java graphql version

--- module/api/graphql/src/main/java/org/openbase/bco/api/graphql/context/BCOGraphQLWebsocketContext.java
+++ module/api/graphql/src/main/java/org/openbase/bco/api/graphql/context/BCOGraphQLWebsocketContext.java
@@ -10,12 +10,12 @@ package org.openbase.bco.api.graphql.context;
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/gpl-3.0.html>.
@@ -26,7 +26,6 @@ import graphql.kickstart.servlet.context.GraphQLWebSocketContext;
 import org.dataloader.DataLoaderRegistry;
 import org.openbase.jul.exception.NotAvailableException;
 
-import javax.security.auth.Subject;
 import javax.websocket.Session;
 import javax.websocket.server.HandshakeRequest;
 import java.util.Locale;
@@ -39,8 +38,8 @@ public class BCOGraphQLWebsocketContext extends AbstractBCOGraphQLContext implem
     private final String token;
     private final String languageCode;
 
-    public BCOGraphQLWebsocketContext(DataLoaderRegistry dataLoaderRegistry, Subject subject, Session session, HandshakeRequest handshakeRequest) {
-        super(dataLoaderRegistry, subject);
+    public BCOGraphQLWebsocketContext(DataLoaderRegistry dataLoaderRegistry, Session session, HandshakeRequest handshakeRequest) {
+        super(dataLoaderRegistry);
         this.session = session;
         this.handshakeRequest = handshakeRequest;
 
