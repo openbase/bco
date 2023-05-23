457221a3ae0dc84ed8197d1dd995ff5d3af0e9a9
Divine Threepwood <divine@openbase.org>
2023-05-23 20:03:25 +0200

    improve docker build log

--- build-docker.sh
+++ build-docker.sh
@@ -10,7 +10,7 @@ WHITE='\033[0;37m'
 
 APP_NAME='docker images'
 APP_NAME=${BLUE}${APP_NAME}${NC}
-echo -e "=== ${APP_NAME} are building${WHITE}${NC}" &&
+echo -e "=== ${APP_NAME} build docker image...${WHITE}${NC}" &&
 docker build -f docker/Dockerfile -t openbaseorg/bco:local .
 docker build -f docker/device-manager/openhab/Dockerfile -t openbaseorg/bco-device-manager-openhab:local --build-arg BCO_BASE_IMAGE_VERSION=local docker/device-manager/openhab
 docker build -f docker/bco-demo/Dockerfile -t openbaseorg/bco-demo:local --build-arg BCO_BASE_IMAGE_VERSION=local docker/bco-demo
