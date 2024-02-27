ea2993ddbbedb82a752df598cde8c38595433ab8
Lukas Panhorst <lukbelter@openbase.org>
2024-02-27 22:35:14 +0100

    first steps on the way to docusaurus

---
+++ doc/src/components/HomepageFeatures/index.tsx
@@ -0,0 +1,68 @@
+import clsx from 'clsx';
+import Heading from '@theme/Heading';
+import styles from './styles.module.css';
+
+type FeatureItem = {
+  title: string;
+  Svg: React.ComponentType<React.ComponentProps<'svg'>>;
+  description: JSX.Element;
+};
+
+
+const FeatureList: FeatureItem[] = [
+  {
+    title: 'Goal-Driven Automation',
+    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    description: (
+      <>
+        Your automation rules are getting too complex and you lost the global overview about your setup? Why not just express your goals and let your smart environment do the rest. 
+      </>
+    ),
+  },
+  {
+    title: 'Complete Action Transparency',
+    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    description: (
+      <>
+        You system does stupid things and you have no clue whats going on? We guarantee the introspection of decisions your system takes and present them in a human intuitive way. 
+      </>
+    ),
+  },
+  {
+    title: 'Smart Resource Allocation',
+    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    description: (
+      <>
+        Too many smart things are fighting in your home? BCO solves such interference depending on the current situation. In addition, human actions are always prioritized to ensure a pleasant smart home experience.
+      </>
+    ),
+  },
+];
+
+function Feature({title, Svg, description}: FeatureItem) {
+  return (
+    <div className={clsx('col col--4')}>
+      <div className="text--center">
+        <Svg className={styles.featureSvg} role="img" />
+      </div>
+      <div className="text--center padding-horiz--md">
+        <Heading as="h3">{title}</Heading>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
+
+export default function HomepageFeatures(): JSX.Element {
+  return (
+    <section className={styles.features}>
+      <div className="container">
+        <div className="row">
+          {FeatureList.map((props, idx) => (
+            <Feature key={idx} {...props} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
