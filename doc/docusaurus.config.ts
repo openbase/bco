7c6e78fb16e2555dc175af349fdd85069d362c37
Lukas Panhorst <lukbelter@openbase.org>
2024-03-12 20:47:39 +0100

    just got it working

--- doc/docusaurus.config.ts
+++ doc/docusaurus.config.ts
@@ -15,8 +15,8 @@ const config: Config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'openbase', // Usually your GitHub org/user name.
+  projectName: 'bco', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
