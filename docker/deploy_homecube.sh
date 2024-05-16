a87a85774348662b5a8c847470f8d14dbbacaaf1
Lukas Panhorst <lukbelter@openbase.org>
2024-05-16 21:04:25 +0200

    [Issue 64] Introduce docker compose for bco dist

---
+++ docker/deploy_homecube.sh
@@ -0,0 +1,25 @@
+!/bin/bash
+
+echo -e "allow_anonymous true\nlistener 1883" > $HOME/.mosquitto.conf
+
+# Create users and groups
+sudo adduser --system --shell /usr/sbin/nologin openhab
+sudo addgroup --system openhab
+sudo usermod -a -G openhab openhab
+sudo usermod -a -G dialout openhab
+sudo usermod -a -G tty openhab
+sudo usermod -a -G openhab ${DEFAULT_USER}
+
+sudo adduser --system --shell /usr/sbin/nologin bco
+sudo addgroup --system bco
+sudo usermod -a -G bco bco
+sudo usermod -a -G bco ${DEFAULT_USER}
+
+# Set environment variables
+export USER_ID=$(id -u openhab)
+export GROUP_ID=$(getent group openhab | cut -d: -f3)
+export BCO_USER_ID=$(id -u bco)
+export BCO_GROUP_ID=$(getent group bco | cut -d: -f3)
+
+# Run docker-compose
+docker-compose -f docker-compose-copilot.yml up --detach
