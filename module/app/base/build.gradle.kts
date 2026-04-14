6e01895f202f5965f16d7c045b7692bd954dcc45
Divine Threepwood <divine@openbase.org>
2026-04-15 01:38:21 +0200

    update readme with latest links and features. Cleanup ci trigger. Cleanup code.

--- module/app/base/build.gradle.kts
+++ module/app/base/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     id("org.openbase.bco")
-//    id("org.graalvm.buildtools.native")
     application
 }
 
@@ -13,29 +12,6 @@ repositories {
 application {
     mainClass.set("org.openbase.bco.app.util.launch.BCOLauncher")
 }
-
-//nativeBuild {
-//    imageName = "bco"
-//    mainClass = "org.openbase.bco.app.util.launch.BCOLauncher"
-//    verbose = true
-//    fallback = false
-//}
-
-//nativeImage {
-//    graalVmHome = System.getenv("JAVA_HOME")
-//    buildType { build ->
-//        build.executable(main = 'org.openbase.bco.app.util.launch.BCOLauncher')
-//    }
-//    executableName = "bco"
-//    outputDirectory = file("$buildDir/executable")
-//    arguments(
-//        "--no-fallback",
-//        "--enable-all-security-services",
-//        options.traceClassInitialization('org.openbase.bco.app.util.launch.BCOLauncher'),
-//        "--initialize-at-run-time=com.example.runtime",
-//        "--report-unsupported-elements-at-runtime"
-//    )
-//}
 
 distributions {
     main {
